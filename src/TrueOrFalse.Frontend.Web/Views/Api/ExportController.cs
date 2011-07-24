94c32b2eb70b936e2833d62dd3afc889ece41974
Stefan Noack <noackstefan@googlemail.com>
2011-07-24 18:57:35 +0200

    WIP Api/Export

---
+++ src/TrueOrFalse.Frontend.Web/Views/Api/ExportController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+using TrueOrFalse.Core;
+using TrueOrFalse.Core.Web.Context;
+using TrueOrFalse.Frontend.Web.Code;
+
+
+public class ExportController : Controller
+{
+    public ExportController()
+    {
+       
+    }
+
+    public ActionResult Questions()
+    {
+        return new ViewResult();
+    }
+}
