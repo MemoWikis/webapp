68544abe83b575c0899dbe2dc9cf09e237a39dd0
Robert Mischke <robertmischke@gmail.com>
2011-12-19 15:06:17 +0100

    autocomplete categories (wip), real call for ui

--- src/TrueOrFalse.Frontend.Web/Views/Api/CategoryApiController.cs
+++ src/TrueOrFalse.Frontend.Web/Views/Api/CategoryApiController.cs
@@ -3,25 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TrueOrFalse.Tests;
 
 namespace TrueOrFalse.View.Web.Views.Api
 {
     public class CategoryApiController : Controller
     {
+        private readonly CategorySearch _categorySearch;
+
+        public CategoryApiController(CategorySearch categorySearch)
+        {
+            _categorySearch = categorySearch;
+        }
+
         public JsonResult ByName(string term)
         {
-            var result = new List<string>
-            {   
-                        "abc",   
-                        "cde",   
-                        "fgh",   
-                        "ijk",   
-                        "lmn"
-                    };
-
-            result = result.Where(s => s.ToLower().StartsWith(term.ToLower())).ToList();
-
-            return Json(result, JsonRequestBehavior.AllowGet); 
+            return Json(_categorySearch.Run(term).Select(c => c.Name), JsonRequestBehavior.AllowGet); 
         }
 
     }
