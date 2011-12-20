2eebf4396557182329739dddf24293a212f951c3
Robert Mischke <robertmischke@gmail.com>
2011-12-20 15:48:56 +0100

    Questions_should_be_persisted, added category assertion

--- src/TrueOrFalse.Tests/2 Domain/Persistence/Question_persistence_tests.cs
+++ src/TrueOrFalse.Tests/2 Domain/Persistence/Question_persistence_tests.cs
@@ -21,7 +21,7 @@ namespace TrueOrFalse.Tests.Persistence
                                     .AddAnswer("Another name for writing acceptance tests")
                                     .AddCategory("A")
                                     .AddCategory("B")
-                                    .AddCategory("D")
+                                    .AddCategory("C")
                                  .AddQuestion("Another Question")
                                     .AddAnswer("Some answer")
                                  .Persist();
@@ -29,6 +29,8 @@ namespace TrueOrFalse.Tests.Persistence
             var questions = Resolve<QuestionRepository>().GetAll();
             questions.Count.Should().Be.EqualTo(2);
             questions[0].Categories.Count.Should().Be.EqualTo(3);
+            questions[0].Categories[0].Name.Should().Be.EqualTo("A");
+            questions[0].Categories[2].Name.Should().Be.EqualTo("C");
             questions[0].Answers.Count.Should().Be.EqualTo(2);
             questions[0].Answers[0].Text.StartsWith("Behaviour").Should().Be.True();
             questions[0].Answers[1].Text.StartsWith("Another").Should().Be.True();
