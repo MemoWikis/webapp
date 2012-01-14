53390ad7d9ec1dcb270d1de0d9f9a9fba606b5e4
Stefan Noack <noackstefan@googlemail.com>
2012-01-14 19:26:21 +0100

    https://true-or-false.atlassian.net/browse/TOF-10

--- src/TrueOrFalse.Frontend.Web/Views/Api/ExportController.cs
+++ src/TrueOrFalse.Frontend.Web/Views/Api/ExportController.cs
@@ -1,7 +1,10 @@
 ﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Web.Mvc;
 using TrueOrFalse.Core;
+using TrueOrFalse.Frontend.Web.Code;
 
 
 public class ExportController : Controller
@@ -16,17 +19,36 @@ public class ExportController : Controller
         _categoryRepository = categoryRepository;
     }
 
-    public ActionResult Questions()
+    public ViewResult Questions()
     {
         const string viewLocation = "~/Views/Api/ExportQuestions.aspx";
         var model = new ExportQuestionsModel(_questionRepository.GetAll());
         return View(viewLocation, model);
     }
 
-    public ActionResult Categories()
+    public ViewResult Categories()
     {
         const string viewLocation = "~/Views/Api/ExportCategories.aspx";
         var model = new ExportCategoriesModel(_categoryRepository.GetAll());
         return View(viewLocation, model);
+    }
+
+    [AccessOnlyLocalAttribute]
+    public ActionResult AllToLocalFile()
+    {
+        ExportToFile("Questions", Server.MapPath("~/SampleData/Questions.xml"));
+        ExportToFile("Categories", Server.MapPath("~/SampleData/Categories.xml"));
+        return RedirectToAction(Links.Knowledge, Links.KnowledgeController);
+    }
+
+    private void ExportToFile(string actionName , string filePath)
+    {
+        var request = WebRequest.Create(Url.Action(actionName, null, null, "http"));
+
+        using (var responseStream = request.GetResponse().GetResponseStream())
+            using (var fileStream = new FileStream(filePath, FileMode.Create))
+            {
+                responseStream.CopyTo(fileStream);
+            }
     }
 }
