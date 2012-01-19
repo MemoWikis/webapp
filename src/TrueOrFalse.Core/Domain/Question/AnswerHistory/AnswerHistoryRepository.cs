e9b97c828b40d116c35ffb57d303e2044a1f5680
Robert Mischke <robertmischke@gmail.com>
2012-01-19 22:00:43 +0100

    AnswerHistory (wip)

---
+++ src/TrueOrFalse.Core/Domain/Question/AnswerHistory/AnswerHistoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NHibernate;
+using Seedworks.Lib.Persistence;
+
+namespace TrueOrFalse.Core
+{
+    public class AnswerHistoryRepository : RepositoryDb<AnswerHistory> 
+    {
+        public AnswerHistoryRepository(ISession session) : base(session)
+        {
+        }
+    }
+}
---
+++ src/TrueOrFalse.Tests/2 Domain/Question.AnswerHistory/QuestionAnswerHistory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using TrueOrFalse.Core;
+
+namespace TrueOrFalse.Tests
+{
+    public class QuestionAnswerHistoryTests
+    {
+        [Test]
+        public void Persistence_Test()
+        {
+            var questionHistory = new AnswerHistory();
+            questionHistory.QuestionId = 12;
+            questionHistory.Milliseconds = 100;
+            questionHistory.UserId = 1;
+            questionHistory.AnswerText = "asdfasfsf";
+
+
+        }
+    }
+}
