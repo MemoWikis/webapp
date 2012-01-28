df9b4a43bbec8a0bee9dcb34f81877c3d1775d8c
Stefan Noack <noackstefan@googlemail.com>
2012-01-28 19:21:47 +0100

    WIP add categories

--- src/TrueOrFalse.Frontend.Web/Views/Categories/Edit/EditCategory.js
+++ src/TrueOrFalse.Frontend.Web/Views/Categories/Edit/EditCategory.js
@@ -10,11 +10,21 @@ $(function () {
     $("#txtNewRelatedCategory").autocomplete({
         source: '/Api/Category/ByName',
         minLength: 1
-    }).keyup(function() {
-        if ($(".ui-autocomplete li:textEquals('" + $(this).val() + "')").size() == 0) {
+    }).keyup(function () {
+        var matched = $(".ui-autocomplete li:textEquals('" + $(this).val() + "')");
+        if (matched.size() == 0) {
             $("#addRelatedCategory").hide();
         } else {
             $("#addRelatedCategory").show();
+            $(this).val(matched.text());
         }
+    });
+
+    var lastCategoryId = 1;
+    $("#addRelatedCategory").click(function () {
+        var catText = $("#txtNewRelatedCategory").val();
+        var catId = "category-" + lastCategoryId;
+        $("#txtNewRelatedCategory").before("<span id='" + catId + "'>" + catText + "</span> ");
+        $("#txtNewRelatedCategory").val('');
     });
 });
