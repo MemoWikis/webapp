1533164c1c327b28277d9299a95d9c8e9bb03420
Stefan Noack <noackstefan@googlemail.com>
2012-04-10 18:04:44 +0200

    WIP filter by specific users

--- src/TrueOrFalse.Core/Domain/Question/Persistence/QuestionSearchSpec.cs
+++ src/TrueOrFalse.Core/Domain/Question/Persistence/QuestionSearchSpec.cs
@@ -1,4 +1,7 @@
-﻿using Seedworks.Lib.Persistence;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Seedworks.Lib.Persistence;
 using TrueOrFalse.Core.Web.Context;
 
 namespace TrueOrFalse.Core
@@ -6,23 +9,35 @@ namespace TrueOrFalse.Core
     public class QuestionSearchSpec : SearchSpecificationBase<QuestionFilter, QuestionOrderBy>
     {
         public bool FilterByMe { get; private set; }
+        public bool FilterByAll { get; private set; }
+        public ReadOnlyCollection<int> FilterByUsers { get; private set; } 
+
         public void SetFilterByMe(bool? value)
         {
-            if (!value.HasValue) return;
+            if (!value.HasValue || value.Value == FilterByMe) return;
             FilterByMe = value.Value;
             UpdateFilter();
         }
-
-        public bool FilterByAll { get; private set; }
+        
         public void SetFilterByAll(bool? value)
         {
-            if (!value.HasValue) return;
+            if (!value.HasValue || value.Value == FilterByAll) return;
             FilterByAll = value.Value;
+            UpdateFilter();
+        }
+        
+        public void SetFilterByUsers(IEnumerable<int> userIds)
+        {
+            var newUserIds = userIds.ToList().AsReadOnly();
+            if (FilterByUsers != null && newUserIds.SequenceEqual(FilterByUsers)) return;
+
+            FilterByUsers = newUserIds;
             UpdateFilter();
         }
 
         private void UpdateFilter()
         {
+            // TODO: FilterByUsers?!
             if (FilterByMe && !FilterByAll)
             {
                 Filter.CreatorId.EqualTo(ServiceLocator.Resolve<SessionUser>().User.Id);
@@ -36,6 +51,7 @@ namespace TrueOrFalse.Core
                 Filter.CreatorId.Remove();
             }
         }
+
     }
 
     public class QuestionFilter : ConditionContainer
