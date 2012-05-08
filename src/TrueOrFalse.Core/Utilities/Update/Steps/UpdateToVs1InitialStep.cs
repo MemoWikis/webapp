fc671ce33bb2af565b1592d22ea1274b29d09c56
Robert Mischke <robertmischke@gmail.com>
2012-05-09 00:51:39 +0200

    Moved ServiceLocator to Infrastrucutre, some minor changes

--- src/TrueOrFalse.Core/Utilities/Update/Steps/UpdateToVs1InitialStep.cs
+++ src/TrueOrFalse.Core/Utilities/Update/Steps/UpdateToVs1InitialStep.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using NHibernate;
-using TrueOrFalse.Core;
+﻿using TrueOrFalse.Core;
 using TrueOrFalse.Core.Infrastructure;
 using TrueOrFalse.Core.Infrastructure.Persistence;
 
