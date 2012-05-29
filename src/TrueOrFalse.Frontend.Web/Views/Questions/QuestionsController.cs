46f620e7d7e3d57af8f481c66153ac8474077627
Stefan Noack <noackstefan@googlemail.com>
2012-05-29 21:03:21 +0200

    filter users

--- src/TrueOrFalse.Frontend.Web/Views/Questions/QuestionsController.cs
+++ src/TrueOrFalse.Frontend.Web/Views/Questions/QuestionsController.cs
@@ -29,20 +29,16 @@ public class QuestionsController : Controller
         _sessionUiData.QuestionSearchSpec.SetFilterByMe(model.FilterByMe);
         _sessionUiData.QuestionSearchSpec.SetFilterByAll(model.FilterByAll);
         _sessionUiData.QuestionSearchSpec.AddFilterByUser(model.AddFilterUser);
-
-        model.FilterByMe = _sessionUiData.QuestionSearchSpec.FilterByMe;
-        model.FilterByAll = _sessionUiData.QuestionSearchSpec.FilterByAll;
-        model.FilterByUsers =
-            _userRepository.GetByIds(_sessionUiData.QuestionSearchSpec.FilterByUsers.ToArray())
-                .ToDictionary(user => user.Id, user => user.Name);
-
+        _sessionUiData.QuestionSearchSpec.DelFilterByUser(model.DelFilterUser);
+        
         if (page.HasValue) _sessionUiData.QuestionSearchSpec.CurrentPage = page.Value;
         return View("Questions",
                     new QuestionsModel(_questionRepository.GetBy(_sessionUiData.QuestionSearchSpec), _sessionUiData.QuestionSearchSpec, _sessionUser.User.Id)
                     {Pager = new PagerModel(_sessionUiData.QuestionSearchSpec),
-                     FilterByMe = model.FilterByMe,
-                     FilterByAll = model.FilterByAll,
-                     FilterByUsers =  model.FilterByUsers});
+                     FilterByMe = _sessionUiData.QuestionSearchSpec.FilterByMe,
+                     FilterByAll = _sessionUiData.QuestionSearchSpec.FilterByAll,
+                     FilterByUsers =  _userRepository.GetByIds(_sessionUiData.QuestionSearchSpec.FilterByUsers.ToArray())
+                                        .ToDictionary(user => user.Id, user => user.Name)});
     }
 
     [HttpPost]
