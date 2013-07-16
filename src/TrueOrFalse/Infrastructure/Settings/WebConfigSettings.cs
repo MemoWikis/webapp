3e74d76b0b090e9e4f61653edf3d4ba9aa226556
Robert Mischke <robertmischke@gmail.com>
2013-07-16 23:23:27 +0200

    get smtp credentials from smtp config section

--- src/TrueOrFalse/Infrastructure/Settings/WebConfigSettings.cs
+++ src/TrueOrFalse/Infrastructure/Settings/WebConfigSettings.cs
@@ -10,10 +10,6 @@ namespace TrueOrFalse.Infrastructure
     {
         private static readonly AppSettingsReader _settingReader = new AppSettingsReader();
 
-        public static string SmtpServer { get { return Get<string>("SmtpServer"); } }
-        public static string SmtpUser { get { return Get<string>("SmtpUser"); } }
-        public static string SmtpPass { get { return Get<string>("SmtpPassword"); } }
-
         public static string EmailDefaultFrom { get { return Get<string>("EmailDefaultFrom"); } }
         
         private static T Get<T>(string settingKey){
