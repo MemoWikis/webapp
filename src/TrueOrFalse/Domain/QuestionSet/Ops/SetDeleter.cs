e6f4075e05c607a9e586dd7d21d5cb12fa5b4c12
Robert Mischke <robertmischke@gmail.com>
2013-10-10 01:12:45 +0200

    Remove set from SOLR index when deleting

--- src/TrueOrFalse/Domain/QuestionSet/Ops/SetDeleter.cs
+++ src/TrueOrFalse/Domain/QuestionSet/Ops/SetDeleter.cs
@@ -1,22 +1,24 @@
 ﻿using System.Linq;
 using NHibernate;
+using TrueOrFalse.Search;
 
 namespace TrueOrFalse
 {
     public class SetDeleter : IRegisterAsInstancePerLifetime
     {
-        private readonly ISession _session;
         private readonly SetRepository _setRepo;
+        private readonly SearchIndexSet _searchIndexSet;
 
-        public SetDeleter(ISession session, SetRepository setRepo)
+        public SetDeleter(SetRepository setRepo, SearchIndexSet searchIndexSet)
         {
-            _session = session;
             _setRepo = setRepo;
+            _searchIndexSet = searchIndexSet;
         }
 
         public void Run(int setId)
         {
             var set = _setRepo.GetById(setId);
+            _searchIndexSet.Delete(set);
             _setRepo.Delete(set);
         }
     }
