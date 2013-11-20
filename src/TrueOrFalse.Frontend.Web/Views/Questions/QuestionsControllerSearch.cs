fd9c198d471e082f0fc5b4cac09624f5537b07ce
Robert Mischke <robertmischke@gmail.com>
2013-11-20 19:23:17 +0100

    Should_order_search_result (to questions ui)

--- src/TrueOrFalse.Frontend.Web/Views/Questions/QuestionsControllerSearch.cs
+++ src/TrueOrFalse.Frontend.Web/Views/Questions/QuestionsControllerSearch.cs
@@ -22,14 +22,18 @@ public class QuestionsControllerSearch : IRegisterAsInstancePerLifetime
 
     public IList<Question> Run(QuestionsModel model, QuestionSearchSpec searchSpec)
     {
-        //if (!_sessionUiData.SearchSpecQuestionAll.OrderBy.IsSet())
-        //    _sessionUiData.SearchSpecQuestionAll.OrderBy.OrderByPersonalRelevance.Desc();
+        var orderBy = SearchQuestionsOrderBy.None;
+        if (searchSpec.OrderBy.OrderByQuality.IsCurrent()) orderBy = SearchQuestionsOrderBy.Quality;
+        else if (searchSpec.OrderBy.OrderByViews.IsCurrent()) orderBy = SearchQuestionsOrderBy.Views;
+        else if (searchSpec.OrderBy.OrderByCreationDate.IsCurrent()) orderBy = SearchQuestionsOrderBy.DateCreated;
+        else if (searchSpec.OrderBy.OrderByPersonalRelevance.IsCurrent()) orderBy = SearchQuestionsOrderBy.Valuation;
 
         var solrResult = _searchQuestions.Run(
             searchSpec.Filter.SearchTerm,
             searchSpec,
             searchSpec.Filter.CreatorId,
-            searchSpec.Filter.ValuatorId
+            searchSpec.Filter.ValuatorId,
+            orderBy: orderBy
         );
             
         return _questionRepository.GetByIds(
