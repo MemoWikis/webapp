22959a9ee8a8d29d24f0dda5d39685e6261e9adf
Robert Mischke <robertmischke@gmail.com>
2013-12-10 03:29:53 +0100

    MyISAM as Engine for unit tests, to improve test speed

---
+++ src/TrueOrFalse/Infrastructure/Persistence/Session/MySQL5FlexibleDialect.cs
@@ -0,0 +1,24 @@
+﻿using NHibernate.Dialect;
+
+namespace TrueOrFalse
+{
+    public class MySQL5FlexibleDialect : MySQL5Dialect
+    {
+        public static string Engine = "InnoDB";
+
+        public override string TableTypeString
+        {
+            get { return " ENGINE=" + MySQL5FlexibleDialect.Engine; }
+        }
+
+        public override bool HasSelfReferentialForeignKeyBug
+        {
+            get { return true; }
+        }
+
+        public override bool SupportsCascadeDelete
+        {
+            get { return true; }
+        }
+    }
+}
