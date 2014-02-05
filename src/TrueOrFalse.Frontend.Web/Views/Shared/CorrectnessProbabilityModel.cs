dcdde60220809b74cc605cc01ed8dc0003c3895e
Robert Mischke <robertmischke@gmail.com>
2014-02-05 18:01:15 +0100

    minor formating

--- src/TrueOrFalse.Frontend.Web/Views/Shared/CorrectnessProbabilityModel.cs
+++ src/TrueOrFalse.Frontend.Web/Views/Shared/CorrectnessProbabilityModel.cs
@@ -14,11 +14,10 @@ namespace TrueOrFalse
         public CorrectnessProbabilityModel(Question question, QuestionValuation questionValuationForUser)
         {
             CP = (questionValuationForUser.CorrectnessProbability == -1)
-            ? question.CorrectnessProbability
-            : questionValuationForUser.CorrectnessProbability;
+                ? question.CorrectnessProbability
+                : questionValuationForUser.CorrectnessProbability;
             CPDerivation = CP - question.CorrectnessProbability;
-            CPDerivationSign =
-                (CP <= question.CorrectnessProbability) ? "+" : "-";
+            CPDerivationSign = (CP <= question.CorrectnessProbability) ? "+" : "-";
         }
 
     }
