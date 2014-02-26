05f816ccbd64abe6ac09da6fd0a35858d513a0d7
Robert Mischke <robertmischke@gmail.com>
2014-02-26 21:44:18 +0100

    Messages (wip)

---
+++ src/TrueOrFalse/Domain/Message/Message.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrueOrFalse
+{
+    [DebuggerDisplay("Id={Id} Title={Text}")]
+    public class Message
+    {
+        public virtual string Subject { get; set; }
+        public virtual string Body { get; set;  }
+        //public virtual 
+    }
+}
