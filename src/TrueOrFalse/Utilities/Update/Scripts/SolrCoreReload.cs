71a636591783665bf68dcf5beb5881bbfb9588a2
Robert Mischke <robertmischke@gmail.com>
2014-05-29 20:58:40 +0200

    added solr administration tool

--- src/TrueOrFalse/Utilities/Update/Scripts/SolrCoreReload.cs
+++ src/TrueOrFalse/Utilities/Update/Scripts/SolrCoreReload.cs
@@ -5,40 +5,81 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
+using NHibernate.Linq;
 using TrueOrFalse.Infrastructure;
 
 namespace TrueOrFalse.Updates
 {
     public class SolrCoreReload
     {
-        public static void ReloadCategory(){Run("Category");}
-        public static void ReloadQuestion() { Run("Question"); }
-        public static void ReloadSet() { Run("Set"); }
-        public static void ReloadUser() { Run("User"); }
+        private static string _solrUrl;
+        private static string _solrPath;
 
-        public static void ReloadAll()
+        private static readonly string[] _coreNames = { "Category", "Question", "Set", "User" };
+
+        public static void ReloadCategory() { RefreshSchema("Category"); }
+        public static void ReloadQuestion() { RefreshSchema("Question"); }
+        public static void ReloadSet() { RefreshSchema("Set"); }
+        public static void ReloadUser() { RefreshSchema("User"); }
+
+        static SolrCoreReload()
         {
-            ReloadCategory();
-            ReloadQuestion();
-            ReloadSet();
-            ReloadUser();
+            if (HttpContext.Current == null)
+                return;
+
+            _solrUrl = WebConfigSettings.SolrUrl;
+            _solrPath = WebConfigSettings.SolrPath;
         }
 
-        public static void Run(string coreName)
+        public static void Set(string url, string path)
         {
-            CopySchema(coreName);
+            _solrUrl = url;
+            _solrPath = path;
+        }
 
-            var url = WebConfigSettings.SolrUrl + "admin/cores?wt=json&action=RELOAD&core=tof" + coreName;
-            WebRequest.Create(url).GetResponse();
+        public static void ReloadAllSchemas(bool testSchemas = false)
+        {
+            _coreNames.ForEach(c => RefreshSchema(c + (testSchemas ? "Test" : "")));
+        }
+
+        public static void ReloadAllConfigs(bool testSchemas = false)
+        {
+            _coreNames.ForEach(c => RefreshSchema(c + (testSchemas ? "Test" : "")));
+        }
+
+        public static void RefreshSchema(string coreName){
+            CopySchema(coreName);
+            ReoladCore(coreName);
+        }
+
+        public static void RefreshConfigs(string coreName){
+            CopyConfig(coreName);
+            ReoladCore(coreName);
         }
 
         private static void CopySchema(string coreName)
         {
             File.Copy(
                 PathTo.SolrSchema("schema" + coreName + ".xml"),
-                WebConfigSettings.SolrPath + "/tof" + coreName + "/conf/" + "schema.xml",
+                _solrPath + "/tof" + coreName + "/conf/" + "schema.xml",
                 overwrite:true
             );
+        }
+
+        private static void CopyConfig(string coreName)
+        {
+            File.Copy(
+                PathTo.SolrSchema("solrconfig" + coreName + ".xml"),
+                _solrPath + "/tof" + coreName + "/conf/" + "schema.xml",
+                overwrite: true
+            );            
+        }
+
+        private static void ReoladCore(string coreName)
+        {
+            var url = _solrUrl + "admin/cores?wt=json&action=RELOAD&core=tof" + coreName;
+            WebRequest.Create(url).GetResponse();
         }
     }
 }
