c29bef3b51ac2972a20408ebadbd866ec49bad17
Robert Mischke <robertmischke@gmail.com>
2014-07-17 21:55:11 +0200

    minor

--- src/TrueOrFalse/Domain/Knowledge/Probability/OnValuation/ProbabilityForUserUpdate.cs
+++ src/TrueOrFalse/Domain/Knowledge/Probability/OnValuation/ProbabilityForUserUpdate.cs
@@ -1,4 +1,6 @@
-﻿namespace TrueOrFalse
+﻿using NHibernate.Linq;
+
+namespace TrueOrFalse
 {
     public class ProbabilityForUserUpdate : IRegisterAsInstancePerLifetime
     {
@@ -16,19 +18,32 @@
             _probabilityCalc = probabilityCalc;
         }
 
+        public void Run(int userId)
+        {
+            _questionValuationRepository.GetByUser(userId).ForEach(Run);
+        }
+
         public void Run(int questionId, int userId)
         {
+            var questionValuation =
+                _questionValuationRepository.GetBy(questionId, userId) ?? new QuestionValuation();
+
+            Run(questionValuation);
+        }
+
+        private void Run(QuestionValuation questionValuation)
+        {
+            var questionId = questionValuation.QuestionId;
+            var userId = questionValuation.UserId;
+
             int correctnessProbability = _probabilityCalc.Run(
                                             _answerHistoryRepository.GetBy(questionId, userId));
-
-            var questionValuation = 
-                _questionValuationRepository.GetBy(questionId, userId) ?? new QuestionValuation();
 
             questionValuation.QuestionId = questionId;
             questionValuation.UserId = userId;
             questionValuation.CorrectnessProbability = correctnessProbability;
 
-            _questionValuationRepository.CreateOrUpdate(questionValuation);
+            _questionValuationRepository.CreateOrUpdate(questionValuation);            
         }
     }
 }
