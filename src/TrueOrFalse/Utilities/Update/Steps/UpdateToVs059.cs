5f2c51866dbae71a4952a66865b4a9b670a65603
Robert Mischke <robertmischke@gmail.com>
2014-08-06 19:42:41 +0200

    category migration

---
+++ src/TrueOrFalse/Utilities/Update/Steps/UpdateToVs059.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NHibernate;
+
+namespace TrueOrFalse.Updates
+{
+    public class UpdateToVs059
+    {
+        public static void Run()
+        {
+            var session = Sl.Resolve<ISession>();
+
+            var dailies = session
+                .QueryOver<Category>()
+                .Where(c => c.Type == CategoryType.Daily)
+                .List<Category>();
+
+            foreach (var daily in dailies)
+            {
+                var typeModel = (CategoryTypeDaily)daily.GetTypeModel();
+                typeModel.Title = daily.Name;
+                daily.TypeJson = typeModel.ToJson();
+
+                session.Update(daily);
+            }
+        }
+    }
+}
