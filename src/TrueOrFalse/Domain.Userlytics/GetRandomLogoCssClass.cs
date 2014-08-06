7a74f4321d6c441a1deca7e81fed19251e1bc67d
Robert Mischke <robertmischke@gmail.com>
2014-08-06 12:14:33 +0200

    Should_get_random_logo_css_class

---
+++ src/TrueOrFalse/Domain.Userlytics/GetRandomLogoCssClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class GetRandomLogoCssClass
+{
+    public static readonly IList<string> CssClasses = new List<string>{"Logo1", "Logo2", "Logo3"};
+
+    public static string Run()
+    {
+        return Run(DateTime.Now);
+    }
+
+    public static string Run(DateTime dateTime)
+    {
+        var random = new Random(dateTime.DayOfYear);
+        var index = random.Next(0, CssClasses.Count);
+
+        return CssClasses[index];
+    }
+}
