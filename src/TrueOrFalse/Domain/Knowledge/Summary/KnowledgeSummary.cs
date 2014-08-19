d46ee266cc0ab22dab9e2cb997ad36501c0b1697
Robert Mischke <robertmischke@gmail.com>
2014-08-19 15:21:23 +0200

    fixes devision by 0

--- src/TrueOrFalse/Domain/Knowledge/Summary/KnowledgeSummary.cs
+++ src/TrueOrFalse/Domain/Knowledge/Summary/KnowledgeSummary.cs
@@ -8,14 +8,21 @@ using System.Web.Mvc;
 public class KnowledgeSummary
 {
     public int Secure = 0;
-    public int SecurePercentage { get { return (int) Math.Round(Secure/(decimal) Total * 100); } }
+    public int SecurePercentage { get { return Percentage(Secure); } }
     
     public int Weak  = 0;
-    public int WeakPercentage { get { return (int)Math.Round(Weak/(decimal)Total * 100); } }
+    public int WeakPercentage { get { return Percentage(Weak); } }
 
     public int Unknown = 0;
-    public int UnknownPercentage { get { return (int)Math.Round(Unknown/(decimal)Total * 100); } }
+    public int UnknownPercentage { get { return Percentage(Unknown); } }
 
     public int Total{ get { return Secure + Weak + Unknown; }}
-}
 
+    private int Percentage(int amount)
+    {
+        if (Total == 0 || amount == 0)
+            return 0;
+
+        return (int)Math.Round(amount / (decimal)Total * 100);
+    }
+}
