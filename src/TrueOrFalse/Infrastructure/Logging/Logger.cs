1d7d1672d1b96fd90992f363da499e829a8c370b
Robert Mischke <robertmischke@gmail.com>
2014-08-20 11:33:27 +0200

    added serilog

---
+++ src/TrueOrFalse/Infrastructure/Logging/Logger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Seq;
+using Serilog;
+
+
+public class Logg
+{
+    public static ILogger r()
+    {
+        return new LoggerConfiguration()
+            .WriteTo.Seq("http://localhost:5341")
+            .CreateLogger();
+    }
+}
