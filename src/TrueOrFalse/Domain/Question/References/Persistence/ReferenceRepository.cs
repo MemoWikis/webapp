9975d0c653abd12b2120f6f2f9aa6b8bbbf9df9e
Jule <jule@true-or-false.com>
2014-08-20 21:48:50 +0200

    Reference persistence

---
+++ src/TrueOrFalse/Domain/Question/References/Persistence/ReferenceRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NHibernate;
+using Seedworks.Lib.Persistence;
+using TrueOrFalse.Search;
+
+namespace TrueOrFalse
+{
+    public class ReferenceRepository : RepositoryDb<Reference>
+    {
+        public ReferenceRepository(ISession session) : base(session)
+        {
+        }
+
+    }
+}
---
+++ src/TrueOrFalse/Domain/Question/References/Reference.cs
@@ -0,0 +1,13 @@
+﻿using Seedworks.Lib.Persistence;
+
+namespace TrueOrFalse
+{
+    public class Reference : DomainEntity
+    {
+        public virtual Question Question { get; set; }
+        public virtual Category Category { get; set; }
+
+        public virtual string AdditionalInfo { get; set; }
+        public virtual string FreeTextReference { get; set; }
+    }
+}
