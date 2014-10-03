724030792fbcd657ce4cfcc07d8399db1dc80b73
Jule <jule@true-or-false.com>
2014-10-03 18:14:58 +0200

    GetReferencesAsJson

--- src/TrueOrFalse.Frontend.Web/Views/Api/QuestionsApiController.cs
+++ src/TrueOrFalse.Frontend.Web/Views/Api/QuestionsApiController.cs
@@ -8,7 +8,23 @@ using TrueOrFalse;
 namespace Api
 {
     public class QuestionsApiController : BaseController
+
     {
+        public class ReferenceJsonResult
+        {
+            public int referenceId { get; set; }
+            public string referenceType { get; set; }
+            public int categoryId { get; set; }
+        }
+
+        private readonly QuestionRepository _questionRepo;
+
+        public QuestionsApiController(
+            QuestionRepository questionRepo)
+        {
+            _questionRepo = questionRepo;
+        }
+
         [HttpPost]
         public void Pin(string questionId)
         {
@@ -21,6 +37,22 @@ namespace Api
         {
             Resolve<UpdateQuestionTotals>()
                 .UpdateRelevancePersonal(Convert.ToInt32(questionId), _sessionUser.User, -1);
+        }
+
+        public JsonResult GetReferencesAsJson(int id)
+        {
+            var references = _questionRepo.GetById(id).References;
+            
+            var result = references.Select(r =>
+                new ReferenceJsonResult
+                {
+                    referenceId = r.Id,
+                    categoryId = r.Category == null ? -1 : r.Category.Id,
+                    referenceType = r.ReferenceType.GetName()
+                }
+            ).ToList();
+
+            return Json(result, JsonRequestBehavior.AllowGet);
         }
     }
 }
