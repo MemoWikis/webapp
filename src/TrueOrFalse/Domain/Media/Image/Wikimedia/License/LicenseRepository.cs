b22ac0ac0d809c8b902784602f8cb75637250804
Jule <jule@true-or-false.com>
2014-11-06 18:05:25 +0100

    Add most important (unported) cc licenses

--- src/TrueOrFalse/Domain/Media/Image/Wikimedia/License/LicenseRepository.cs
+++ src/TrueOrFalse/Domain/Media/Image/Wikimedia/License/LicenseRepository.cs
@@ -25,52 +25,101 @@ public class LicenseRepository
             //(Id, WikiSearchString, LicenseRequirementsType or AuthorRequired/LicenseLinkRequired/CopyOfLicenseTextRequired, LicenseLink/CopyOfLicenseTextUrl if required)
             //Run Tests "Authorized_licenses_should_contain_all_necessary_information()" and "Registered_licenses_should_not_contain_duplicates()"
             //Find template on Wikimedia "http://commons.wikimedia.org/wiki/template:" + WikiSearchString + "?uselang=de"
-            new License()
-            {
-                Id = 1,
-                WikiSearchString = "cc-by-sa-3.0",
-                LicenseApplicability = LicenseApplicability.LicenseAuthorizedAndAllRequirementsRecorded,
-                
-
-                LicenseRequirementsType = LicenseRequirementsType.Cc_By_Sa,
-                LicenseLink = "http://creativecommons.org/licenses/by-sa/3.0/legalcode",
-                
-                LicenseShortDescriptionLink = "http://creativecommons.org/licenses/by-sa/3.0/deed.de",
-                LicenseLongName = "Namensnennung - Weitergabe unter gleichen Bedingungen 3.0 Unported",
-                LicenseShortName = "CC BY-SA 3.0",
-            },
+            //Overview of all wikimedia licenses: https://commons.wikimedia.org/wiki/Commons:Image_copyright_tags_visual
             
             new License()
             {
-                Id = 2,
-                WikiSearchString = "cc-by-sa-2.5",
+                Id = 1,
+                WikiSearchString = "cc-by-1.0",
                 LicenseApplicability = LicenseApplicability.LicenseAuthorizedAndAllRequirementsRecorded,
                 
-                LicenseRequirementsType = LicenseRequirementsType.Cc_By_Sa,
-                LicenseLink = "http://creativecommons.org/licenses/by-sa/2.5/legalcode",
+                LicenseRequirementsType = LicenseRequirementsType.Cc_By,
+                LicenseLink = "http://creativecommons.org/licenses/by/1.0/legalcode",
                 
-                LicenseShortDescriptionLink = "http://creativecommons.org/licenses/by-sa/2.5/deed.de",
-                LicenseLongName = "Creative Commons: Namensnennung - Weitergabe unter gleichen Bedingungen 2.5 Generic",
-                LicenseShortName = "CC BY-SA 2.5",
+                LicenseShortDescriptionLink = "http://creativecommons.org/licenses/by/1.0/deed.de",
+                LicenseLongName = "Creative Commons: Namensnennung 1.0 Generic",
+                LicenseShortName = "CC BY 1.0",
 
             },
+
+            new License()
+            {
+                Id = 2,
+                WikiSearchString = "cc-by-2.0",
+                LicenseApplicability = LicenseApplicability.LicenseAuthorizedAndAllRequirementsRecorded,
+                
+                LicenseRequirementsType = LicenseRequirementsType.Cc_By,
+                LicenseLink = "http://creativecommons.org/licenses/by/2.0/legalcode",
+                
+                LicenseShortDescriptionLink = "http://creativecommons.org/licenses/by/2.0/deed.de",
+                LicenseLongName = "Creative Commons: Namensnennung 2.0 Generic",
+                LicenseShortName = "CC BY 2.0",
+
+            },
+
             new License()
             {
                 Id = 3,
-                WikiSearchString = "cc-by-sa-2.0",
+                WikiSearchString = "cc-by-2.5",
                 LicenseApplicability = LicenseApplicability.LicenseAuthorizedAndAllRequirementsRecorded,
                 
-                LicenseRequirementsType = LicenseRequirementsType.Cc_By_Sa,
-                LicenseLink = "http://creativecommons.org/licenses/by-sa/2.0/legalcode",
+                LicenseRequirementsType = LicenseRequirementsType.Cc_By,
+                LicenseLink = "http://creativecommons.org/licenses/by/2.5/legalcode",
                 
-                LicenseShortDescriptionLink = "http://creativecommons.org/licenses/by-sa/2.0/deed.de",
-                LicenseLongName = "Creative Commons: Namensnennung - Weitergabe unter gleichen Bedingungen 2.0 Generic",
-                LicenseShortName = "CC BY-SA 2.0",
+                LicenseShortDescriptionLink = "http://creativecommons.org/licenses/by/2.5/deed.de",
+                LicenseLongName = "Creative Commons: Namensnennung 2.5 Generic",
+                LicenseShortName = "CC BY 2.5",
 
             },
+
             new License()
             {
                 Id = 4,
+                WikiSearchString = "cc-by-3.0",
+                LicenseApplicability = LicenseApplicability.LicenseAuthorizedAndAllRequirementsRecorded,
+                
+                LicenseRequirementsType = LicenseRequirementsType.Cc_By,
+                LicenseLink = "http://creativecommons.org/licenses/by/3.0/legalcode",
+                
+                LicenseShortDescriptionLink = "http://creativecommons.org/licenses/by/3.0/deed.de",
+                LicenseLongName = "Creative Commons: Namensnennung 3.0 Unported",
+                LicenseShortName = "CC BY 3.0",
+
+            },
+
+            new License()
+            {
+                Id = 5,
+                WikiSearchString = "cc-by-3.0,2.5,2.0,1.0",
+                LicenseApplicability = LicenseApplicability.LicenseAuthorizedAndAllRequirementsRecorded,
+                
+                LicenseRequirementsType = LicenseRequirementsType.Cc_By,
+                LicenseLink = "http://creativecommons.org/licenses/by/3.0/legalcode",
+                
+                LicenseShortDescriptionLink = "http://creativecommons.org/licenses/by/3.0/deed.de",
+                LicenseLongName = "Creative Commons: Namensnennung 3.0 Unported",
+                LicenseShortName = "CC BY 3.0",
+
+            },
+
+            new License()
+            {
+                Id = 6,
+                WikiSearchString = "cc-sa-1.0",
+                LicenseApplicability = LicenseApplicability.LicenseAuthorizedAndAllRequirementsRecorded,
+                
+                LicenseRequirementsType = LicenseRequirementsType.Cc_Sa,
+                LicenseLink = "http://creativecommons.org/licenses/sa/1.0/legalcode",
+                
+                LicenseShortDescriptionLink = "http://creativecommons.org/licenses/sa/1.0/deed.de",
+                LicenseLongName = "Creative Commons: Weitergabe unter gleichen Bedingungen 1.0 Generic",
+                LicenseShortName = "CC SA 1.0",
+
+            },
+
+            new License()
+            {
+                Id = 7,
                 WikiSearchString = "cc-by-sa-1.0",
                 LicenseApplicability = LicenseApplicability.LicenseAuthorizedAndAllRequirementsRecorded,
                 
@@ -85,7 +134,52 @@ public class LicenseRepository
 
             new License()
             {
-                Id = 5,
+                Id = 8,
+                WikiSearchString = "cc-by-sa-2.0",
+                LicenseApplicability = LicenseApplicability.LicenseAuthorizedAndAllRequirementsRecorded,
+                
+                LicenseRequirementsType = LicenseRequirementsType.Cc_By_Sa,
+                LicenseLink = "http://creativecommons.org/licenses/by-sa/2.0/legalcode",
+                
+                LicenseShortDescriptionLink = "http://creativecommons.org/licenses/by-sa/2.0/deed.de",
+                LicenseLongName = "Creative Commons: Namensnennung - Weitergabe unter gleichen Bedingungen 2.0 Generic",
+                LicenseShortName = "CC BY-SA 2.0",
+
+            },
+
+            new License()
+            {
+                Id = 9,
+                WikiSearchString = "cc-by-sa-2.5",
+                LicenseApplicability = LicenseApplicability.LicenseAuthorizedAndAllRequirementsRecorded,
+                
+                LicenseRequirementsType = LicenseRequirementsType.Cc_By_Sa,
+                LicenseLink = "http://creativecommons.org/licenses/by-sa/2.5/legalcode",
+                
+                LicenseShortDescriptionLink = "http://creativecommons.org/licenses/by-sa/2.5/deed.de",
+                LicenseLongName = "Creative Commons: Namensnennung - Weitergabe unter gleichen Bedingungen 2.5 Generic",
+                LicenseShortName = "CC BY-SA 2.5",
+
+            },
+
+            new License()
+            {
+                Id = 10,
+                WikiSearchString = "cc-by-sa-3.0",
+                LicenseApplicability = LicenseApplicability.LicenseAuthorizedAndAllRequirementsRecorded,
+                
+
+                LicenseRequirementsType = LicenseRequirementsType.Cc_By_Sa,
+                LicenseLink = "http://creativecommons.org/licenses/by-sa/3.0/legalcode",
+                
+                LicenseShortDescriptionLink = "http://creativecommons.org/licenses/by-sa/3.0/deed.de",
+                LicenseLongName = "Namensnennung - Weitergabe unter gleichen Bedingungen 3.0 Unported",
+                LicenseShortName = "CC BY-SA 3.0",
+            },
+            
+            new License()
+            {
+                Id = 11,
                 WikiSearchString = "cc-by-sa-3.0,2.5,2.0,1.0",
                 LicenseApplicability = LicenseApplicability.LicenseAuthorizedAndAllRequirementsRecorded,
                 
