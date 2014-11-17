23cb479c29363853d6464a175daea3ba7080913d
Jule <jule@true-or-false.com>
2014-11-17 19:44:27 +0100

    Parse image markup (wip)

--- src/TrueOrFalse/Domain/Media/Image/Wikimedia/Markup2Html/Markup2Html.cs
+++ src/TrueOrFalse/Domain/Media/Image/Wikimedia/Markup2Html/Markup2Html.cs
@@ -9,17 +9,47 @@ namespace TrueOrFalse.WikiMarkup
 {
     public class Markup2Html
     {
-        public static string Run(string markup)
+        public static string TransformAll(string markup)
         {
-            //finds "[[de:pageName|displayName]]" transforms to "displayName"
-            markup = Regex.Replace(markup, "\\[\\[.*?\\|(.*?)\\]\\]", match => match.Groups[1].Value);
-            //finds "[[displayName]]" transforms to "displayName"
-            markup = Regex.Replace(markup, "\\[\\[(.*?)\\]\\]", match => match.Groups[1].Value);
-            //finds "[displayName]" transforms to "displayName"
-            markup = Regex.Replace(markup, "\\[(.*?)\\]", match => match.Groups[1].Value);
+            markup = TransformFormattingMarkup(markup);
+
+            markup = TransformLinkMarkup(markup);
+
+            return markup;
+        }
+
+        public static string TransformFormattingMarkup(string markup)
+        {
+            //Overview wikimedia markup formatting: https://www.mediawiki.org/wiki/Help:Formatting
+            
+            //finds "''''displayName''''" transforms to "<i><b>displayName</b></i>"
+            markup = Regex.Replace(markup, "''(.*?)''", match => "<i><b>" + match.Groups[1].Value + "</b></i>");
+            //finds "'''displayName'''" transforms to "<b>displayName</b>"
+            markup = Regex.Replace(markup, "''(.*?)''", match => "<b>" + match.Groups[1].Value + "</b>");
             //finds "''displayName''" transforms to "<i>displayName</i>"
             markup = Regex.Replace(markup, "''(.*?)''", match => "<i>" + match.Groups[1].Value + "</i>");
-            //$temp: Add //finds "[http://link.com  diplayname]" transforms to ... ?
+
+            return markup;
+        }
+
+        public static string TransformLinkMarkup(string markup)
+        {
+            //Overview wikimedia markup links: https://www.mediawiki.org/wiki/Help:Links
+
+            //finds "[[User: userName|displayName]]" transforms to "displayName"
+            //http://commons.wikimedia.org/wiki/File:20100723_liege07.JPG
+            //|Author=[[User:Jeanhousen|Jean Housen]]
+            markup = Regex.Replace(markup, "\\[\\[User:.*?\\|(.*?)\\]\\]", match => match.Groups[1].Value);
+            //finds "[[pageName|displayName]]" (internal piped link) transform to "displayName"
+            //matches also: "[[:de:pageName|displayName]]" (link to wikipedia article in indicated language, displayed as "de: displayName") transforms to "displayName"
+            //matches also: "[[de:pageName|displayName]]" (link to wikipedia article in indicated language, displayed as "displayName") transforms to "displayName"
+            markup = Regex.Replace(markup, "\\[\\[.*?\\|(.*?)\\]\\]", match => match.Groups[1].Value);
+            //finds "[[displayName]]" (internal link) transforms to "displayName"
+            markup = Regex.Replace(markup, "\\[\\[(.*?)\\]\\]", match => match.Groups[1].Value);
+            //finds "[http://link.com  diplayName]" transforms to "displayName, http://link.com"
+            markup = Regex.Replace(markup, "\\[(http://[\\S]*)\\s(.*)\\]", match => match.Groups[2].Value + ", " + match.Groups[1]);
+            //finds "[displayName]" (numbered external link, displayed on wiki as "[1]") transforms to "displayName"
+            markup = Regex.Replace(markup, "\\[(.*?)\\]", match => match.Groups[1].Value);
 
             return markup;
         }
