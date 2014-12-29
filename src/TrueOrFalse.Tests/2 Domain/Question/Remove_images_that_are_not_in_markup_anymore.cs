3523e794e04b6cbb04a93506da859dfdf04550e1
Robert Mischke <robertmischke@gmail.com>
2014-12-29 22:49:31 +0100

    DeleteUnusedImages.Run

---
+++ src/TrueOrFalse.Tests/2 Domain/Question/Remove_images_that_are_not_in_markup_anymore.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace TrueOrFalse.Tests
+{
+    public class Remove_images_that_are_not_in_markup_anymore
+    {
+        [Test]
+        public void Detect_removal()
+        {
+
+        }
+    }
+}
---
+++ src/TrueOrFalse/Domain/Question/Question.Image/DeleteUnusedImages.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+
+namespace TrueOrFalse.Web
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class DeleteUnusedImages
+    {
+        public static void Run(string markup, int questionId)
+        {
+            var searchString = "/images/questions/" + questionId;
+
+            if (markup.ToLower().Contains(searchString))
+                return;
+
+            var imageSettings = new QuestionImageSettings();
+            
+            var filesToDelete = Directory.GetFiles(imageSettings.BasePath, questionId + "_*");
+            foreach (var file in filesToDelete)
+                File.Exists(file);
+
+            var imageRepo = ServiceLocator.R<ImageMetaDataRepository>();
+            var imageToDelete = imageRepo.GetBy(questionId, ImageType.Question);
+            
+            if(imageToDelete != null)
+                imageRepo.Delete(imageToDelete);
+        }
+    }
+}
