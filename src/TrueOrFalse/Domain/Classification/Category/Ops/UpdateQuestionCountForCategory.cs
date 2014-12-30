c4809167c92eb5a065fbd8bd01bad8deadb9099b
Jule <jule@true-or-false.com>
2014-12-30 15:24:24 +0100

    Questions for category: restrict to public questions

--- src/TrueOrFalse/Domain/Classification/Category/Ops/UpdateQuestionCountForCategory.cs
+++ src/TrueOrFalse/Domain/Classification/Category/Ops/UpdateQuestionCountForCategory.cs
@@ -20,7 +20,12 @@ namespace TrueOrFalse
             {
                 var query =
                     "UPDATE category SET CountQuestions = " +
-                    "(SELECT COUNT(*) FROM categoriestoquestions WHERE Category_id = category.Id )" +
+                    " (SELECT COUNT(*) " +
+                    "  FROM categoriestoquestions as cq" +
+                    "  LEFT JOIN  question as q" +
+                    "  ON cq.Question_id = q.Id " +
+                    "  WHERE Category_id = category.Id" +
+                    "  AND q.Visibility = 0) " +
                     "WHERE Id = " + category.Id;
 
                 _session.CreateSQLQuery(query).ExecuteUpdate();                
