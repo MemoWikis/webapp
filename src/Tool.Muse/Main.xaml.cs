4912ed746a3c04cc30437a7081ded8cf35b4e742
Robert Mischke <robertmischke@gmail.com>
2015-04-25 22:27:38 +0200

    Tool.Muse initial structure

---
+++ src/Tool.Muse/Main.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Tool.Muse
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Main : Window
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnStartMuseIO_OnClick(object sender, RoutedEventArgs e)
+        {
+            Process.Start(
+                "muse-io.exe", 
+                "--preset 14  --osc osc.udp://localhost:5000");
+        }
+    }
+}
