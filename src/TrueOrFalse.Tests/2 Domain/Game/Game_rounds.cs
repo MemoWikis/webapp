ec831a7718a7ab8b855541548d47cf42e872835c
Robert Mischke <robertmischke@gmail.com>
2015-05-16 15:39:48 +0200

    Games, Rounds persistence (AddRoundsToGame)

--- src/TrueOrFalse.Tests/2 Domain/Game/Game_rounds.cs
+++ src/TrueOrFalse.Tests/2 Domain/Game/Game_rounds.cs
@@ -1,10 +1,11 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 using TrueOrFalse.Tests;
 
 public class Game_rounds : BaseTest
 {
     [Test]
-    public void Should_create_rounds()
+    public void Should_persist_rounds()
     {
         var gameRepo = R<GameRepo>();
         var game = ContextGame.New().Add().Persist().All[0];
@@ -13,7 +14,7 @@ public class Game_rounds : BaseTest
                 .AddQuestion("A", "AS")
                 .AddQuestion("B", "BS")
                 .AddQuestion("C", "CS")
-                .AddQuestion("B", "CS")
+                .AddQuestion("D", "DS")
             .Persist()
             .All[0];
 
@@ -23,5 +24,33 @@ public class Game_rounds : BaseTest
         gameRepo.Update(game);
         gameRepo.Flush();
         
+    }
+
+    [Test]
+    public void Should_create_random_rounds()
+    {
+        var set = ContextSet.New()
+            .AddSet("Set")
+                .AddQuestion("A", "AS")
+                .AddQuestion("B", "BS")
+                .AddQuestion("C", "CS")
+                .AddQuestion("D", "DS")
+            .Persist()
+            .All[0];
+
+        var game = ContextGame.New().Add().Persist().All[0];
+        game.RoundCount = 50;
+        game.Sets.Add(set);
+
+        var firstItemIsACount = 0;
+        for (var i = 0; i < 400; i++)
+        {
+            R<AddRoundsToGame>().Run(game);
+            if (game.Rounds[i].Question.Text == "A")
+                firstItemIsACount++;
+        }
+
+        Assert.That(firstItemIsACount > 85 && firstItemIsACount < 115, Is.True);
+        Console.WriteLine(firstItemIsACount);
     }
 }
---
+++ src/TrueOrFalse/Infrastructure/ListExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+
+public static class ListExtensions
+{
+    public static void Shuffle<T>(this IList<T> list)
+    {
+        //http://stackoverflow.com/questions/273313/randomize-a-listt-in-c-sharp
+        var provider = new RNGCryptoServiceProvider();
+        int n = list.Count;
+        while (n > 1)
+        {
+            byte[] box = new byte[1];
+            do provider.GetBytes(box);
+            while (!(box[0] < n * (Byte.MaxValue / n)));
+            int k = (box[0] % n);
+            n--;
+            T value = list[k];
+            list[k] = list[n];
+            list[n] = value;
+        }
+    }
+}
