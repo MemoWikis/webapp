94606b9cb74dd30cfccfc2d365a5accd74f18b25
Robert Mischke <robertmischke@gmail.com>
2015-06-04 23:05:42 +0200

    autocomplete sets (wip)

---
+++ src/TrueOrFalse.Frontend.Web/Scripts/autocompletes/AutocompleteSets.js
@@ -0,0 +1,16 @@
+﻿var AutocompleteSets = (function () {
+    function AutocompleteSets(inputSelector) {
+        var elemContainer = this._elemInput.closest(".JS-RelatedCategories");
+
+        var autocomplete = $(inputSelector).autocomplete({
+            minLength: 0,
+            source: function (request, response) {
+                $.get("/Api/Sets/ByName?term=" + request.term, function (data) {
+                    response(data);
+                });
+            }
+        });
+    }
+    return AutocompleteSets;
+})();
+//# sourceMappingURL=AutocompleteSets.js.map
---
+++ src/TrueOrFalse.Frontend.Web/Scripts/autocompletes/AutocompleteSets.ts
@@ -0,0 +1,23 @@
+﻿class AutocompleteSets {
+    OnAdd: (setId?: number) => void;
+    OnRemove: (setId?: number) => void;
+
+    _elemInput: JQuery;
+
+    constructor(inputSelector: string) {
+
+        var elemContainer = this._elemInput.closest(".JS-RelatedCategories");
+
+        var autocomplete = $(inputSelector).autocomplete({
+            minLength: 0,
+            source: function(request, response) {
+                $.get("/Api/Sets/ByName?term=" + request.term, function (data) {
+                    response(data);
+                });                
+            }
+
+
+        });
+
+    }
+ }
