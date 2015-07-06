7bab0e96141d2e2f88deb9dd1a297df0de1dd268
Jule <jule@true-or-false.com>
2015-07-06 13:45:31 +0200

    LearningSession, test

--- src/TrueOrFalse.Tests/2 Domain/LearningSession_persistence.cs
+++ src/TrueOrFalse.Tests/2 Domain/LearningSession_persistence.cs
@@ -1,18 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 
 public class LearningSession_persistence : BaseTest
 {
     [Test]
     public void Should_persist()
     {
+        var user = ContextUser.GetUser();
+        var set = ContextSet.New().AddSet("Setname", numberOfQuestions: 5).Persist().All[0];
+
+        Sl.R<SessionUser>().Login(user);
+
         var learningSession = new LearningSession
         {
-            
+            User = user,
+            Steps = GetLearningSessionSteps.Run(set)
         };
+
+        R<LearningSessionRepo>().Create(learningSession);
+
+        RecycleContainer();
+
+        var learningSessionFromDb = R<LearningSessionRepo>().GetById(learningSession.Id);
+        Assert.That(learningSessionFromDb.Steps.Count, Is.EqualTo(5));
     }
 }
