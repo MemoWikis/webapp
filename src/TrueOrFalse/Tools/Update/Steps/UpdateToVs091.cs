c7f43e202cd25c22d4a82e691729f52a90814683
Jule <jule@true-or-false.com>
2015-07-28 15:42:20 +0200

    Fix update step confusion

--- src/TrueOrFalse/Tools/Update/Steps/UpdateToVs091.cs
+++ src/TrueOrFalse/Tools/Update/Steps/UpdateToVs091.cs
@@ -2,15 +2,14 @@
 
 namespace TrueOrFalse.Updates
 {
-    public class UpdateToVs091
+    public class UpdateToVs089
     {
         public static void Run()
         {
             Sl.Resolve<ISession>()
               .CreateSQLQuery(
                 @"ALTER TABLE `imagemetadata`
-	                ADD COLUMN `LicenseState` TINYINT NULL DEFAULT NULL AFTER `Notifications`,
-	                DROP COLUMN `LicenseState`;"
+	                ADD COLUMN `LicenseState` TINYINT NULL DEFAULT NULL AFTER `Notifications`;"
             ).ExecuteUpdate();
 
         }
