722b17ad654f60b6a37309b39c020751a7b9d85d
Robert Mischke <robertmischke@gmail.com>
2015-08-10 23:44:49 +0200

    remove unused files #42490955987902

--- src/TrueOrFalse.Frontend.Web/Views/Help/HelpController.cs
+++ src/TrueOrFalse.Frontend.Web/Views/Help/HelpController.cs
@@ -4,23 +4,4 @@ public class HelpController : BaseController
 {
     [SetMenu(MenuEntry.Help)]
     public ActionResult FAQ() {return View(new HelpModel()); }
-    
-    [SetMenu(MenuEntry.Help)]
-    public ActionResult Willkommen(){ AddActionToHistory(); return View(new HelpModel()); }
-    [SetMenu(MenuEntry.Help)]
-    public ActionResult DatenSicherheit() { AddActionToHistory(); return View(new HelpModel()); }
-    [SetMenu(MenuEntry.Help)]
-    public ActionResult Reputation() { AddActionToHistory(); return View(new HelpModel()); }
-    [SetMenu(MenuEntry.Help)]
-    public ActionResult Wissen() { AddActionToHistory(); return View(new HelpModel()); }
-    [SetMenu(MenuEntry.Help)]
-    public ActionResult UrheberrechtInhalte() { AddActionToHistory(); return View(new HelpModel()); }
-
-    public void AddActionToHistory(){
-        _sessionUiData.VisitedHelpPages.Add(new HelpHistoryItem(GetActionName()));
-    }
-
-    public string GetActionName(){
-        return this.RouteData.Values["action"].ToString();
-    }
 }
