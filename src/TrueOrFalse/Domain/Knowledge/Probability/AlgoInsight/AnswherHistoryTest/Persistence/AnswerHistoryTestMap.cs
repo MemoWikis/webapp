263c0449b093a9023c4c9556405f040ed1aaf8ef
Robert Mischke <robertmischke@gmail.com>
2015-09-07 23:52:08 +0200

    AlgoInsight (wip)

--- src/TrueOrFalse/Domain/Knowledge/Probability/AlgoInsight/AnswherHistoryTest/Persistence/AnswerHistoryTestMap.cs
+++ src/TrueOrFalse/Domain/Knowledge/Probability/AlgoInsight/AnswherHistoryTest/Persistence/AnswerHistoryTestMap.cs
@@ -5,5 +5,13 @@ public class AnswerHistoryTestMap : ClassMap<AnswerHistoryTest>
 	public AnswerHistoryTestMap()
 	{
 		Table("AnswerHistory_Test");
-	}
+
+		Id(x => x.Id);
+
+		Map(x => x.AlgoId);
+		References(x => x.AnswerHistory);
+
+        Map(x => x.Probability);
+        Map(x => x.IsCorrect);
+    }
 }
