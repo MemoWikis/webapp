30916dfe5762d7f8f7869f663785a3d7426cc27a
Robert Mischke <robertmischke@gmail.com>
2015-09-12 14:55:46 +0200

    Probability: added logging

--- src/TrueOrFalse/Domain/Knowledge/Probability/ForCategory/ProbabilityUpdate_Category.cs
+++ src/TrueOrFalse/Domain/Knowledge/Probability/ForCategory/ProbabilityUpdate_Category.cs
@@ -1,18 +1,28 @@
-﻿public class ProbabilityUpdate_Category
+﻿using System.Diagnostics;
+
+public class ProbabilityUpdate_Category
 {
     public static void Run()
     {
+        var sp = Stopwatch.StartNew();
+
         foreach (var category in Sl.R<CategoryRepository>().GetAll())
             Run(category);
+
+        Logg.r().Information("Calculated all category probabilities in {elapsed} ", sp.Elapsed);
     }
 
     public static void Run(Category category)
     {
+        var sp = Stopwatch.StartNew();
+
         var answerHistoryItems = Sl.R<AnswerHistoryRepository>().GetByCategories(category.Id);
 
         category.CorrectnessProbability =
             ProbabilityCalc_Category.Run(answerHistoryItems);
 
         Sl.R<CategoryRepository>().Update(category);
+
+        Logg.r().Information("Calculated probability in {elapsed} for category {categoryId}", sp.Elapsed, category.Id);
     }
 }
--- src/TrueOrFalse/Domain/Knowledge/Probability/ForQuestion/ProbabilityUpdate_Question.cs
+++ src/TrueOrFalse/Domain/Knowledge/Probability/ForQuestion/ProbabilityUpdate_Question.cs
@@ -1,9 +1,15 @@
-﻿public class ProbabilityUpdate_Question : IRegisterAsInstancePerLifetime
+﻿using System.Diagnostics;
+
+public class ProbabilityUpdate_Question : IRegisterAsInstancePerLifetime
 {
     public static void Run()
     {
+        var sp = Stopwatch.StartNew();
+
         foreach (var question in Sl.R<QuestionRepository>().GetAll())
             Run(question);
+
+        Logg.r().Information("Calculated all question probabilities in {elapsed} ", sp.Elapsed);
     }
 
     public static void Run(Question question)
