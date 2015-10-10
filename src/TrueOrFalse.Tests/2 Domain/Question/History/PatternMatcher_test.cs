5014785324f46e8a3b9439a4bcb7d882437dc404
Jule <jule@true-or-false.com>
2015-10-10 22:37:01 +0200

    PatternMatcher_test (wip)

--- src/TrueOrFalse.Tests/2 Domain/Question/History/PatternMatcher_test.cs
+++ src/TrueOrFalse.Tests/2 Domain/Question/History/PatternMatcher_test.cs
@@ -22,7 +22,7 @@ public class PatternMatcher_test
 
         //ASSERT
         Assert.That(matches.Count, Is.EqualTo(1));
-        Assert.That(matches[0].Name, Is.EqualTo("X-Days-3"));
+        Assert.That(matches[0].Name, Is.EqualTo("X-Days-Exactly-3"));
     }
 
     [Test]
@@ -42,7 +42,33 @@ public class PatternMatcher_test
 
         //ASSERT
         Assert.That(matches.Count, Is.EqualTo(1));
-        Assert.That(matches[0].Name, Is.EqualTo("X-Days-4"));
+        Assert.That(matches[0].Name, Is.EqualTo("X-Days-Exactly-4"));
+    }
+
+    [Test]
+    public void Should_determine_max_streak()
+    {
+        //ARRANGE
+        var listOfAnswers = new List<AnswerHistory>()
+        {
+            new AnswerHistory {DateCreated = new DateTime(2015, 12, 3, 14, 00, 21)},
+            new AnswerHistory {DateCreated = new DateTime(2015, 12, 4, 14, 00, 21)},
+            new AnswerHistory {DateCreated = new DateTime(2015, 12, 6, 14, 00, 21)},
+            new AnswerHistory {DateCreated = new DateTime(2015, 12, 6, 14, 00, 21)},
+            new AnswerHistory {DateCreated = new DateTime(2015, 12, 6, 14, 00, 21)},
+            new AnswerHistory {DateCreated = new DateTime(2015, 12, 6, 14, 00, 21)},
+            new AnswerHistory {DateCreated = new DateTime(2015, 12, 7, 14, 00, 21)},
+            new AnswerHistory {DateCreated = new DateTime(2015, 12, 8, 14, 00, 21)},
+            new AnswerHistory {DateCreated = new DateTime(2015, 12, 9, 14, 00, 21)},
+            new AnswerHistory {DateCreated = new DateTime(2015, 12, 10, 14, 00, 21)},
+        };
+
+        //ACT
+        var maxStreak = MaxStreak.MaxStreakDays(listOfAnswers);
+
+        //ASSERT
+        Assert.That(maxStreak, Is.EqualTo(5));
+        Assert.That(MaxStreak.MaxStreakDays(new List<AnswerHistory>()), Is.EqualTo(0));
     }
 }
 
@@ -75,8 +101,8 @@ public class AnswerPatternRepo
     {
         return new List<IAnswerPattern>()
         {
-            new XDays3(),
-            new XDays4(),
+            new XDaysExactly3(),
+            new XDaysExactly4(),
         };
     }
 }
@@ -87,7 +113,7 @@ public interface IAnswerPattern
     bool IsMatch(List<AnswerHistory> listOfAnswers);
 }
 
-public class XDays
+public class XDaysExactly
 {
     public bool IsMatch(List<AnswerHistory> listOfAnswers, int days)
     {
@@ -115,9 +141,9 @@ public class XDays
     }
 }
 
-public class XDays3 : XDays, IAnswerPattern
+public class XDaysExactly3 : XDaysExactly, IAnswerPattern
 {
-    public string Name {get { return "X-Days-3"; } }
+    public string Name {get { return "X-Days-Exactly-3"; } }
 
     public bool IsMatch(List<AnswerHistory> listOfAnswers)
     {
@@ -125,12 +151,67 @@ public class XDays3 : XDays, IAnswerPattern
     }
 }
 
-public class XDays4 : XDays, IAnswerPattern
+public class XDaysExactly4 : XDaysExactly, IAnswerPattern
 {
-    public string Name { get { return "X-Days-4"; } }
+    public string Name { get { return "X-Days-Exactly-4"; } }
 
     public bool IsMatch(List<AnswerHistory> listOfAnswers)
     {
         return IsMatch(listOfAnswers, 4);
+    }
+}
+
+public class MaxStreak
+{
+    public static int MaxStreakDays(List<AnswerHistory> listOfAnswers)
+    {
+        var listOfAnswersOrderedByDate = listOfAnswers.OrderBy(x => x.DateCreated).ToList();
+        var count = listOfAnswersOrderedByDate.Count;
+        var maxDays = 0;
+
+        for (var i = 0; i < count; i++)
+        {
+            var maxDaysRange = 0;
+            var range = listOfAnswersOrderedByDate.GetRange(i, count - i);
+            var previousDate = DateTime.MinValue.Date;
+            for (var j = 0; j < range.Count; j++)
+            {
+                if (j == 0)
+                {
+                    previousDate = range[j].DateCreated.Date;
+                    maxDaysRange++;
+                    continue;
+                }
+
+                if(range[j].DateCreated.Date == previousDate) continue;
+
+                if (range.All(ah => ah.DateCreated.Date != previousDate.AddDays(1))) break;
+                
+                previousDate = range[j].DateCreated.Date;
+                maxDaysRange ++;
+            }
+
+            if (maxDaysRange > maxDays)
+            {
+                maxDays = maxDaysRange;
+            }
+        }
+
+        return maxDays;
+    }
+    public bool IsMatch(List<AnswerHistory> listOfAnswers, int days)
+    {
+        return false;
+    }
+}
+
+public class MaxStreakOf3 : MaxStreak, IAnswerPattern
+{
+    
+    public string Name { get { return "Max-Streak-Of-3"; } }
+
+    public bool IsMatch(List<AnswerHistory> listOfAnswers)
+    {
+        return MaxStreakDays(listOfAnswers) == 3;
     }
 }
