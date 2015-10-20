92b9da021298fc5d3fab0ddf0a4f691ebafd360e
Robert Mischke <robertmischke@gmail.com>
2015-10-20 22:34:00 +0200

    fix: QuestionSet_should_be_persisted

--- src/TrueOrFalse/Domain/QuestionSet/Set.Persistence/SetMap.cs
+++ src/TrueOrFalse/Domain/QuestionSet/Set.Persistence/SetMap.cs
@@ -12,7 +12,7 @@ public class SetMap : ClassMap<Set>
         Map(x => x.TotalRelevancePersonalAvg);
         Map(x => x.TotalRelevancePersonalEntries);
 
-        HasMany(x => x.QuestionsInSet).Table("questionInSet").Cascade.Delete().OrderBy("Sort");
+        HasMany(x => x.QuestionsInSet).Table("questionInSet").Cascade.All().OrderBy("Sort");
         HasManyToMany(x => x.Categories).Table("categories_to_sets").Cascade.SaveUpdate();
                 
         References(x => x.Creator);
