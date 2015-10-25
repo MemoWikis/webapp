0fb89a4932dbf1080c7f9a39214e3045e7abee39
Robert Mischke <robertmischke@gmail.com>
2015-10-25 11:01:16 +0100

    added enum ForgettingCurveInterval

--- src/TrueOrFalse/Domain/Knowledge/Learning/LearningAnalyzer/ForgettingCurveLoader.cs
+++ src/TrueOrFalse/Domain/Knowledge/Learning/LearningAnalyzer/ForgettingCurveLoader.cs
@@ -4,10 +4,29 @@ using System.Linq;
 
 public class ForgettingCurveLoader
 {
-    public static ForgettingCurve Get(List<AnswerHistory> answerHistories, TimeSpan intervalLength)
+    public static ForgettingCurve Get(List<AnswerHistory> answerHistories, ForgettingCurveInterval interval)
     {
         var answerHistoryPairs = AnswerPairFromHistoryRows.Get(answerHistories);
         var listOfExaminedAnswerObjects = answerHistoryPairs.OrderBy(x => x.TimePassedInSeconds).ToList();
-        return Intervalizer.Run(listOfExaminedAnswerObjects, intervalLength);
+        return Intervalizer.Run(listOfExaminedAnswerObjects, interval.ToTimespan());
+    }
+}
+
+public enum ForgettingCurveInterval{Minutes, Hours, Days, Week, Month, Logarithmic}
+
+public static class ForgettingCurveIntervalExt
+{
+    public static TimeSpan ToTimespan(this ForgettingCurveInterval interval)
+    {
+        switch (interval)
+        {
+            case ForgettingCurveInterval.Minutes: return new TimeSpan(0, 1, 0);
+            case ForgettingCurveInterval.Hours: return new TimeSpan(1, 0, 0);
+            case ForgettingCurveInterval.Days: return new TimeSpan(1, 0, 0, 0);
+            case ForgettingCurveInterval.Week: return new TimeSpan(7, 0, 0, 0);
+            case ForgettingCurveInterval.Month: return new TimeSpan(30, 0, 0);
+        }
+
+        throw new Exception("not implemented interval");
     }
 }
