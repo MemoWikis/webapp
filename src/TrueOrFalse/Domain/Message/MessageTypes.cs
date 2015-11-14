de421fd1cf8f012066f42236845e9bee8ef9cf76
Robert Mischke <robertmischke@gmail.com>
2015-11-14 19:13:32 +0100

    fixed comment receivers #52680569414354

---
+++ src/TrueOrFalse/Domain/Message/MessageTypes.cs
@@ -0,0 +1,8 @@
+﻿public class MessageTypes
+{
+    public const string Welcome = "Welcome";
+    public const string Comment = "Comment";
+    public const string CommentAnswer = "CommentAnswer";
+
+    public const string Custom = "Custom";
+}
--- src/TrueOrFalse/Infrastructure/Persistence/Constants.cs
+++ src/TrueOrFalse/Infrastructure/Persistence/Constants.cs
@@ -1,4 +1,5 @@
 ﻿public static class Constants
 {
     public const int VarCharMaxLength = 8000;
+    public const int MemuchoAdminUserId = 26;
 }
