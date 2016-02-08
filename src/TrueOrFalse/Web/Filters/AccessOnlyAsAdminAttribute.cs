33d92ac3895db51b066b745d2bd18708e82951c1
Robert Mischke <robertmischke@gmail.com>
2016-02-08 22:13:13 +0100

    added attribute suffix to custom attributes

---
+++ src/TrueOrFalse/Web/Filters/AccessOnlyAsAdminAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+public class AccessOnlyAsAdminAttribute : ActionFilterAttribute
+{
+    public override void OnActionExecuting(ActionExecutingContext filterContext)
+    {
+        var userSession = new SessionUser();
+        if (!userSession.IsInstallationAdmin)
+            throw new InvalidAccessException();
+
+        base.OnActionExecuting(filterContext);
+    }
+}
