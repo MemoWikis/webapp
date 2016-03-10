77cdacf8a732b22d13d7e0fd631c3e8390e72f97
Robert Mischke <robertmischke@gmail.com>
2016-03-10 23:10:06 +0100

    games: always add memucho user

--- src/TrueOrFalse.Frontend.Web/Code/BaseController.cs
+++ src/TrueOrFalse.Frontend.Web/Code/BaseController.cs
@@ -3,15 +3,15 @@
 [AccessBeta]
 public class BaseController : Controller
 {
-    protected SessionUser _sessionUser{ get { return Resolve<SessionUser>(); } }
-    protected SessionUiData _sessionUiData { get { return Resolve<SessionUiData>(); } }
-    public int UserId { get { return _sessionUser.UserId; } }
+    protected SessionUser _sessionUser => Resolve<SessionUser>();
+    protected SessionUiData _sessionUiData => Resolve<SessionUiData>();
+    public int UserId => _sessionUser.UserId;
 
     /// <summary>The user fresh from the db</summary>
-    public User UserFresh()
-    {
-        return R<UserRepo>().GetById(UserId);
-    }
+    public User UserFresh(){ return R<UserRepo>().GetById(UserId);}
+    public User MemuchoUser(){ return R<UserRepo>().GetById(Settings.MemuchoUserId); }
+
+    public bool IsMemuchoUser => _sessionUser.IsLoggedIn && Settings.MemuchoUserId == UserId;
 
     protected T Resolve<T>()
     {
