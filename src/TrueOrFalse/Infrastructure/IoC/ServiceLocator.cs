ef2046caeb32a1c5e4b130ade478c41cb330da31
Jule <jule@true-or-false.com>
2016-04-25 22:08:38 +0200

    ServiceLocator fix

--- src/TrueOrFalse/Infrastructure/IoC/ServiceLocator.cs
+++ src/TrueOrFalse/Infrastructure/IoC/ServiceLocator.cs
@@ -3,6 +3,8 @@ using System.Web.Mvc;
 using Autofac;
 using Autofac.Integration.Mvc;
 using NHibernate;
+using System.Collections.Generic;
+using System.Threading;
 
 public class Sl
 {
@@ -24,14 +26,41 @@ public static class SlExt
 public class ServiceLocator
 {
     private static IContainer _container;
+    private static readonly Dictionary<int /*managed thread id*/, ILifetimeScope> _liftimeScopes = new Dictionary<int, ILifetimeScope>();
 
     public static void Init(IContainer container)
     {
         _container = container;
     }
 
+    public static void AddScopeForCurrentThread(ILifetimeScope lifetimeScope)
+    {
+        _liftimeScopes.Add(Thread.CurrentThread.ManagedThreadId, lifetimeScope);
+        Logg.r().Information("AddScopeForCurrentThread {ManagedThreadId} {LifetimeScopeTag}", Thread.CurrentThread.ManagedThreadId, lifetimeScope.Tag);
+    }
+
+    public static void RemoveScopeForCurrentThread()
+    {
+        var tag = _liftimeScopes[Thread.CurrentThread.ManagedThreadId].Tag;
+        _liftimeScopes.Remove(Thread.CurrentThread.ManagedThreadId);
+        var keys = string.Join(",", _liftimeScopes.Keys);
+        Logg.r().Information("RemoveScopeForCurrentThread {ManagedThreadId} {LifetimeScopeTag}, Active threads: " + keys, Thread.CurrentThread.ManagedThreadId, tag);
+    }
+
     public static T Resolve<T>()
     {
+        var currentThreadId = Thread.CurrentThread.ManagedThreadId;
+        if (_liftimeScopes.ContainsKey(currentThreadId))
+        {
+            var scope = _liftimeScopes[currentThreadId];
+
+            Logg.r().Information("ResolveFromScope {ManagedThreadId} {LifetimeScopeTag}", Thread.CurrentThread.ManagedThreadId, scope.Tag);
+            
+            //2: what happens on exception?
+
+            return scope.Resolve<T>();
+        }
+
         if (HttpContext.Current == null)
             return _container.Resolve<T>();
 
