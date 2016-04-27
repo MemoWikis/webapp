148980306c526db1ea248ae37fbe3966bf57ac8a
Jule <jule@true-or-false.com>
2016-04-27 22:14:33 +0200

    TrainingPlanUpdateCheck (wip)

--- src/TrueOrFalse/Tools/ScheduledJobs/Jobs/TrainingPlanUpdateCheck.cs
+++ src/TrueOrFalse/Tools/ScheduledJobs/Jobs/TrainingPlanUpdateCheck.cs
@@ -1,4 +1,5 @@
-﻿using NHibernate;
+﻿using System;
+using NHibernate;
 using Quartz;
 
 namespace TrueOrFalse.Utilities.ScheduledJobs
@@ -6,26 +7,47 @@ namespace TrueOrFalse.Utilities.ScheduledJobs
     public class TrainingPlanUpdateCheck : IJob
     {
         public const int IntervalInMinutes = 30;
+        public static bool IsRunning = false;
+        public string JobHashCode;
 
         public void Execute(IJobExecutionContext context)
         {
+            JobHashCode = DateTime.Now.ToString().GetHashCode().ToString("x");
+
             JobExecute.Run(scope =>
             {
-                Logg.r().Information("Training Plan Update Check started.");
-                
+                if (IsRunning)
+                {
+                    Logg.r().Information("TrainingPlanUpdateCheck not executed because already running");
+                    return;
+                }
+
+                IsRunning = true;
+
                 var trainingPlans = scope.R<TrainingPlanRepo>().AllWithNewMissedDates();
 
-                foreach (var trainingPlan in trainingPlans)
+                try
                 {
-                    Logg.r().Information("Updated training plan Id=" + trainingPlan.Id + " because of missed date.");
+                    foreach (var trainingPlan in trainingPlans)
+                    {
+                        var session = scope.R<ISession>();
 
-                    TrainingPlanUpdater.Run(trainingPlan);
+                        Logg.r().Information("Updating training plan Id=" + trainingPlan.Id + " because of missed date.");
 
-                    var session = scope.R<ISession>();
-                    session.Update(trainingPlan);
-                    session.Flush();
+                        TrainingPlanUpdater.Run(trainingPlan);
+
+                        session.Update(trainingPlan);
+                        session.Flush();
+                    }
                 }
-            }, "TrainingPlanUpdateCheck");
+                catch (Exception e)
+                {
+                    Logg.r().Information(e, "");
+                }
+
+                IsRunning = false;
+
+            }, "TrainingPlanUpdateCheck " + JobHashCode);
         }
     }
 }
