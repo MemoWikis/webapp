0f482be778f14eead86c75b381b258fb4df1e343
c.mauersberger@googlemail.com <c.mauersberger@googlemail.com>
2016-05-09 13:32:15 +0200

    fix: don't allow dates with 0 questions (was accepted if set was added, even if set had 0 questions) #130904497666938

--- src/TrueOrFalse/Web/AutocompleteUtils.cs
+++ src/TrueOrFalse/Web/AutocompleteUtils.cs
@@ -10,10 +10,10 @@ public class AutocompleteUtils
         var _categoryRepo = ServiceLocator.Resolve<CategoryRepository>();
         return
             (from key in postData.AllKeys
-             where key.StartsWith("cat-")
-             select _categoryRepo.GetById(Convert.ToInt32(postData[key])))
-             .Where(category => category != null)
-             .ToList();
+                where key.StartsWith("cat-")
+                select _categoryRepo.GetById(Convert.ToInt32(postData[key])))
+                .Where(category => category != null)
+                .ToList();
     }
 
     public static IList<Set> GetSetsFromPostData(NameValueCollection postData)
@@ -21,9 +21,20 @@ public class AutocompleteUtils
         var _setRepo = ServiceLocator.Resolve<SetRepo>();
         return
             (from key in postData.AllKeys
-             where key.StartsWith("set-")
-             select _setRepo.GetById(Convert.ToInt32(postData[key])))
-             .Where(category => category != null)
-             .ToList();
+                where key.StartsWith("set-")
+                select _setRepo.GetById(Convert.ToInt32(postData[key])))
+                .Where(category => category != null)
+                .ToList();
+    }
+
+    public static int GetQuestionsCountFromPostData(NameValueCollection postData)
+    {
+        var sets = GetSetsFromPostData(postData);
+        var questionCount = 0;
+        foreach (var set in sets)
+        {
+            questionCount += set.Questions().Count;
+        }
+        return questionCount;
     }
 }
