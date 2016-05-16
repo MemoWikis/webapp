4cb766eb5f33e368223b3fc75c79e8c1adc4aef6
Jule <jule@true-or-false.com>
2016-05-16 13:32:24 +0200

    KnowledgeStatus (refactor)

---
+++ src/TrueOrFalse/Domain/Knowledge/GetKnowledgeStatus.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class GetKnowledgeStatus
+{
+    public static KnowledgeStatus Run(IList<Answer> answers)
+    {
+        // Definition KnowledgeStatus:
+        //   NotLearned = no answer existing
+        //   NeedsLearning = Last answer wrong OR among last five answers: wrong >= correct
+        //   NeedsConsolidation = residual category
+        //   Solid = Last three answers correct AND c<4b
+        //   Solid (old) = Last three answers correct AND ((no wrong answer existing AND c<4b) OR (b > c-a))
+        //      where:  a = Distance between last incorrect answer and beginning of positive streak
+        //              b = Length of last positive streak
+        //              c = Distance from end of last posivite streak to now
+        //      example (o=correct; x=wrong answer; |=now): ... x   o  o o   o        o     | 
+        //                                                       -a- ---------b------- --c--
+
+        if (answers.Count == 0)
+        {
+            return KnowledgeStatus.NotLearned;
+        }
+        if (!answers.Last().AnsweredCorrectly())
+        {
+            return KnowledgeStatus.NeedsLearning;
+        }
+
+        var lastFiveAnswers = answers.OrderByDescending(a => a.DateCreated).Take(5).ToList();
+        if (lastFiveAnswers.Count(a => !a.AnsweredCorrectly()) >=
+            lastFiveAnswers.Count(a => a.AnsweredCorrectly()))
+        {
+            return KnowledgeStatus.NeedsLearning;
+        }
+
+        var lastStreak = answers.OrderByDescending(a => a.DateCreated).TakeWhile(a => a.AnsweredCorrectly()).ToList();
+        //var tmp_lengthStreak = lastStreak.First().DateCreated - lastStreak.Last().DateCreated;
+        //var tmp_DistanceSinceLastStreak = DateTime.Now - lastStreak.First().DateCreated;
+        //var tmp_4Times = TimeSpan.FromTicks(tmp_DistanceSinceLastStreak.Ticks*4);
+
+        if ((lastStreak.Count >= 3) &&
+            (DateTime.Now - lastStreak.First().DateCreated) <
+            TimeSpan.FromTicks((lastStreak.First().DateCreated - lastStreak.Last().DateCreated).Ticks * 4))
+        {
+            return KnowledgeStatus.Solid;
+        }
+
+        return KnowledgeStatus.NeedsConsolidation;
+    }
+}
