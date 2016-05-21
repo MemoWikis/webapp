a92c76e4858e34db6dc2a01967dd8e350bbaa430
Robert Mischke <robertmischke@gmail.com>
2016-05-21 22:27:53 +0200

    added Newtonsoft.Json, minor renames

--- src/TrueOrFalse.Tests/2 Domain/Knowledge/Learning/TrainingPlan/Should_create_trainingsplan.cs
+++ src/TrueOrFalse.Tests/2 Domain/Knowledge/Learning/TrainingPlan/Should_create_trainingsplan.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using NUnit.Framework;
-using TrueOrFalse.Utilities.ScheduledJobs;
-using EnumerableExtensions = NHibernate.Util.EnumerableExtensions;
 
 public class Should_create_trainingsplan : BaseTest
 {
