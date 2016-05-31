72236b5784b85dd1ce826eef2c22abfb69853164
c.mauersberger@googlemail.com <c.mauersberger@googlemail.com>
2016-05-31 22:49:51 +0200

    dynamic spacingBetweenSessionsInMinutes using arctan-function #138488295512667

--- src/TrueOrFalse/Domain/Knowledge/Learning/TrainingPlan/Configuration/TrainingPlanSettings.cs
+++ src/TrueOrFalse/Domain/Knowledge/Learning/TrainingPlan/Configuration/TrainingPlanSettings.cs
@@ -7,7 +7,12 @@ public class TrainingPlanSettings
     public virtual int QuestionsPerDate_Minimum { get; set; } = 7;
     public virtual int QuestionsPerDate_IdealAmount { get; set; } = 10;
 
-    public virtual int SpacingBetweenSessionsInMinutes { get; set; } = 60 * 3;
+    public virtual int SpacingBetweenSessionsInMinutes { get; set; } = 60 * 3; //will be obsolete, to delete in mapping as well
+
+    // aprox.: after EqualizedSpacingBetweenSessionsDividerDays days, MinSpacingBetweenSessionsInMinutes is multiplied by EqualizedSpacingBetweenSessionsFactorMax
+    public virtual int MinSpacingBetweenSessionsInMinutes { get; set; } = 60 * 3;
+    public virtual int EqualizedSpacingBetweenSessionsFactorMax { get; set; } = 60; // higher values lead to higher spacing
+    public virtual int EqualizedSpacingBetweenSessionsDividerDays { get; set; } = 180; // higher values lead spacing effect to be effective when distance till date is greater
 
     /// <summary>
     /// When answer probability drops below this threshold, 
@@ -31,6 +36,30 @@ public class TrainingPlanSettings
     {
         return SnoozePeriods.Any(p => p.IsInPeriod(dateTime));
     }
+
+    public virtual int GetEqualizedMinSpacingInMinutes(int distanceTillDateInDays, int questionCount = 100)
+    {
+        //todo: does not account for number of questions yet
+
+        //double distanceTillDateInDays = (Date.DateTime - proposedDateTime).Days; //needs to be double to not lose fraction in division below
+
+        /* spacing is done via an arctan-function: f * arctan(x/d).
+        ** x is number of days untill date (=exam)
+        ** f distorts curve up, e.g.: higher f lead to higher spacing
+        ** d distorts curve to the right, e.g.: higher values lead spacing effect to be effective when distance till date is greater
+        ** min-value should be 1, e.g., spacing should be at least as great as MinSpacing in Settings
+        */
+        if (distanceTillDateInDays > 0)
+            return
+                Convert.ToInt32(
+                    Math.Max(1,
+                        EqualizedSpacingBetweenSessionsFactorMax *
+                        Math.Atan((double)distanceTillDateInDays / EqualizedSpacingBetweenSessionsDividerDays)) *
+                    MinSpacingBetweenSessionsInMinutes);
+        else
+            return MinSpacingBetweenSessionsInMinutes;
+    }
+
 }
 
 public enum LearningPlanStrategy
