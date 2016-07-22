7e9ed8d1ee413fa68ed450983d3eb0ba27b6d2e9
Jule <jule@true-or-false.com>
2016-07-22 15:32:15 +0200

    Revert "Simulation: Leitner only"
    This reverts commit f8f906f726b9446d3789e45fe7a4e66a97d53f42.

--- src/TrueOrFalse.Tests/_ leitner simulation/LeitnerSimulationTest.cs
+++ src/TrueOrFalse.Tests/_ leitner simulation/LeitnerSimulationTest.cs
@@ -9,14 +9,13 @@ public class LeitnerSimulationTest : BaseTest
     [Test]
     public void ExecuteSimulation()
     {
-        //for(var i = 1; i <= 5; i++) { 
-        //    ExecuteSimulation(7, 10 * (i * 2));
-        //}
+        for(var i = 1; i <= 5; i++) { 
+            ExecuteSimulation(7, 10 * (i * 2));
+        }
 
-        //for (var i = 1; i <= 7; i++){
-        //    ExecuteSimulation(4 * i, 100);
-        //}
-        ExecuteSimulation(16, 200);
+        for (var i = 1; i <= 7; i++){
+            ExecuteSimulation(4 * i, 100);
+        }
     }
 
     private static void ExecuteSimulation(int numberOfDays, int numberOfQuestions)
@@ -24,20 +23,20 @@ public class LeitnerSimulationTest : BaseTest
         var leitnerSimulation = new LeitnerSimulation();
         leitnerSimulation.Start(numberOfDays, numberOfQuestions);
 
-        //var memuchoTrainingPlan = Get_comparison_with_training_plan_creator(numberOfQuestions, numberOfDays);
+        var memuchoTrainingPlan = Get_comparison_with_training_plan_creator(numberOfQuestions, numberOfDays);
 
         var sb = new StringBuilder();
 
         sb.AppendLine($"Amount questions: {numberOfQuestions}");
         sb.AppendLine($"Amount days: {numberOfDays}");
         sb.AppendLine("");
-        sb.AppendLine($"Total Leitner repetitions: {leitnerSimulation.Days.GetSumOfRepetitions()}");
+        sb.AppendLine($"Total Leitner repititions: {leitnerSimulation.Days.GetSumOfRepetitions()}");
         sb.AppendLine("");
 
-        //TrainingPlan.AvgRepetitionsPerTraingDate = 2;
-        //sb.AppendLine("Total memucho repetitions: " + memuchoTrainingPlan.GetSumOfRepetitions() + " (avg. 2 repetitions per training session)");
-        //TrainingPlan.AvgRepetitionsPerTraingDate = 3;
-        //sb.AppendLine("Total memucho repetitions: " + memuchoTrainingPlan.GetSumOfRepetitions() + " (avg. 3 repetitions per training session)");
+        TrainingPlan.AvgRepetitionsPerTraingDate = 2;
+        sb.AppendLine("Total memucho repetitions: " + memuchoTrainingPlan.GetSumOfRepetitions() + " (avg. 2 repetitions per training session)");
+        TrainingPlan.AvgRepetitionsPerTraingDate = 3;
+        sb.AppendLine("Total memucho repetitions: " + memuchoTrainingPlan.GetSumOfRepetitions() + " (avg. 3 repetitions per training session)");
 
 
         sb.AppendLine("----------------------------------------------------------");
@@ -51,21 +50,21 @@ public class LeitnerSimulationTest : BaseTest
             {
                 sb.AppendLine(
                     $"Box{(box.ToRepeat ? "*" : "")}{box.Number}: " +
-                    $"(before:) {box.Questions.Count} / (after:) {day.BoxesAfter.ByNumber(box.Number).Questions.Count}"); 
+                    $"(before:) {box.Questions.Count} / (after:) {day.BoxesAfter.ByNumber(box.Number).Questions.Count}");
             }
-            sb.AppendLine($"Sum of repetitions: {day.GetSumOfRepetitions()}");
+            sb.AppendLine($"Sum of repititions: {day.GetSumOfRepetitions()}");
         }
 
-        //sb.AppendLine("----------------------------------------------------------");
-        //sb.AppendLine("Memucho Details:");
-        //sb.AppendLine("");
-        //TrainingPlan.AvgRepetitionsPerTraingDate = 2;
-        //sb.AppendLine($"(avg. {TrainingPlan.AvgRepetitionsPerTraingDate} repetitions per training session)");
-        //sb.AppendLine("");
+        sb.AppendLine("----------------------------------------------------------");
+        sb.AppendLine("Memucho Details:");
+        sb.AppendLine("");
+        TrainingPlan.AvgRepetitionsPerTraingDate = 2;
+        sb.AppendLine($"(avg. {TrainingPlan.AvgRepetitionsPerTraingDate} repetitions per training session)");
+        sb.AppendLine("");
 
-        //foreach (var trainingDate in memuchoTrainingPlan.Dates)
-        //    sb.AppendLine($"Day: {(trainingDate.DateTime - DateTime.Now).Days} " +
-        //                  $"{trainingDate.DateTime} repetitions: {trainingDate.AllQuestionsInTraining.Count * TrainingPlan.AvgRepetitionsPerTraingDate} ");
+        foreach (var trainingDate in memuchoTrainingPlan.Dates)
+            sb.AppendLine($"Day: {(trainingDate.DateTime - DateTime.Now).Days} " +
+                          $"{trainingDate.DateTime} repetitions: {trainingDate.AllQuestionsInTraining.Count * TrainingPlan.AvgRepetitionsPerTraingDate} ");
         
         var fileName = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, $"leitner-days-{numberOfDays}-questions-{numberOfQuestions}.txt");
         File.WriteAllText(fileName, sb.ToString());
