13cf1aaf24a1052e8631e9c2541155bfadc3a595
Jule <jule@true-or-false.com>
2016-08-16 14:32:35 +0200

    LearningSessionResult: correct numbers

--- src/TrueOrFalse.Frontend.Web/Views/Questions/Answer/LearningSession/LearningSessionResultModel.cs
+++ src/TrueOrFalse.Frontend.Web/Views/Questions/Answer/LearningSession/LearningSessionResultModel.cs
@@ -6,7 +6,8 @@ using System.Web;
 public class LearningSessionResultModel : BaseModel
 {
     public LearningSession LearningSession;
-    public int TotalNumberSteps;
+    public int NumberSteps;
+    public int NumberQuestions;
     public int NumberCorrectAnswers;
     public int NumberWrongAnswers;
     public int NumberNotAnswered;
@@ -18,18 +19,19 @@ public class LearningSessionResultModel : BaseModel
     public LearningSessionResultModel(LearningSession learningSession)
     {
         LearningSession = learningSession;
-        TotalNumberSteps = LearningSession.Steps.Count();
+        NumberSteps = LearningSession.Steps.Count();
+        NumberQuestions = LearningSession.Questions().Count;
 
-        if (TotalNumberSteps > 0)
+        if (NumberSteps > 0)
         {
             var answeredSteps = LearningSession.Steps.Where(s => s.AnswerState == StepAnswerState.Answered).ToList();
             NumberCorrectAnswers = answeredSteps.Count(s => s.Answer.AnswerredCorrectly != AnswerCorrectness.False);
             NumberWrongAnswers = answeredSteps.Count(s => s.Answer.AnswerredCorrectly == AnswerCorrectness.False);
             NumberNotAnswered = LearningSession.Steps.Count(s => s.AnswerState == StepAnswerState.Skipped || s.AnswerState == StepAnswerState.NotViewedOrAborted);
 
-            NumberCorrectPercentage = (int)Math.Round(NumberCorrectAnswers / (float)TotalNumberSteps * 100);
-            NumberWrongAnswersPercentage = (int)Math.Round(NumberWrongAnswers / (float)TotalNumberSteps * 100);
-            NumberNotAnsweredPercentage = (int)Math.Round(NumberNotAnswered / (float)TotalNumberSteps * 100);
+            NumberCorrectPercentage = (int)Math.Round(NumberCorrectAnswers / (float)NumberSteps * 100);
+            NumberWrongAnswersPercentage = (int)Math.Round(NumberWrongAnswers / (float)NumberSteps * 100);
+            NumberNotAnsweredPercentage = (int)Math.Round(NumberNotAnswered / (float)NumberSteps * 100);
         }
     }
 }
