2893183e20edccc1ef19fb39d4a4ac90c775acaf
c.mauersberger@googlemail.com <c.mauersberger@googlemail.com>
2016-10-02 21:49:18 +0200

    fix for firefox vs. Chrome: fix layout bug for labels that are too long #176277435109670

--- src/TrueOrFalse.Frontend.Web/Scripts/Site.ts
+++ src/TrueOrFalse.Frontend.Web/Scripts/Site.ts
@@ -25,10 +25,12 @@ function InitLabelTooltips() {
     });
     $('.label-set').each(function () {
         $(this).addClass('show-tooltip');
-        if ($(this)[0].scrollWidth > $(this).innerWidth())
+        //if ($(this)[0].scrollWidth > $(this).innerWidth()) //this is simpler and more to the point, but in cases when content is just truncated does not work; reason: scrollWidth gives different values in FF and Chrome
+        if ($(this).innerWidth() == (parseInt($(this).css('max-width')) - 2*(parseInt($(this).css('border-left-width')))))
             $(this).attr('title', 'Zum Fragesatz "' + $(this).html()+'"').attr('data-placement', 'top');
         else 
             $(this).attr('title', 'Zum Fragesatz').attr('data-placement', 'top');
+        //console.log("clientWidth: " + $(this)[0].clientWidth + " -scrollWidth: " + $(this)[0].scrollWidth + " -offsetWidth: " + $(this)[0].offsetWidth + " -innerWidth: " + $(this).innerWidth() + " -maxWidth:" + $(this).css('maxWidth'));
     });
 }
 
