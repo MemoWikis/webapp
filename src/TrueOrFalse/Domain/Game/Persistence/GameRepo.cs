4eb4daa3ccb118873006d639606fea80fc2cd11d
Robert Mischke <robertmischke@gmail.com>
2016-10-13 17:21:24 +0200

    memucho average player #195198485688958

--- src/TrueOrFalse/Domain/Game/Persistence/GameRepo.cs
+++ src/TrueOrFalse/Domain/Game/Persistence/GameRepo.cs
@@ -46,13 +46,8 @@ public class GameRepo : RepositoryDbBase<Game>
 
     public IList<Game> GetRunningGames()
     {
-        Round roundsAlias = null;
-        Answer answerAlias = null;
-
         return _session
             .QueryOver<Game>()
-            .JoinAlias(g => g.Rounds, () => roundsAlias)
-            .JoinAlias(() => roundsAlias.Answers, () => answerAlias)
             .Where(g => g.Status == GameStatus.InProgress)
             .List<Game>();        
     }
