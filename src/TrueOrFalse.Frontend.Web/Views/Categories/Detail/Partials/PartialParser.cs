bf4109635c32672a9f4701b2b7bdf0497fa1eb9e
Jule <jule@true-or-false.com>
2016-12-22 12:16:51 +0100

    Category content editor #232673717766961

--- src/TrueOrFalse.Frontend.Web/Views/Categories/Detail/Partials/PartialParser.cs
+++ src/TrueOrFalse.Frontend.Web/Views/Categories/Detail/Partials/PartialParser.cs
@@ -4,41 +4,48 @@ using System.Linq;
 using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Mvc;
+using Newtonsoft.Json;
 
 public class PartialParser
 {
-    public static string Run(string stringToParse)
+    public static string Run(string stringToParse, ControllerContext controllerContext)
     {
         var regex = new Regex(@"\[\[(.*?)\]\]", RegexOptions.Singleline);//Matches "[[something]]" non-greedily, across multiple lines and only if not nested
 
         return regex.Replace(stringToParse, match =>
         {
-            var partialString = GetPartial(match.Value.Substring(2, match.Value.Length - 4));
+            var partialString = GetPartial(
+                                    match.Value
+                                        .Substring(2, match.Value.Length - 4)
+                                        .Replace("&quot;", @""""),
+                                    controllerContext);
 
             return string.IsNullOrEmpty(partialString) ? match.Value : partialString;
         });
     }
 
-    public static string Run(string stringToParse, ControllerContext controllerContext)
+    private static string GetPartial(string partialTemplate, ControllerContext controllerContext)
     {
-        return Run(stringToParse);
+        PartialJson partialJson;
+
+        try
+        {
+            partialJson = JsonConvert.DeserializeObject<PartialJson>(partialTemplate);
+        }
+
+        catch
+        {
+           return "";
+        }
+
+        return GetSetCollectionPartial(partialJson, controllerContext);
     }
 
-    public static MatchCollection ExtractPartialTemplates(string text)
+    public static string GetSetCollectionPartial(PartialJson partialJson, ControllerContext controllerContext)
     {
-        var regex = new Regex(@"\[\[(.*?)\]\]");
+        if (partialJson.PartialName != "SetCollection") return "";
 
-        return regex.Matches(text);
-    }
-
-    private static string GetPartial(string partialTemplate)
-    {
-        return partialTemplate.ToUpper();
-    }
-
-    public static string ReturnSetCollectionPartial(string stringToParse, ControllerContext controllerContext)
-    {
-        var set = Sl.R<SetRepo>().GetById(9);
+        var set = Sl.R<SetRepo>().GetById(partialJson.CategoryId);
 
         return ViewRenderer.RenderPartialView(
             "~/Views/Categories/Detail/Partials/SetCollection.ascx",
