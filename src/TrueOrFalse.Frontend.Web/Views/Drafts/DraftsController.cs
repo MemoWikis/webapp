4489a5f9933d309023adfe11a6d3ba453f8091b7
Jule <jule@true-or-false.com>
2017-01-20 11:42:11 +0100

    Add category example/tutorial page

--- src/TrueOrFalse.Frontend.Web/Views/Drafts/DraftsController.cs
+++ src/TrueOrFalse.Frontend.Web/Views/Drafts/DraftsController.cs
@@ -67,4 +67,50 @@ public class DraftsController : BaseController
     {
         return View(new WelcomeModel());
     }
+
+    [AccessOnlyAsAdmin]
+    public ActionResult CategoryPartials()
+    {
+       
+        var category = Sl.R<CategoryRepository>().GetById(145);
+        category.TopicMarkdown =
+@"[[{ ""TemplateName"":""DivStart"", ""CssClasses"":""Box""}]]
+#### Box-Inhalt
+[[{""TemplateName"":""DivEnd""}]]
+[[{""TemplateName"":""DivStart"", ""CssClasses"":""row CardsLandscape""}]]
+[[{""TemplateName"":""SetListCard"",
+""Title"":""Einbürgerungstest für Thüringen"",
+""Description"":""Lerne die allgemeinen Fragen und die landesspezifischen Fragen für Thüringen"",
+""SetListIds"":""27,9,48""}]]
+[[{""TemplateName"":""SetListCard"",
+""Title"":""Einbürgerungstest für Berlin"",
+""TitleRowCount"": ""2"",
+""Description"":""TestDescription"",
+""DescriptionRowCount"":0,
+""SetListIds"":""27,49"",
+""RowCount"":3}]]
+[[{""TemplateName"":""DivEnd""}]]
+[[{""TemplateName"":""CategoryNetwork""}]]
+[[{""TemplateName"":""ContentLists""}]]
+
+#### Die besten Fragesätze dieser Kategorie
+[[{""TemplateName"":""DivStart"", ""CssClasses"":""row CardsPortrait""}]]
+[[{""TemplateName"": ""SingleSet"",
+""SetId"": 3,
+""SetText"": ""test set text""}]]
+[[{""TemplateName"": ""SingleSet"",
+""SetId"": 7}]]
+[[{""TemplateName"": ""SingleSet"",
+""SetId"": 9}]]
+[[{""TemplateName"":""DivEnd""}]]";
+
+        var contentHtml = string.IsNullOrEmpty(category.TopicMarkdown)
+           ? null
+           : MarkdownToHtml.Run(category, ControllerContext);
+
+        return View(new CategoryModel(category)
+        {
+            CustomPageHtml = contentHtml
+        });
+    }
 }
