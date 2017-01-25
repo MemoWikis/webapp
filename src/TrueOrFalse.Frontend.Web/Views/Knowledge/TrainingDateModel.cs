a3027fd4a92e6cc2a3dc56cfe5b6b7cfb0ad20df
c.mauersberger@googlemail.com <c.mauersberger@googlemail.com>
2017-01-25 21:24:23 +0100

    fix bug that different training times for training sessions are displayed

--- src/TrueOrFalse.Frontend.Web/Views/Knowledge/TrainingDateModel.cs
+++ src/TrueOrFalse.Frontend.Web/Views/Knowledge/TrainingDateModel.cs
@@ -9,8 +9,8 @@ public class TrainingDateModel
     public Date Date;
     public TrainingDate TrainingDate;
 
-    public int LearningTimeInMin => (int) Math.Ceiling(30/*seconds per question*/ * QuestionCount / 60d);
-    //public int LearningTimeInMin => TrainingDate.TimeEstimated().Minutes;
+    //public int LearningTimeInMin => (int) Math.Ceiling(30/*seconds per question*/ * QuestionCount / 60d);
+    public int LearningTimeInMin => TrainingDate.TimeEstimated().Minutes;
 
     private TimeSpanLabel _timeSpanLabel;
     public TimeSpanLabel TimeSpanLabel => _timeSpanLabel ?? (_timeSpanLabel = new TimeSpanLabel(DateTime.Now - DateTime));
@@ -20,7 +20,7 @@ public class TrainingDateModel
     public KnowledgeSummary SummaryBefore;
     public KnowledgeSummary SummaryAfter;
 
-    public TrainingDateModel() {}
+    //public TrainingDateModel() {}
     public TrainingDateModel(TrainingDate trainingDate)
     {
         Id = trainingDate.Id;
