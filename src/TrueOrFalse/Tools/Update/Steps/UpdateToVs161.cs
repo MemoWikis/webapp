f74d2c9cbc859e4e3fc06b2c6799b51f62a912e6
Jule <jule@true-or-false.com>
2017-01-26 10:06:00 +0100

    LearningSession for category (wip)

---
+++ src/TrueOrFalse/Tools/Update/Steps/UpdateToVs161.cs
@@ -0,0 +1,16 @@
+﻿using NHibernate;
+
+namespace TrueOrFalse.Updates
+{
+    public class UpdateToVs161
+    {
+        public static void Run()
+        {
+            Sl.Resolve<ISession>()
+              .CreateSQLQuery(
+                @"ALTER TABLE `learningsession`
+	                ADD COLUMN `CategoryToLearn_id` INT(11) NULL DEFAULT NULL AFTER `SetToLearn_Id`;"
+            ).ExecuteUpdate();
+        }
+    }
+}
