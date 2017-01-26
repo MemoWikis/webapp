c1ccdaf8e83b102b7abab76be0d8b8c3b9890751
Robert Mischke <robertmischke@gmail.com>
2017-01-26 17:23:51 +0100

    set videos: get youtube key, added JetBrains.dotMemoryUnit

---
+++ src/TrueOrFalse/Infrastructure/YoutubeVideo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using static System.String;
+
+public class YoutubeVideo
+{
+    public static string GetVideoKeyFromUrl(string url) =>
+        !IsNullOrEmpty(url) && url.Contains("v=")
+            ? url.Split(new[] {"v="}, StringSplitOptions.RemoveEmptyEntries)[1] 
+            : "";
+}
