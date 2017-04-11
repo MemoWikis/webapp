129c13f9209249788902f2358551df240bfa1565
Robert Mischke <robertmischke@gmail.com>
2017-04-11 14:46:11 +0200

    search box wip

---
+++ src/TrueOrFalse.Frontend.Web/Scripts/header/SearchBox.ts
@@ -0,0 +1,60 @@
+﻿class SearchBox
+{
+    _elemInput: JQuery;
+
+    constructor() {
+        console.log("hui search box");
+
+        var self = this;
+        this._elemInput = $("#headerSearchBox");
+        if (this._elemInput.length == 0)
+            return;
+
+        var autocomplete = this._elemInput.autocomplete({
+            minLength: 0,
+            source: function (request, response) {
+                $.get("/Api/Sets/ByName?term=" + request.term, function (data) {
+                    if (data.Items.length == 0)
+                        response([{ NoResults: true }]);
+                    else
+                        response(data.Items);
+                });
+            },
+            select: function (event, ui) {
+
+                self._elemInput.data("set-id", ui.item.Id);
+                self._elemInput.val(ui.item.Name);
+
+                return false;
+            },
+        });
+
+        autocomplete.data("ui-autocomplete")._renderItem = <any>function (ul, item: SetItem): any {
+
+            var html = "";
+
+            if (item.NoResults) {
+                html =
+                    "<div class='SetListItem'>" +
+                    "Keine Treffer. Bitte weitertippen <br> oder anderen Suchbegriff verwenden." +
+                    "</div>";
+            } else {
+                html =
+                    "<div class='SetListItem'>" +
+                    "<img src=" + item.ImageUrl + "/>"
+                    + item.Name +
+                    "</div>";
+            }
+
+            return $("<li></li>")
+                .data("ui-autocomplete-item", item)
+                .append(html)
+                .appendTo(ul);
+        }
+
+    }
+}
+
+$(() => {
+    new SearchBox();
+});
--- src/TrueOrFalse.Frontend.Web/Views/Shared/Site.Partial.MasterHeader.ascx
+++ src/TrueOrFalse.Frontend.Web/Views/Shared/Site.Partial.MasterHeader.ascx
@@ -6,7 +6,7 @@
         <div class="row">
             <div class="col-md-12">
                 <div class="row HeaderMainRow">
-                    <div class="col-xs-6 col-Logo">
+                    <div class="col-xs-7 col-Logo">
                         <a id="MenuButton"><i class="fa fa-bars"></i><span class="caret"></span></a>
                         <a id="LogoLink" href="/">
                             <div id="Pictogram">
@@ -23,8 +23,17 @@
                             <%--<img src="/Images/Logo/beta-logo.png" width="22" height="56">--%>
                             [beta]
                         </a>
+                        <%--<input type="text" id="headerSearchBox" placeholder="Suche" class="form-control"  />--%>
+
+                        <div class="input-group" style="margin-left: 4px; margin-top: 2px; width: 190px;">
+                            <input type="text" class="form-control" placeholder="Suche" id="headerSearchBox">
+                            <div class="input-group-btn">
+                                <button class="btn btn-default" type="submit"><i class="fa fa-search" aria-hidden="true"></i></button>
+                            </div>
+                        </div>
+
                     </div>
-                    <div class="col-xs-6 col-LoginAndHelp">
+                    <div class="col-xs-5 col-LoginAndHelp">
             	        <div id="loginAndHelp" >
                             <% Html.RenderPartial(UserControls.Logon); %>
                         </div>
