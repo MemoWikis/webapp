b578c6bf0e413a89b9d0e8c92bab1128b8ff5572
c.mauersberger@googlemail.com <c.mauersberger@googlemail.com>
2017-04-25 17:34:11 +0200

    fix mailto: add missing script file

---
+++ src/TrueOrFalse.Frontend.Web/Scripts/various/mailto.js
@@ -0,0 +1,12 @@
+﻿
+$(function () {
+    $("span.mailme")
+        .each(function () {
+            var spt = this.innerHTML;
+            var at = / at /;
+            var dot = / dot /g;
+            var addr = spt.replace(at, "@").replace(dot, ".");
+            $(this).after('<a href="mailto:' + addr + '" title="Schreibe eine E-Mail">' + addr + '</a>');
+            $(this).remove();
+        });
+});
