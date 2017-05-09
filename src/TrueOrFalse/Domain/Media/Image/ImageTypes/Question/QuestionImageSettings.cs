f44a3aa54be1142eb6a84ea33767a1ad75a80afe
Robert Mischke <robertmischke@gmail.com>
2017-05-10 01:07:00 +0200

    search box progress: load category image if no question image is available

--- src/TrueOrFalse/Domain/Media/Image/ImageTypes/Question/QuestionImageSettings.cs
+++ src/TrueOrFalse/Domain/Media/Image/ImageTypes/Question/QuestionImageSettings.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
 
 public class QuestionImageSettings : ImageSettings, IImageSettings
 {
@@ -10,7 +12,21 @@ public class QuestionImageSettings : ImageSettings, IImageSettings
     public override string BasePath => "/Images/Questions/";
     public string BaseDummyUrl => "/Images/no-question-";
 
+    private Question __question;
+    private Question _question
+    {
+        get => __question ?? (__question = Sl.QuestionRepo.GetById(Id));
+        set => __question = value;
+    }
+
     public QuestionImageSettings(){}
+
+    public QuestionImageSettings(Question question)
+    {
+        _question = question;
+
+        Init(question.Id);
+    }
 
     public QuestionImageSettings(int questionId){
         Init(questionId);
@@ -20,7 +36,22 @@ public class QuestionImageSettings : ImageSettings, IImageSettings
         Id = questionId; //$temp: wenn id = questionId, was ist dann bei mehreren Bildern
     }
 
-    public ImageUrl GetUrl_50px_square() { return GetUrl(50, isSquare: true); }
+    public ImageUrl GetUrl_50px_square() => 
+        GetUrl_WithCategoriesFailover(50, isSquare: true);
+
+    private ImageUrl GetUrl_WithCategoriesFailover(int width, bool isSquare )
+    {
+        var imageUrl = GetUrl(50, isSquare: true);
+
+        if (imageUrl.HasUploadedImage)
+            return imageUrl;
+
+        if (_question.Categories.Any())
+            return new CategoryImageSettings(_question.Categories.First().Id).GetUrl(width, isSquare);
+
+        return imageUrl;
+    }
+
     public ImageUrl GetUrl_128px_square() { return GetUrl(128, isSquare: true); }
     public ImageUrl GetUrl_128px() { return GetUrl(128); }
     public ImageUrl GetUrl_435px() { return GetUrl(435); }
