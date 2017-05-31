6300e6b3f6f2b260efefb859df41f6a2267b8915
Jule <jule@true-or-false.com>
2017-05-31 09:55:23 +0200

    Category.AggregatedContentJson: LazyLoad

--- src/TrueOrFalse/Domain/Classification/Category/Persistence/CategoryMap.cs
+++ src/TrueOrFalse/Domain/Classification/Category/Persistence/CategoryMap.cs
@@ -20,7 +20,8 @@ public class CategoryMap : ClassMap<Category>
         Map(x => x.TopicMarkdown);
         Map(x => x.FeaturedSetsIdsString);
 
-        Map(x => x.AggregatedContentJson);
+        Map(x => x.AggregatedContentJson).LazyLoad();
+
         Map(x => x.CountQuestionsAggregated);
         Map(x => x.CountSetsAggregated);
 
