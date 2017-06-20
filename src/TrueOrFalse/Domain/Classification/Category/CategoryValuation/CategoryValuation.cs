15529b94d24e127e9cc5dc70fc176c6678138c20
Robert Mischke <robertmischke@gmail.com>
2017-06-21 01:39:30 +0200

    fixed weird nhibernate bug
    (nhibernate expects a mapping, because of the new fields in the CategoryValuation table. I can't reproduce this locally and have no idea why..)

--- src/TrueOrFalse/Domain/Classification/Category/CategoryValuation/CategoryValuation.cs
+++ src/TrueOrFalse/Domain/Classification/Category/CategoryValuation/CategoryValuation.cs
@@ -7,6 +7,11 @@ public class CategoryValuation : DomainEntity
 
     public virtual int RelevancePersonal { get; set; }
 
+    public virtual int CountNotLearned { get; set; }
+    public virtual int CountNeedsLearning { get; set; }
+    public virtual int CountNeedsConsolidation { get; set; }
+    public virtual int CountSolid { get; set; }
+
     public CategoryValuation()
     {
         RelevancePersonal = -1;
