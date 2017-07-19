468af827a8ee720ab6e010fda971c3d2d867f2d7
Julian Theisen <julian@theisen.in>
2017-07-19 11:50:47 +0200

    Set up basic Structure of CategoryKnowledgeBar

---
+++ src/TrueOrFalse.Frontend.Web/Views/Categories/Detail/CategoryKnowledgeBar.ascx
@@ -0,0 +1,4 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" 
+    Inherits="System.Web.Mvc.ViewUserControl<CategoryKnowledgeBarModel>" %>
+
+KNOWLEDGE BAR GOES HERE
---
+++ src/TrueOrFalse.Frontend.Web/Views/Categories/Detail/CategoryKnowledgeBarModel.cs
@@ -0,0 +1,9 @@
+﻿public class CategoryKnowledgeBarModel : BaseModel
+{
+    public Category Category;
+
+   public CategoryKnowledgeBarModel(Category category)
+   {
+       Category = category;
+   }
+}
