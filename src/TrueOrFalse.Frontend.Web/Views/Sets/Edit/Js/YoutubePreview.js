a94e2d5c165bf9c968edeb70c3c0a3e744450148
Daniel Majunke <daniel.majunke@gmx.de>
2017-08-24 13:04:29 +0200

    BugFixes: playerLoad  #394193602509334

--- src/TrueOrFalse.Frontend.Web/Views/Sets/Edit/Js/YoutubePreview.js
+++ src/TrueOrFalse.Frontend.Web/Views/Sets/Edit/Js/YoutubePreview.js
@@ -13,8 +13,6 @@ var optionsYoutube = {
                 'videoId': match[2],
                 'suggestedQuality': 'large'
             });
-
-            console.log('https://www.youtube.com/embed/' + match[2]);
             $('#player').fadeIn();
 
 
@@ -27,17 +25,12 @@ var optionsYoutube = {
     allowSubmit: false,
     captureLength: 0,
     allowSameSearch: true
-}
-
-
-
-
-
+};
 
 
 // 2. This code loads the IFrame Player API code asynchronously.
 var tag = document.createElement('script');
-
+var isLoaded = false;
     tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
@@ -49,14 +42,24 @@ function onYouTubeIframeAPIReady() {
     player = new YT.Player("player", {
         height: "360",
         width: "640",
-        videoId: ""
+        videoId: "",
+        events: {
+            // ist der Youtubeplayer fertig geladen wird isLoadet auf true gesetzt , da wir nicht wissen was zuerst fertig ist Seite oder player
+            'onReady': function () {
+                isLoaded = true;
+                // wird ein Trigger abgesetzt der aufs Document geht 
+                $(document).trigger('playerIsLoaded');
+            }
+        }
         
     });
 }
 
 
 
-   
+   function simulatdedClickOfVideoUrlInput() {
+       $("#VideoUrl").trigger(jQuery.Event("keydown", { keycode: 39 }));
+   }
 
    
    
@@ -68,16 +71,14 @@ $(function () {
         if (player.getCurrentTime() % 60 < 10) {
              temp = Math.floor(player.getCurrentTime() / 60) + ":" + "0" + (player.getCurrentTime() % 60).toFixed();            
         }else{
-            temp = Math.floor(player.getCurrentTime() / 60) + ":" + (player.getCurrentTime() % 60).toFixed(); 
+             temp = Math.floor(player.getCurrentTime() / 60) + ":" + (player.getCurrentTime() % 60).toFixed(); 
         }
         $(this).parent().find(".form-control").val(temp);
         var timeCode = temp;       
         var questionInSetId = $(this).attr("data-in-set-id");
         $.post("/SetVideo/SaveTimeCode/", { timeCode: timeCode, questionInSetId: questionInSetId });
     
-        player.stopVideo();
-
-       
+        player.pauseVideo();   
     });
 
     $("#VideoUrl").typeWatch(optionsYoutube);
@@ -85,5 +86,16 @@ $(function () {
     if ($("#VideoUrl").val() === "") {
         $("#player").hide();
     }
-
+    // der Trigger enthält den Wert der Variablen isLoadet ist der Wert true wird der Trigger auf das Input feld gesetzt und dieses neu geladen
+    // Sinn der Aktion ist es ,das typewatch aktualisiert wird und ein Wert an den Player übergeben wird 
+    if (isLoaded) {
+       // $('#VideoUrl').trigger(jQuery.Event('keydown', { keycode: 39 }));
+        simulatdedClickOfVideoUrlInput();
+    } else {
+        // ist der Status false ,on wird erst ausgeführt wenn playerIsLoadet getriggert wurde
+        $(document).on('playerIsLoaded', function () {
+            simulatdedClickOfVideoUrlInput();
+            //$('#VideoUrl').trigger(jQuery.Event('keydown', { keycode: 39 }));
+        });
+    }
 });
