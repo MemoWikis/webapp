31300bdd4fe406ee5927dea9afb1ec7252dce277
Jule <jule@true-or-false.com>
2017-09-05 15:19:23 +0200

    Speed up reloading of KnowledgeWheel of pinned/unpinned category (wip)

--- src/TrueOrFalse.Frontend.Web/Views/Knowledge/Wheel/KnowledgeWheelController.cs
+++ src/TrueOrFalse.Frontend.Web/Views/Knowledge/Wheel/KnowledgeWheelController.cs
@@ -1,4 +1,7 @@
-﻿public class KnowledgeWheelController : BaseController
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class KnowledgeWheelController : BaseController
 {
     public string GetForSet(int setId)
     {
@@ -10,6 +13,29 @@
     public string GetForCategory(int categoryId)
     {
         var knowledgeSummary = KnowledgeSummaryLoader.RunFromMemoryCache(categoryId, UserId);
+        return RenderPartialView(knowledgeSummary);
+    }
+
+    public string GetForAddedCategoryTemp(int categoryId)
+    {
+        var questions = Sl.QuestionRepo.GetForCategoryAggregated(categoryId, UserId);
+        var probResults = new List<ProbabilityCalcResult>();
+        foreach (var question in questions)
+        {
+            var result = Sl.R<ProbabilityCalc_Simple1>().Run(question, User_());
+            if (result != null)
+            {
+                probResults.Add(result);
+            }
+        }
+
+        var knowledgeSummary = new KnowledgeSummary(
+            0,
+            probResults.Count(r => r.KnowledgeStatus == KnowledgeStatus.NotLearned),
+            probResults.Count(r => r.KnowledgeStatus == KnowledgeStatus.NeedsConsolidation),
+            probResults.Count(r => r.KnowledgeStatus == KnowledgeStatus.NeedsLearning),
+            probResults.Count(r => r.KnowledgeStatus == KnowledgeStatus.Solid)
+        );
         return RenderPartialView(knowledgeSummary);
     }
 
