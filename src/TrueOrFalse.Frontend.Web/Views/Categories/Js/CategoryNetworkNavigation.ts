be45ad5fecaf7a09b16a55ad2776a82e9a201861
c.mauersberger@googlemail.com <c.mauersberger@googlemail.com>
2017-09-12 23:37:10 +0200

    Add box to quickly navigate around categories and comprehend hierarchical tree (only visible for admins) #419070450369687

---
+++ src/TrueOrFalse.Frontend.Web/Views/Categories/Js/CategoryNetworkNavigation.ts
@@ -0,0 +1,15 @@
+﻿class CategoryNetworkNavigation {
+
+    constructor() {
+        $("#categoryNetworkNavigationWrapper").on("click", '.networkNavigationUpdate', function (e) {
+            e.preventDefault();
+            var newCenterCategoryId = $(this).attr("data-category-id");
+            $.get("/Categories/RenderCategoryNetworkNavigation/" + newCenterCategoryId,
+                htmlResult => {
+                    $("#categoryNetworkNavigation").html(htmlResult);
+                });
+
+        });
+    }
+
+}
