2c84081a5e9297479a51fb77a3c862cb936bc9c1
Jule <jule@true-or-false.com>
2017-09-14 11:40:34 +0200

    Entity: schedule  job for refreshing (wip)

---
+++ src/TrueOrFalse/Tools/ScheduledJobs/Jobs/RefreshEntityCache.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using Autofac;
+using NHibernate.Util;
+using Quartz;
+
+namespace TrueOrFalse.Utilities.ScheduledJobs
+{
+    public class RefreshEntityCache : IJob
+    {
+        public const int IntervalInMinutes = 60;
+
+        public void Execute(IJobExecutionContext context)
+        {
+            JobExecute.Run(scope => 
+            {
+                EntityCache.Init();
+            }, "RefreshEntityCache");
+        }
+
+    }
+}
