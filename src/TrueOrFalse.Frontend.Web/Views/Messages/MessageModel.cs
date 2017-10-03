702917301c82466d10b27310b4637286227eeaf6
c.mauersberger@googlemail.com <c.mauersberger@googlemail.com>
2017-10-03 18:47:33 +0200

    Display only unread messages to user, allow him/her to show all messages including read ones #408706935746668

--- src/TrueOrFalse.Frontend.Web/Views/Messages/MessageModel.cs
+++ src/TrueOrFalse.Frontend.Web/Views/Messages/MessageModel.cs
@@ -1,21 +1,48 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
+using System.Security.Policy;
 using TrueOrFalse;
+using TrueOrFalse.Frontend.Web.Code;
 
 public class MessageModel : BaseModel
 {
-    public List<MessageModelRow> Rows = new List<MessageModelRow>();
+    public List<MessageModelRow> Messages = new List<MessageModelRow>();
+    public int ReadMessagesCount;
 
     public MessageModel()
     {
-        
+        if (!_sessionUser.IsLoggedIn)
+        {
+            Messages.Add(new MessageModelRow(new Message
+            {
+                Subject = "Hallo Unbekannte(r)!",
+                Body = @"<p>
+                                schön, dass du bei uns vorbeischaust. 
+                                Du bist nicht eingeloggt, daher gibt es hier eigentlich nichts zu sehen.
+                                <a href='#' data-btn-login='true'><i class='fa fa-sign-in'></i>&nbsp;Logge dich am besten gleich ein</a> oder 
+                                <a href=" + Links.Register() +
+                       @"><i class='fa fa-user-plus'></i>&nbsp;registriere dich</a> als neuer Benutzer, es dauert nur wenige Sekunden.
+                            </p>
+                            <p>Wir wünschen dir weiter viel Spaß beim Stöbern.</p>
+                            <p>
+                                Viele Grüße,<br>
+                                Christof, Jule & Robert
+                            </p>",
+                DateCreated = DateTime.Now,
+                IsRead = false
+            }));
+        }
+
+        Messages = Resolve<MessageRepo>()
+            .GetForUser(_sessionUser.User.Id, true)
+            .Select(m => new MessageModelRow(m))
+            .ToList();
+
+        ReadMessagesCount = Resolve<MessageRepo>().GetNumberOfReadMessages(_sessionUser.UserId);
     }
 
-    public MessageModel(IList<Message> messages)
-    {
-        foreach (var msg in messages)
-            Rows.Add(new MessageModelRow(msg));
-    }
 }
 
 public class MessageModelRow
