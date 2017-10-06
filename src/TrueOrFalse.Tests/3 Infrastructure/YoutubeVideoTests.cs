d41b202445bcd3de13db3343bc71da84d93fb50b
Daniel Majunke <daniel.majunke@gmx.de>
2017-10-06 16:28:09 +0200

    Revert "disable part of test temporarily"
    This reverts commit 3d4d15f9b24b14cd1a204c6acbea3bac59f03d44.

--- src/TrueOrFalse.Tests/3 Infrastructure/YoutubeVideoTests.cs
+++ src/TrueOrFalse.Tests/3 Infrastructure/YoutubeVideoTests.cs
@@ -9,7 +9,7 @@ public class YoutubeVideoTests
     [Test]
     public void GetVideoKey()
     {
- //       Assert.That(YoutubeVideo.GetVideoKeyFromUrl("https://www.youtube.com/watch?v=KZp15FYS_ds"), Is.EqualTo("KZp15FYS_ds"));
+        Assert.That(YoutubeVideo.GetVideoKeyFromUrl("https://www.youtube.com/watch?v=KZp15FYS_ds"), Is.EqualTo("KZp15FYS_ds"));
         Assert.That(YoutubeVideo.GetVideoKeyFromUrl("https://www.youtube.com/"), Is.EqualTo(""));
         Assert.That(YoutubeVideo.GetVideoKeyFromUrl(null), Is.EqualTo(""));
         Assert.That(YoutubeVideo.GetVideoKeyFromUrl(""), Is.EqualTo(""));
