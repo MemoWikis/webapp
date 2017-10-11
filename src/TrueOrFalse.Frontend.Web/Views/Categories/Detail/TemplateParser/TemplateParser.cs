e62ef7728c56548642720a4b68080879e854187c
c.mauersberger@googlemail.com <c.mauersberger@googlemail.com>
2017-10-11 19:19:51 +0200

    Rename template "TestSetWidget" to "SetTestSessionNoStartScreen", add new Template "SetTestSessionAssessment" #447031916356702

--- src/TrueOrFalse.Frontend.Web/Views/Categories/Detail/TemplateParser/TemplateParser.cs
+++ src/TrueOrFalse.Frontend.Web/Views/Categories/Detail/TemplateParser/TemplateParser.cs
@@ -53,7 +53,8 @@ public class TemplateParser
         {
             case "topicnavigation":
             case "videowidget":
-            case "testsetwidget":
+            case "settestsessionnostartscreen":
+            case "settestsessionassessment":
             case "categorynetwork":
             case "contentlists":
             case "singleset":
@@ -96,8 +97,10 @@ public class TemplateParser
                 return new TopicNavigationModel(category, templateJson.Title, templateJson.Text, templateJson.Load, templateJson.Order);
             case "videowidget":
                 return new VideoWidgetModel(templateJson.SetId);
-            case "testsetwidget":
-                return new TestSetWidgetModel(templateJson.SetId, templateJson.Title, templateJson.Text);
+            case "settestsessionnostartscreen":
+                return new SetTestSessionNoStartScreenModel(templateJson.SetId, templateJson.Title, templateJson.Text);
+            case "settestsessionassessment":
+                return new SetTestSessionAssessmentModel(templateJson.SetId, templateJson.Title, templateJson.Text);
             case "categorynetwork":
             case "contentlists":
                 return new CategoryModel(category, loadKnowledgeSummary : false);
