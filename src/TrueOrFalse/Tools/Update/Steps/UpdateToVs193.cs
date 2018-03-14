af93d8110907619f3c75d1b9662eb2b7d6ee48e8
Dandor <daniel.majunke@gmx.de>
2018-03-14 15:10:42 +0100

    improved performance of get view count

---
+++ src/TrueOrFalse/Tools/Update/Steps/UpdateToVs193.cs
@@ -0,0 +1,18 @@
+﻿using NHibernate;
+
+namespace TrueOrFalse.Updates
+{
+    public class UpdateToVs193
+    {
+        public static void Run()
+        {
+            Sl.Resolve<ISession>()
+              .CreateSQLQuery(
+                @"ALTER TABLE `questionview`
+	                ADD INDEX `QuestionId` (`QuestionId`),
+	                ADD INDEX `UserId` (`UserId`);"
+            ).ExecuteUpdate();
+        }
+    }
+}
+
