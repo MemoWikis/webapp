f4c8f1a3dbc910b17cd259d5b3f3dc4afc3ed1c5
Dandor <daniel.majunke@gmx.de>
2018-05-19 14:57:50 +0200

    DeleteMarker work , delete AjaxCall not work #672864450340615

--- src/TrueOrFalse.Frontend.Web/Views/Categories/Js/CategoryDelete.ts
+++ src/TrueOrFalse.Frontend.Web/Views/Categories/Js/CategoryDelete.ts
@@ -5,7 +5,10 @@
         var categoryIdToDelete;
         var self = this;
 
+       
+
         $(document).on("click", 'a[href*=#modalDeleteCategory]', function (e) {
+           
             categoryIdToDelete = $(this).attr("data-categoryId");
             self.PopulateDeleteCategory(categoryIdToDelete);
         });
@@ -16,41 +19,64 @@
         });
 
         $('#confirmDelete').click(function (e) {
+            self.SafeDeleteCategory(435,443);
             self.DeleteCategory(categoryIdToDelete);
             $('#modalDeleteCategory').modal('hide');
             e.preventDefault();
+
         });
     }
 
     PopulateDeleteCategory(catId) {
-        $.ajax({
-            type: 'POST',
-            url: "/Categories/DeleteDetails/" + catId,
-            cache: false,
-            success: function (result) {
-                $("#spanCategoryTitle").html(result.categoryTitle.toString());
-            },
-            error: function (result) {
-                window.console.log(result);
-                window.alert("Ein Fehler ist aufgetreten");
-            }
-        });
+        //$.ajax({
+        //    type: 'POST',
+        //    url: "/Categories/DeleteDetails/" + catId,
+        //    cache: false,
+        //    success: function (result) {
+        //        $("#spanCategoryTitle").html(result.categoryTitle.toString());
+        //    },
+        //    error: function (result) {
+        //        window.console.log(result);
+        //        window.alert("Ein Fehler ist aufgetreten");
+        //    }
+        //});
     }
 
     DeleteCategory(catId) {
         $.ajax({
             type: 'POST',
-            url: "/Categories/Delete/" + catId,
+            url: "/Categories/SetDeleteMarker/" + catId,
             cache: false,
-            success: function() {
-                window.alert("Das Thema wurde erfolgreich gelöscht.");
-                window.location.href = "/Kategorien";
+            success(data) {
+                console.log(data);
             },
-            error: function (result) {
-                window.console.log(result);
-                window.alert("Ein Fehler ist aufgetreten");
+            error(xhr, ajaxOptions, thrownError) {
+                alert(xhr.status);
+                alert(thrownError);
             }
+
         });
+
+        //$.ajax({
+        //    type: 'POST',
+        //    url: "/Categories/Delete/" + catId,
+        //    cache: false,
+        //    success: function() {
+        //        window.alert("Das Thema wurde erfolgreich gelöscht.");
+        //        window.location.href = "/Kategorien";
+        //    },
+        //    error: function (result) {
+        //        window.console.log(result);
+        //        window.alert("Ein Fehler ist aufgetreten");
+        //    }
+        //});
+    }
+
+    SafeDeleteCategory(catId,userID) {
+       
+        console.log("Hallo Daniel");
+           
+      
     }
     
 }
