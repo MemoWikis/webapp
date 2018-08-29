43da3b5c293e71ef8230e211af5ede3d81373752
Robert Mischke <robertmischke@gmail.com>
2018-08-29 12:55:41 +0200

    EduSharingApiController, change Name encoding, reverted

--- src/TrueOrFalse.Frontend.Web/Views/Api/EduSharingApiController.cs
+++ src/TrueOrFalse.Frontend.Web/Views/Api/EduSharingApiController.cs
@@ -2,7 +2,6 @@
 using System.Linq;
 using System.Web.Mvc;
 using Seedworks.Lib.Persistence;
-using SolrNet.Utils;
 using TrueOrFalse.Frontend.Web.Code;
 
 public class EduSharingApiController : BaseController
@@ -39,7 +38,7 @@ public class EduSharingApiController : BaseController
                 .Select(category => new
                 {
                     TopicId = category.Id,
-                    Name = HttpUtility.HtmlEncode(category.Name) ,
+                    Name = category.Name,
                     ImageUrl = Settings.CanonicalHost + new CategoryImageSettings(category.Id).GetUrl_350px_square().Url,
                     ItemUrl = Settings.CanonicalHost + Links.CategoryDetail(category.Name, category.Id),
                     Licence = "CC_BY",
