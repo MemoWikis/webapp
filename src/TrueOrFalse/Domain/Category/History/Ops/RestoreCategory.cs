dabf638d24b714f5594fc8c7e237c4e51d1c1f9c
Florian Klumb <florian.klumb@rwth-aachen.de>
2018-10-02 14:47:49 +0200

    added draft for notifying category author (among others) of a user restoring an old revision of a category

--- src/TrueOrFalse/Domain/Category/History/Ops/RestoreCategory.cs
+++ src/TrueOrFalse/Domain/Category/History/Ops/RestoreCategory.cs
@@ -6,6 +6,21 @@
         var categoryChange = Sl.CategoryChangeRepo.GetByIdEager(categoryChangeId);
         var historicCategory = categoryChange.ToHistoricCategory();
         Sl.CategoryRepo.Update(historicCategory, author);
+
+        NotifyAboutRestore(categoryChange);
     }
 
+    private static void NotifyAboutRestore(CategoryChange categoryChange)
+    {
+        var category = categoryChange.Category;
+        var currentUser = Sl.UserRepo.GetById(Sl.CurrentUserId);
+        var subject = $"Kategorie {category.Name} zurückgesetzt";
+        var body = $"Die Kategorie '{category.Name}' wurde gerade zurückgesetzt.\n" +
+                   $"Zurückgesetzt auf Revision: vom {categoryChange.DateCreated} (Id {categoryChange.Id})\n" +
+                   $"Von Benutzer: {currentUser.Name} (Id {currentUser.Id})";
+
+        CustomMsg.Send(category.Creator.Id, subject, body);
+        CustomMsg.Send(Constants.MemuchoAdminUserId, subject, body);
+        //CustomMsg.Send("Franziska", subject, body);
+    }
 }
