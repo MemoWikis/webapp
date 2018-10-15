fc57e5cdff2490fac399ebb29236bb7194a9ca35
Florian Klumb <florian.klumb@rwth-aachen.de>
2018-10-15 08:59:52 +0200

    improved: changes of category name shown, prepared to show changes of relations

--- src/TrueOrFalse.Frontend.Web/Views/Categories/History/Detail/CategoryHistoryDetail.aspx
+++ src/TrueOrFalse.Frontend.Web/Views/Categories/History/Detail/CategoryHistoryDetail.aspx
@@ -74,18 +74,23 @@
                         Zwischen den beiden Revisionen (vom <%= Model.PrevChange.DateCreated %> und 
                         vom <%= Model.CurrentChange.DateCreated %>) gibt es <b>keine inhaltlichen Unterschiede</b>.
                     </div>
+                    <input type="hidden" id="currentName" value="<%= Server.HtmlEncode(Model.CurrentName) %>"/>
+                    <input type="hidden" id="prevName" value="<%= Server.HtmlEncode(Model.PrevName) %>"/>
                     <input type="hidden" id="currentMarkdown" value="<%= Server.HtmlEncode(Model.CurrentMarkdown) %>"/>
                     <input type="hidden" id="prevMarkdown" value="<%= Server.HtmlEncode(Model.PrevMarkdown) %>"/>
                     <input type="hidden" id="currentDescription" value="<%= Server.HtmlEncode(Model.CurrentDescription) %>"/>
                     <input type="hidden" id="prevDescription" value="<%= Server.HtmlEncode(Model.PrevDescription) %>"/>
                     <input type="hidden" id="currentWikipediaUrl" value="<%= Server.HtmlEncode(Model.CurrentWikipediaUrl) %>"/>
                     <input type="hidden" id="prevWikipediaUrl" value="<%= Server.HtmlEncode(Model.PrevWikipediaUrl) %>"/>
+                    <%--<input type="hidden" id="relationsDiff" value="<%= Server.HtmlEncode(Model.RelationsDiff) %>"/>--%>
                     <input type="hidden" id="currentDateCreated" value="<%= Model.CurrentChange.DateCreated %>" />
                     <input type="hidden" id="prevDateCreated" value="<%= Model.PrevChange.DateCreated %>" />
                     <div id="diffPanel">
+                        <div id="diffName"></div>
                         <div id="diffDescription"></div>
                         <div id="diffWikipediaUrl"></div>
                         <div id="diffData"></div>
+                        <div id="diffRelations" class="alert alert-info" role="alert"></div>
                     </div>
                 <% } %>
             </div>
--- src/TrueOrFalse.Frontend.Web/Views/Categories/History/Detail/CategoryHistoryDetailModel.cs
+++ src/TrueOrFalse.Frontend.Web/Views/Categories/History/Detail/CategoryHistoryDetailModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Microsoft.Ajax.Utilities;
 using Newtonsoft.Json;
 
 public class CategoryHistoryDetailModel : BaseModel
@@ -10,12 +11,15 @@ public class CategoryHistoryDetailModel : BaseModel
     public string CategoryName;
     public CategoryChange CurrentChange;
     public CategoryChange PrevChange;
+    public string CurrentName;
+    public string PrevName;
     public string CurrentMarkdown;
     public string PrevMarkdown;
     public string CurrentDescription;
     public string PrevDescription;
     public string CurrentWikipediaUrl;
     public string PrevWikipediaUrl;
+    //public string RelationsDiff;
     public string AuthorName;
     public string AuthorImageUrl;
 
@@ -25,16 +29,22 @@ public class CategoryHistoryDetailModel : BaseModel
         PrevChange = prevChange;
 
         var currentRevisionData = currentChange.GetCategoryChangeData();
+        CurrentName = currentRevisionData.Name;
         CurrentMarkdown = currentRevisionData.TopicMardkown?.Replace("\\r\\n", "\r\n");
         CurrentDescription = currentRevisionData.Description?.Replace("\\r\\n", "\r\n");
         CurrentWikipediaUrl = currentRevisionData.WikipediaURL;
 
         if (PrevChange != null)
         {
-            var prevRevisionData = prevChange?.GetCategoryChangeData();
+            var prevRevisionData = PrevChange.GetCategoryChangeData();
+            PrevName = prevRevisionData?.Name;
             PrevMarkdown = prevRevisionData?.TopicMardkown?.Replace("\\r\\n", "\r\n");
             PrevDescription = prevRevisionData?.Description?.Replace("\\r\\n", "\r\n");
             PrevWikipediaUrl = prevRevisionData?.WikipediaURL;
+
+            //var prevRelations = prevRevisionData.CategoryRelations;
+            //RelationsDiff = string.Join(", ", currentRevisionData.CategoryRelations.Except(prevRelations)
+            //    .Select(c => $"Bezugsthema: {c.RelatedCategoryId}, Beziehung: {c.RelationType}"));
         }
 
         CategoryId = currentChange.Category.Id;
