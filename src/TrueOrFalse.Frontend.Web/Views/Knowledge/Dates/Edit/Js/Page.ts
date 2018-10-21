fdba0ab804d53e5b63aef8b6f2ca6231793099a4
Dandor <daniel.majunke@gmx.de>
2018-10-21 11:01:35 +0200

    Move EditDate to Knowledge/Dates

---
+++ src/TrueOrFalse.Frontend.Web/Views/Knowledge/Dates/Edit/Js/Page.ts
@@ -0,0 +1,10 @@
+﻿$(function () {
+    new AutocompleteSets("#txtSet");
+
+
+    $("#EditDateForm").submit(function () {
+        $("#btnSave").html("<i class=\"fa fa-spinner fa-spin\">&nbsp;</i>" + $("#btnSave").html());
+        $("#btnSave").prop("disabled", true);
+        return true;
+    });
+}); 
