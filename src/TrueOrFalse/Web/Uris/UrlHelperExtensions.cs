b269bd6996d62584a9bad6e4536a022ae55fd7d6
Florian Klumb <florian.klumb@rwth-aachen.de>
2018-12-13 10:15:04 +0100

    added: extension method for UrlHelper which allows to ignore current route values

---
+++ src/TrueOrFalse/Web/Uris/UrlHelperExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+public static class UrlHelperExtensions
+{
+    public static string Action(this UrlHelper urlHelper, string actionName, string controllerName, object routeValues, bool ignoreCurrentRouteValues = false)
+    {
+        var routeValueDictionary = new RouteValueDictionary(routeValues);
+        var requestContext = urlHelper.RequestContext;
+        if (ignoreCurrentRouteValues)
+        {
+            var currentRouteData = requestContext.RouteData;
+            var newRouteData = new RouteData(currentRouteData.Route, currentRouteData.RouteHandler);
+            requestContext = new RequestContext(requestContext.HttpContext, newRouteData);
+        }
+
+        return UrlHelper.GenerateUrl(null, actionName, controllerName, routeValueDictionary,
+            urlHelper.RouteCollection, requestContext, includeImplicitMvcValues: false);
+    }
+}
