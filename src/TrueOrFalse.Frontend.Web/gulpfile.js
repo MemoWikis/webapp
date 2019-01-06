ee54f0a352b880806142c50b2ce9e7a46f5835af
tj-mai <mai.t.tung@gmail.com>
2019-01-06 22:21:30 +0100

    added gulp file, copy tiptap to npm folder, added tiptap to sources

---
+++ src/TrueOrFalse.Frontend.Web/gulpfile.js
@@ -0,0 +1,30 @@
+﻿/// <binding AfterBuild='copyScripts' />
+var deps = {
+    "tiptap": {
+        "dist/tiptap.js": ""
+    }
+};
+
+var merge = require('merge-stream'); 
+var gulp = require('gulp');
+
+function copyScripts(cb) {
+
+    var streams = [];
+
+    for (var prop in deps) {
+        console.log("Prepping Scripts for: " + prop);
+
+        //console.log(gulp.dest("scripts/npm/"));
+
+        for (var itemProp in deps[prop]) {
+            streams.push(gulp.src("node_modules/" + prop + "/" + itemProp)
+                .pipe(gulp.dest("scripts/npm/" + prop + "/" + deps[prop][itemProp])));
+        }
+    }
+    merge(streams);
+
+    cb();
+}
+
+exports.copyScripts = copyScripts;
