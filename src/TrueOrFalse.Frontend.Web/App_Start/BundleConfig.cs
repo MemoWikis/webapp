66fb4f07397011f41258249390c71798e51fb2cd
Dandor <daniel.majunke@gmx.de>
2019-01-06 13:26:07 +0100

    Error in Bundle.config corrected

--- src/TrueOrFalse.Frontend.Web/App_Start/BundleConfig.cs
+++ src/TrueOrFalse.Frontend.Web/App_Start/BundleConfig.cs
@@ -62,8 +62,7 @@ namespace TrueOrFalse.View
                 .IncludeDirectory("~/Views/Help/Js", "*.js"));
 
             bundles.Add(new StyleBundle("~/bundles/Category")
-                .Include("~/Views/Categories/Detail/Category.css")
-                .Include("~/Views/Categories/Detail/Js/SquareWishKnowledge.js"));
+                .Include("~/Views/Categories/Detail/Category.css"));
 
             bundles.Add(new ScriptBundle("~/bundles/js/Category")
                 .Include("~/Views/Knowledge/Wheel/KnowledgeWheel.js")
