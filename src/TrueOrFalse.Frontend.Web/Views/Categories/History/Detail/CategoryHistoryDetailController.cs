ea418c0495e7859871d7c6a44f6bfb52696b74be
Florian Klumb <florian.klumb@rwth-aachen.de>
2019-01-10 14:28:38 +0100

    refactoring

--- src/TrueOrFalse.Frontend.Web/Views/Categories/History/Detail/CategoryHistoryDetailController.cs
+++ src/TrueOrFalse.Frontend.Web/Views/Categories/History/Detail/CategoryHistoryDetailController.cs
@@ -6,8 +6,8 @@ public class CategoryHistoryDetailController : Controller
     public ActionResult Detail(int categoryChangeId)
     {
         var currentRevision = Sl.CategoryChangeRepo.GetByIdEager(categoryChangeId);
-        var previousRevision = currentRevision.GetPreviousRevision();
-        var nextRevision = currentRevision.GetNextRevision();
+        var previousRevision = Sl.CategoryChangeRepo.GetPreviousRevision(currentRevision);
+        var nextRevision = Sl.CategoryChangeRepo.GetNextRevision(currentRevision);
 
         return View("~/Views/Categories/History/Detail/CategoryHistoryDetail.aspx", 
             new CategoryHistoryDetailModel(currentRevision, previousRevision, nextRevision));
