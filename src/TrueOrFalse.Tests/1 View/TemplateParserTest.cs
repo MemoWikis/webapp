1dc9810de8e988850bbe34673a2fb28df301473f
tj-mai <mai.t.tung@gmail.com>
2019-02-12 22:31:58 +0100

    minor

---
+++ src/TrueOrFalse.Tests/1 View/TemplateParserTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+[TestFixture]
+class TemplateParserTest
+{
+    [Test]
+    public void Parser_should_parse_correct()
+    {
+        //TemplateParser.Run("Test", new Category("test"), )
+    }
+}
