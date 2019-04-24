78abeeb32eade93a45931a7856c4658ff99b0373
tj-mai <mai.t.tung@gmail.com>
2019-04-24 23:23:00 +0200

    WIP DescriptionMigration, moved test to right folder

---
+++ src/TrueOrFalse/Tools/Update/TemplateMigration/DescriptionMigration.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using static System.String;
+
+namespace TemplateMigration
+{
+    public partial class DescriptionMigration
+    {
+        public static void Start()
+        {
+            var allCategories = Sl.CategoryRepo.GetAll();
+
+            foreach (var category in allCategories)
+            {
+                if (!IsNullOrEmpty(category.Description) && !IsNullOrEmpty(category.WikipediaURL) && !IsNullOrEmpty(category.Url) && !IsNullOrEmpty(category.UrlLinkText))
+                {
+                    var topicMarkdownBeforeUpdate = category.TopicMarkdown;
+                    var categoryId = category.Id;
+                    string newMarkdown = "";
+
+                    if (!IsNullOrEmpty(category.Description))
+                    {
+                        newMarkdown = newMarkdown + category.Description + System.Environment.NewLine;
+                        category.Description = "";
+                    }
+
+                    if (!IsNullOrEmpty(category.WikipediaURL))
+                    {
+                        newMarkdown = newMarkdown + category.WikipediaURL + System.Environment.NewLine;
+                        category.WikipediaURL = "";
+
+                    }
+
+                    if (!IsNullOrEmpty(category.Url))
+                    {
+                        newMarkdown = newMarkdown + "[" + category.UrlLinkText + "](" + category.Url + ")" + System.Environment.NewLine;
+                        category.Url = "";
+                    }
+
+                    category.UrlLinkText = "";
+
+                    newMarkdown = newMarkdown + category.TopicMarkdown;
+
+                    category.TopicMarkdown = newMarkdown;
+
+                    Sl.CategoryRepo.UpdateBeforeEntityCacheInit(category);
+
+                    Logg.r().Information("{categoryId} {beforeMarkdown} {afterMarkdown}", categoryId, topicMarkdownBeforeUpdate, category.TopicMarkdown);
+                }
+            }
+        }
+    }
+}
