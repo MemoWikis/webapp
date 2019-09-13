fb5dbcd282bbc6e84c9fd9ff1a1b8c3cae28d71d
Dandor <daniel.majunke@gmx.de>
2019-09-13 10:55:10 +0200

    TestSession "Hab ich gewusst " performance is fixed

---
+++ src/TrueOrFalse/Tools/Update/Steps/UpdateToVs203.cs
@@ -0,0 +1,17 @@
+﻿using NHibernate;
+
+namespace TrueOrFalse.Updates
+{
+    public class UpdateToVs203
+    {
+        public static void Run()
+        {
+            Sl.Resolve<ISession>()
+                .CreateSQLQuery(
+                    @"CREATE INDEX `IX_QuestionViewGuid` ON `answer`(`QuestionViewGuid`) USING BTREE;"
+                ).ExecuteUpdate();
+
+            
+        }
+    }
+}
