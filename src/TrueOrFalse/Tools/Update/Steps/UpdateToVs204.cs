4edd9c37ff817f7ded8bf64e64364404d32087e1
Dandor <daniel.majunke@gmx.de>
2019-09-24 11:10:29 +0200

    Update questionview Userid can be set to null

---
+++ src/TrueOrFalse/Tools/Update/Steps/UpdateToVs204.cs
@@ -0,0 +1,18 @@
+﻿using NHibernate;
+
+namespace TrueOrFalse.Updates
+{
+    public class UpdateToVs204
+    {
+        public static void Run()
+        {
+            Sl.Resolve<ISession>()
+                .CreateSQLQuery(
+            @"ALTER TABLE `memucho`.`questionview` 
+            CHANGE COLUMN `UserId` `UserId` INT(10) NULL ;")
+                .ExecuteUpdate();
+
+            
+        }
+    }
+}
