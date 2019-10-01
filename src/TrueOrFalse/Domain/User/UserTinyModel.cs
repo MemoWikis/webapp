56ddcd82788972010c107dbfc58980ad91fba8e2
Dandor <daniel.majunke@gmx.de>
2019-10-01 17:26:46 +0200

    Change UserTinymodel from Frontend to Backend
    Add IUserTinyModel
    user add ITinyModel and remove isNullOrEmpty for isFacebook and IsGoogle- user

---
+++ src/TrueOrFalse/Domain/User/UserTinyModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Seedworks.Lib.Persistence;
+
+
+public interface IUserTinyModel
+{
+    string Name { get; set; }
+    int Id { get; set; }
+    string EmailAddress { get; set; }
+    bool IsFacebookUser { get; }
+    bool IsGoogleUser { get; }
+    string FacebookId { get; set; }
+
+    string GoogleId { get; set; }
+}
+
+public class UserTinyModel : IUserTinyModel
+{
+    public string Name { get; set; }
+    public int Id { get; set; }
+    public string EmailAddress { get; set; }
+    public bool IsFacebookUser { get; set; }
+    public bool IsGoogleUser { get; }
+    public string FacebookId { get; set; }
+    public string GoogleId { get; set; }
+
+    private readonly User _user;
+
+    public UserTinyModel(User user)
+    {
+        _user = user;
+
+        if (_user == null)
+        {
+            Name = "Unbekannt";
+        }
+        else
+        {
+            Name = _user.Name;
+            EmailAddress = _user.EmailAddress;
+        }
+
+        EmailAddress = _user == null ? "unbekannt" : _user.EmailAddress;
+        IsFacebookUser = _user != null && _user.IsFacebookUser;
+        IsGoogleUser = _user != null && _user.IsGoogleUser;
+        Id = _user == null ? -1 : _user.Id;
+        FacebookId = _user == null ? "null" : _user.FacebookId;
+        GoogleId = _user == null ? "null" : _user.GoogleId;
+    }
+}
