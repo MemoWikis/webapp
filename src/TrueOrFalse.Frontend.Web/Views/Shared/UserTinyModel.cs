bcb21462082f5fc98f9a15cca6567a7fbbe032ae
Dandor <daniel.majunke@gmx.de>
2019-10-01 13:34:14 +0200

    Add UserTinyModel

---
+++ src/TrueOrFalse.Frontend.Web/Views/Shared/UserTinyModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+public class UserTinyModel
+{
+    public string Name;
+    public string ImageUrl; 
+
+
+    private readonly User _user;
+
+    public UserTinyModel(User user)
+    {
+        _user = user;
+
+
+        if (_user == null)
+        {
+            Name = "Unbekannt";
+        }
+        else
+        {
+            Name = _user.Name;
+
+        }
+
+    }
+
+    
+}
