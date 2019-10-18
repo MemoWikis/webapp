57893295e830cb353ca8c9faf68edda512c8a587
Dandor <daniel.majunke@gmx.de>
2019-10-18 09:49:49 +0200

    fix bug KnowledgeStatus

--- src/TrueOrFalse/Domain/Knowledge/KnowledgeStatus.cs
+++ src/TrueOrFalse/Domain/Knowledge/KnowledgeStatus.cs
@@ -24,9 +24,8 @@ public static class KnowledgeStatusExt
             case KnowledgeStatus.NeedsConsolidation: return "#FDD648";
             case KnowledgeStatus.Solid: return "#90EE90";
             default: Logg.r().Information("unknown Konwledge Status/ KonwledgeStatus.cs row 26 ");
-                break;
+                return "#C0C0C0";
         }
-        throw new Exception("unknown status");
     }
 
     public static string GetText(this KnowledgeStatus status)
