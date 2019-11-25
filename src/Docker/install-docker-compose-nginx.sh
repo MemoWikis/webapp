8e88999b98f8c857c247ccf86cce99e9c529e6b7
Dandor <daniel.majunke@gmx.de>
2019-11-25 13:01:10 +0100

    sh for install to nginx frontend,docker and docker compose with reboot

---
+++ src/Docker/install-docker-compose-nginx.sh
@@ -0,0 +1,33 @@
+sudo apt-get update
+
+apt-get install nginx
+service nginx restart
+
+
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg2 \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+
+apt-key fingerprint 0EBFCD88
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+
+apt-get update
+
+apt-get install docker-ce docker-ce-cli containerd.io
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo mv /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+sudo chmod +x /usr/bin/docker-compose
+
+reboot
