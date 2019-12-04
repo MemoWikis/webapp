f958deb31d926d1c945fa2c90f49039eca76829d
tj-mai <mai.t.tung@gmail.com>
2019-12-04 11:15:12 +0100

    solrfield fix

--- src/TrueOrFalse/Search/Category/Model/ToCategorytSolrMap.cs
+++ src/TrueOrFalse/Search/Category/Model/ToCategorytSolrMap.cs
@@ -12,7 +12,10 @@ namespace TrueOrFalse.Search
             result.Name = category.Name;
             result.Description = category.Description;
 
-            result.CreatorId = category.Creator.Id;
+            if (category.Creator != null)
+                result.CreatorId = category.Creator.Id;
+            else
+                result.CreatorId = -1;
 
             result.ValuatorIds = valuations.Where(v => v.RelevancePersonal != -1).Select(x => x.UserId).ToList();
             result.ValuationsCount = category.TotalRelevancePersonalEntries;
