1a8889c7870c7e14005cccd694e8cd6bea0b18ec
Dandor <daniel.majunke@gmx.de>
2019-12-10 21:26:57 +0100

    update complete TotalInOthersWishknowledge

--- src/TrueOrFalse/Tools/Update/Steps/UpdateToVs207.cs
+++ src/TrueOrFalse/Tools/Update/Steps/UpdateToVs207.cs
@@ -1,7 +1,8 @@
 ﻿using TrueOrFalse;
-using NHibernate;
+using RabbitMQ.Client.Impl;
 using Seedworks.Lib.Persistence;
 using TrueOrFalse.Search;
+using ISession = NHibernate.ISession;
 
 
 namespace TrueOrFalse.Updates
@@ -10,9 +11,22 @@ namespace TrueOrFalse.Updates
     {
         public static void Run()
         {
-            Sl.Resolve<ISession>()
+            var session = Sl.Resolve<ISession>();
+
+            session
                 .CreateSQLQuery(
                     @"ALTER TABLE `memucho`.`user` ADD COLUMN `TotalInOthersWishknowledge` INT NULL AFTER `DateModified`;"
+                ).ExecuteUpdate();
+
+            session
+                .CreateSQLQuery(
+                    @"UPDATE user SET TotalInOthersWishknowledge = (
+                        SELECT count(*) FROM questionvaluation qv
+                        JOIN question q
+                        ON qv.Questionid = q.Id
+                        WHERE qv.RelevancePersonal > 0
+                        AND q.Creator_id = user.Id
+                        AND qv.UserId <> user.Id);"
                 ).ExecuteUpdate();
         }
     }
