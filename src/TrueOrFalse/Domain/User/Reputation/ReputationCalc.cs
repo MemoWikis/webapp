0ee18f2a7fcae6af24ba4be4becf830808d83084
Dandor <daniel.majunke@gmx.de>
2019-12-11 10:26:25 +0100

    change GetCountOfCategoriesInOtherPeoplesWishknowledge from function to mysql query
    Remove unneeded code

--- src/TrueOrFalse/Domain/User/Reputation/ReputationCalc.cs
+++ src/TrueOrFalse/Domain/User/Reputation/ReputationCalc.cs
@@ -39,8 +39,8 @@ public class ReputationCalc : IRegisterAsInstancePerLifetime
 
         /*Calculate Reputation for Questions, Sets, Categories in other user's wish knowledge */
 
-        var countQuestionsInOtherWishknowledge = GetCountOfQuestionsInOtherPeoplesWishknowledge(user);
-        result.ForQuestionsInOtherWishknowledge = countQuestionsInOtherWishknowledge * PointsPerQuestionInOtherWishknowledge;
+        var countQuestionsInOtherWishknowledge = Sl.UserRepo.GetByIds(user.Id);
+        result.ForQuestionsInOtherWishknowledge = countQuestionsInOtherWishknowledge[0]. * PointsPerQuestionInOtherWishknowledge;
 
         var countSetsInOtherWishknowledge = GetCountOfSetsInOtherPeoplesWishknowledge(user);
         result.ForSetsInOtherWishknowledge = countSetsInOtherWishknowledge * PointsPerSetInOtherWishknowledge;
@@ -64,30 +64,8 @@ public class ReputationCalc : IRegisterAsInstancePerLifetime
 
         return result;
     }
-
-    private int GetCountOfQuestionsInOtherPeoplesWishknowledge(User user)
-    {
-        var countQuestionsInOtherWishknowledge = _session.QueryOver<QuestionValuation>()
-            .Where(qv => qv.User != user)
-            .And(qv => qv.RelevancePersonal != -1)
-            .JoinQueryOver<Question>(qv => qv.Question)
-            .Where(q => q.Creator == user)
-            .RowCount();
-        return countQuestionsInOtherWishknowledge;
-    }
-
     private int GetCountOfSetsInOtherPeoplesWishknowledge(User user)
     {
-        //The following doesn't work, unfortunately (reason: Set is not a property of setValuation)
-        //var countSetsInOtherWishknowledge = _session.QueryOver<SetValuation>()
-        //    .Where(sv => sv.UserId != user.Id)
-        //    .And(qv => qv.RelevancePersonal != -1)
-        //    .Left.JoinAlias()
-        //    .JoinQueryOver<Set>(sv => sv.Set)
-        //    .Where(s => s.Creator == user)
-        //    .RowCount();
-
-        //this is the alternative by writing sql-code (it is working)
         var tinyUser = new UserTinyModel(user);
         var query =
             $@"SELECT count(*)
@@ -101,20 +79,4 @@ public class ReputationCalc : IRegisterAsInstancePerLifetime
         var countSetsInOtherWishknowledge = Convert.ToInt32(_session.CreateSQLQuery(query).UniqueResult());
         return countSetsInOtherWishknowledge;
     }
-
-    private int GetCountOfCategoriesInOtherPeoplesWishknowledge(User user)
-    {
-        var query =
-            $@"SELECT count(*)
-                FROM categoryvaluation cv
-                LEFT JOIN questionset s
-                ON cv.CategoryId = s.Id
-                WHERE s.Creator_id = {user.Id}
-                AND sv.UserId <> {user.Id}
-                AND sv.RelevancePersonal <> -1";
-
-        var countSetsInOtherWishknowledge = Convert.ToInt32(_session.CreateSQLQuery(query).UniqueResult());
-        return countSetsInOtherWishknowledge;
-    }
-
 }
