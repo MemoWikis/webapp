8929635eb09e05db23f9a14fbc656df2a7174eb6
Dandor <daniel.majunke@gmx.de>
2019-12-17 12:00:08 +0100

    add and fill column FollowerCount

---
+++ src/TrueOrFalse/Tools/Update/Steps/UpdateToVs208.cs
@@ -0,0 +1,29 @@
+﻿using TrueOrFalse;
+using RabbitMQ.Client.Impl;
+using Seedworks.Lib.Persistence;
+using TrueOrFalse.Search;
+using ISession = NHibernate.ISession;
+
+
+namespace TrueOrFalse.Updates
+{
+    public class UpdateToVs208
+    {
+        public static void Run()
+        {
+            var session = Sl.Resolve<ISession>();
+
+            session
+                .CreateSQLQuery(
+                    @"ALTER TABLE `user` ADD COLUMN `FollowerCount` INT NULL AFTER `DateModified`;"
+                ).ExecuteUpdate();
+
+            session
+                .CreateSQLQuery(
+                    @"UPDATE user u SET FollowerCount = (
+                        SELECT count(*) FROM  user_to_follower uf
+                        WHERE uf.User_id =u.id);"
+                ).ExecuteUpdate();
+        }
+    }
+}
