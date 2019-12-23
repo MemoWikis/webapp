63123f02e998defb2e62433ae76aab8a27937c5e
Dandor <daniel.majunke@gmx.de>
2019-12-23 17:02:58 +0100

    Code review

--- src/TrueOrFalse/Domain/Question/QuestionValuation/Ops/QuestionInKnowledge.cs
+++ src/TrueOrFalse/Domain/Question/QuestionValuation/Ops/QuestionInKnowledge.cs
@@ -38,24 +38,18 @@ public static class QuestionInKnowledge
         session.Flush();
     }
 
-    private static void ChangeTotalInOthersWishknowledge( bool isIncrement, User user, int questionId)
+    private static void ChangeTotalInOthersWishknowledge(User creator)
     {
-        var question = EntityCache.GetQuestionById(questionId);
-        if (question.Creator.Id != -1 && question.Creator.Id != user.Id)
-        {
-            if (isIncrement)
-                Sl.Resolve<ISession>()
-                    .CreateSQLQuery(
-                        "Update user Set TotalInOthersWishknowledge = TotalInOthersWishknowledge + 1 where id = " +
-                        question.Creator.Id + ";")
-                    .ExecuteUpdate();
-            else
-                Sl.Resolve<ISession>()
-                    .CreateSQLQuery(
-                        "Update user Set TotalInOthersWishknowledge = TotalInOthersWishknowledge - 1 where id = " +
-                        question.Creator.Id + ";")
-                    .ExecuteUpdate();
-        }
+        if (creator == null)
+            return;
+
+        Sl.Resolve<ISession>()
+            .CreateSQLQuery(
+                "UPDATE user SET TotalInOthersWishknowledge =" +
+                " (SELECT count(*) FROM questionvaluation qv" +
+                " JOIN question q ON qv.Questionid = q.Id WHERE qv.RelevancePersonal > 0 " +
+                "AND q.Creator_id =" + creator.Id + " AND qv.UserId <>" + creator.Id + ") Where user.Id =" + creator.Id + ";")
+            .ExecuteUpdate();
     }
 
     private static void UpdateRelevancePersonal(IList<Question> questions, User user, int relevance = 50, SaveType saveType = SaveType.CacheAndDatabase)
@@ -65,7 +59,7 @@ public static class QuestionInKnowledge
         foreach (var question in questions)
         {
             CreateOrUpdateValuation(question, questionValuations.ByQuestionId(question.Id), user, relevance, saveType);
-            ChangeTotalInOthersWishknowledge(false, user, question.Id);
+            ChangeTotalInOthersWishknowledge(question.Creator);
 
             if (saveType == SaveType.DatabaseOnly || saveType == SaveType.CacheAndDatabase)
                 Sl.Session.CreateSQLQuery(GenerateRelevancePersonal(question.Id)).ExecuteUpdate();
@@ -80,11 +74,11 @@ public static class QuestionInKnowledge
         var creatorGroups = questions.Select(q => q.Creator).GroupBy(c => c.Id);
         foreach (var creator in creatorGroups)
             ReputationUpdate.ForUser(creator.First());
+        
     }
 
     private static void UpdateRelevancePersonal(int questionId, User user, int relevance = 50, SaveType saveType = SaveType.CacheAndDatabase)
     {
-        ChangeTotalInOthersWishknowledge(false, user, questionId);
         CreateOrUpdateValuation(questionId, user, relevance, saveType);
 
         if(saveType == SaveType.CacheOnly || saveType == SaveType.CacheAndDatabase)
@@ -101,6 +95,9 @@ public static class QuestionInKnowledge
             if (relevance != -1)
                 ProbabilityUpdate_Valuation.Run(questionId, user.Id);
         }
+
+        var question = EntityCache.GetQuestionById(questionId);
+        ChangeTotalInOthersWishknowledge(question.Creator);
     }
 
     public static void SetUserWishCountQuestions(User user)
