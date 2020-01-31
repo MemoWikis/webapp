a8d7091b8a1593af30702e948b63995dc4bb8f62
dandor <daniel.majunke@gmx.de>
2020-01-31 07:29:35 +0100

    see last commit

--- src/TrueOrFalse.Frontend.Web/Views/Questions/Answer/Js/AnswerQuestionCards.ts
+++ src/TrueOrFalse.Frontend.Web/Views/Questions/Answer/Js/AnswerQuestionCards.ts
@@ -4,9 +4,19 @@
         this.hideChildren(children);
         $("#MoreParentsAndChildrens").addClass("hide");
 
+        $(window).on("resize",
+            () => {
+                if ($(window).width() <= 750)
+                    $(".singleCatFullWidth").children().eq(0).removeClass("flex");
+                else
+                    $(".singleCatFullWidth").children().eq(0).addClass("flex");
+
+            });
+
         if (children.length > 5) {
             $("#MoreParentsAndChildrens").removeClass("hide").css("cursor", "pointer");
             $("#MoreParentsAndChildrens").children().eq(0).text("Alle Themen anzeigen (" + children.length + ")");
+
         }
 
         $("#MoreParentsAndChildrens").on("click", () => {
