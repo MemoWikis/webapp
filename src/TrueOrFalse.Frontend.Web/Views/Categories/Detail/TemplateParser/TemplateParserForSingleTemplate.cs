469db48d5a7a84e2787118fcf4f67731f8b68019
tj-mai <mai.t.tung@gmail.com>
2020-03-06 11:05:49 +0100

    categorynetwork module fix

--- src/TrueOrFalse.Frontend.Web/Views/Categories/Detail/TemplateParser/TemplateParserForSingleTemplate.cs
+++ src/TrueOrFalse.Frontend.Web/Views/Categories/Detail/TemplateParser/TemplateParserForSingleTemplate.cs
@@ -57,6 +57,7 @@ public class TemplateParserForSingleTemplate
             case "topicnavigation":
                 return new TopicNavigationModel(category, JsonConvert.DeserializeObject<TopicNavigationJson>(templateJson.OriginalJson));
             case "categorynetwork":
+                return new CategoryModel(category, loadKnowledgeSummary: false);
             case "inlinetext":
                 return new InlineTextModel(templateJson.InlineText);
             default:
