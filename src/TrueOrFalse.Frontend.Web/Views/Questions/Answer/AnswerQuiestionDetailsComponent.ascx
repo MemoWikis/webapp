bf735baa2b1cedbeedc36108af86492586883381
tj-mai <mai.t.tung@gmail.com>
2020-03-11 17:25:42 +0100

    wip statistics

---
+++ src/TrueOrFalse.Frontend.Web/Views/Questions/Answer/AnswerQuiestionDetailsComponent.ascx
@@ -0,0 +1,22 @@
+﻿<%@ Control Language="C#" Inherits="System.Web.Mvc.ViewUserControl<AnswerQuestionModel>" %>
+<%@ Import Namespace="TrueOrFalse.Frontend.Web.Code" %>
+
+<question-details-component>
+    
+    <div v-if="showTopBorder"></div>
+    <div id="questionDetailsContainer">
+        <div id="questionStatistics">
+            <div id="probabilityContainer">
+                <div id="semiPieChart"></div>
+                <div id="probabilityText">
+                    <div v-if="isLoggedIn"></div>
+                    <div v-else></div>
+                </div>
+                <div id="probabilityState" :class="state"></div>
+            </div>
+            <div id="counterContainer"></div>
+        </div>
+        
+        <div id="categoryList" v-if="showCategoryList"></div>
+    </div>
+</question-details-component>
---
+++ src/TrueOrFalse.Frontend.Web/Views/Questions/Answer/Js/InlineEditComponent.ts
@@ -0,0 +1,47 @@
+﻿Vue.component('inline-text-component', {
+    props: {
+        markdown: String,
+        id: String,
+    },
+
+    template: '#inlinetext-edit',
+
+    data() {
+        return {
+            parentId: this.$parent.id,
+            textContent: this.$parent.markdown,
+            textAreaId: this.$parent.textAreaId,
+            textCanBeEdited: this.$parent.textCanBeEdited,
+            textBeforeEdit: '',
+        };
+    },
+
+    watch: {
+        textContent: function() {
+            this.$parent.markdown = this.textContent;
+        },
+    },
+
+    mounted: function () {
+        this.$refs[this.textAreaId].$el.focus();
+        this.textBeforeEdit = this.textContent;
+    },
+
+    methods: {
+
+        applyNewMarkdown() {
+            if (this.textCanBeEdited) {
+                this.$parent.isListening = true;
+                Utils.ApplyMarkdown(this.textContent, this.parentId);
+            }
+        },
+
+        cancelTextEdit() {
+            this.textContent = this.textBeforeEdit;
+            this.$parent.markdown = this.textContent;
+            this.$parent.textCanBeEdited = false;
+            this.$parent.hoverState = false;
+        }
+    },
+});
+
