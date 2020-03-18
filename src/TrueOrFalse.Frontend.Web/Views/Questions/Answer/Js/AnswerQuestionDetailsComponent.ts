bb2d593a60cda0f704b6528120d504890b8acf1b
tj-mai <mai.t.tung@gmail.com>
2020-03-18 22:38:25 +0100

    wip update values and animation

--- src/TrueOrFalse.Frontend.Web/Views/Questions/Answer/Js/AnswerQuestionDetailsComponent.ts
+++ src/TrueOrFalse.Frontend.Web/Views/Questions/Answer/Js/AnswerQuestionDetailsComponent.ts
@@ -24,7 +24,7 @@ Vue.component('question-details-component', {
 
     watch: {
         personalProbability: function () {
-            this.reDrawSemiPie();
+            this.updateData();
         },
     },
 
@@ -33,13 +33,40 @@ Vue.component('question-details-component', {
     },
 
     methods: {
-        reDrawSemiPie() {
+
+        updateData() {
             var semiPieRef = this.$refs.semiPie;
-            var semiPie = d3.select(semiPieRef);
+            var semiPie = d3.select(semiPieRef).transition();
 
             var personalKnowledgeState = this.personalProbability / 100;
             var avgKnowledgeState = this.averageProbability / 100;
 
+            var arcKnowledgeState = d3.arc()
+                .innerRadius(45)
+                .outerRadius(60)
+                .startAngle(-0.5 * this.pi)
+                .endAngle((-0.5 + personalKnowledgeState) * this.pi);
+
+            var arcAvg = d3.arc()
+                .innerRadius(42)
+                .outerRadius(62)
+                .startAngle((-0.5 + avgKnowledgeState) * this.pi - 0.01)
+                .endAngle((-0.5 + avgKnowledgeState) * this.pi + 0.01);
+
+            semiPie.select(".arcPersonal")
+                .attr("d", arcKnowledgeState);
+
+            semiPie.select(".arcAvg")
+                .attr("d", arcAvg);
+        },
+
+        drawSemiPie() {
+
+            var semiPieRef = this.$refs.semiPie;
+            var semiPie = d3.select(semiPieRef).append("svg");
+
+            var personalKnowledgeState = this.personalProbability / 100;
+            var avgKnowledgeState = this.averageProbability / 100;
 
             var arcBase = d3.arc()
                 .innerRadius(50)
@@ -62,60 +89,19 @@ Vue.component('question-details-component', {
             semiPie.attr("width", "400").attr("height", "400")
                 .append("path")
                 .attr("d", arcBase)
-                .attr("fill", "grey")
-                .attr("transform", "translate(200,200)");
-
-            semiPie.enter.append("path")
-                .attr("d", arcKnowledgeState)
-                .attr("fill", "red")
-                .attr("transform", "translate(200,200)");
-
-            semiPie.enter.append("path")
-                .attr("d", arcAvg)
-                .attr("fill", "black")
-                .attr("transform", "translate(200,200)");
-        },
-        drawSemiPie() {
-
-            var semiPieRef = this.$refs.semiPie;
-            var semiPie = d3.select(semiPieRef).append("svg");
-
-            var personalKnowledgeState = this.personalProbability / 100;
-            var avgKnowledgeState = this.averageProbability / 100;
-
-            var endAngle = 0.5 * this.pi;
-
-            var arcBase = d3.arc()
-                .innerRadius(50)
-                .outerRadius(55)
-                .startAngle(-endAngle)
-                .endAngle(endAngle);
-
-            var arcKnowledgeState = d3.arc()
-                .innerRadius(45)
-                .outerRadius(60)
-                .startAngle(endAngle)
-                .endAngle(endAngle + personalKnowledgeState);
-
-            var arcAvg = d3.arc()
-                .innerRadius(42)
-                .outerRadius(62)
-                .startAngle(avgKnowledgeState * (this.pi / 180) - .5)
-                .endAngle(avgKnowledgeState * (this.pi / 180) + .5);
-
-            semiPie.attr("width", "400").attr("height", "400")
-                .append("path")
-                .attr("d", arcBase)
+                .attr("class", "arcBase")
                 .attr("fill", "grey")
                 .attr("transform", "translate(200,200)");
 
             semiPie.append("path")
                 .attr("d", arcKnowledgeState)
+                .attr("class", "arcPersonal")
                 .attr("fill", "red")
                 .attr("transform", "translate(200,200)");
 
             semiPie.append("path")
                 .attr("d", arcAvg)
+                .attr("class", "arcAvg")
                 .attr("fill", "black")
                 .attr("transform", "translate(200,200)");
         }
