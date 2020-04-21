793623c8cb00cc6a179d447688f262bd22cc8d2e
Robert Mischke <robertmischke@gmail.com>
2020-04-21 16:13:30 +0200

    removed signalR hub registration

--- src/TrueOrFalse.Frontend.Web/App_Start/Startup.cs
+++ src/TrueOrFalse.Frontend.Web/App_Start/Startup.cs
@@ -1,9 +1,4 @@
-﻿using System.Reflection;
-using Autofac;
-using Autofac.Integration.SignalR;
-using Microsoft.AspNet.SignalR;
-using Owin;
-using TrueOrFalse.Infrastructure;
+﻿using Owin;
 
 namespace TrueOrFalse
 {
@@ -11,18 +6,6 @@ namespace TrueOrFalse
     {
         public void Configuration(IAppBuilder app)
         {
-            var hubConfiguration = new HubConfiguration();
-            hubConfiguration.EnableDetailedErrors = true;
-
-            var container = AutofacWebInitializer.Run(
-                registerForAspNet: false, 
-                registerForSignalR: true,
-                assembly:Assembly.GetExecutingAssembly());
-            
-            hubConfiguration.Resolver = new AutofacDependencyResolver(container);
-
-            app.UseAutofacMiddleware(container);
-            app.MapSignalR(hubConfiguration);
         }
     }    
 }
