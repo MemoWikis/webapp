4b3bec573e00c5d7f9f38d06a62457a0fa94f2cf
dandor <daniel.majunke@gmx.de>
2020-04-26 12:00:36 +0200

    add LearningSessionNew.cs
    changed csproj, deleted file is in the old commit

---
+++ src/TrueOrFalse/Domain/Knowledge/Learning/LearningSessionNew/LearningSessionNew.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class LearningSessionNew
+{
+    public static List<Question> GetRandom(int amountQuestions, List<Question> questions)
+    {
+        if (amountQuestions > questions.Count || amountQuestions < 0)
+            return new List<Question>();
+
+        var result = new List<Question>();
+        var rand = new Random();
+        for (var i = 0; i < amountQuestions; i++)
+        {
+            var nextQuestion = rand.Next(0, questions.Count - 1);
+            questions.RemoveAt(nextQuestion);
+            result.Add(questions[nextQuestion]);
+        }
+
+        return result;
+    }
+}
