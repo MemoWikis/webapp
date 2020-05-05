96fc1a5e06259b6c33a8dd77fa4519e83bc8eadc
dandor <daniel.majunke@gmx.de>
2020-05-05 11:51:51 +0200

    Change Name

---
+++ src/TrueOrFalse.Tests/2 Domain/Question/LearnSession/Sort_by_probability.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace TrueOrFalse.Tests
+{
+    class Sort_by_probability : BaseTest
+    {
+        [Test]
+        public void Should_be_sorted()
+        {
+            ContextQuestion.PutQuestionIntoMemoryCache(answerProbability: 50);
+            ContextQuestion.PutQuestionIntoMemoryCache(answerProbability: 10);
+            ContextQuestion.PutQuestionIntoMemoryCache(answerProbability: 20);
+
+            var learningSession = LearningSessionNewCreator.ForLoggedInUser(new LearningSessionConfig {CategoryId = 0});
+            
+            //Todo: Get user probability
+            Assert.That(learningSession.Steps[0].Question.CorrectnessProbability, Is.EqualTo(10)) ;
+            Assert.That(learningSession.Steps[1].Question.CorrectnessProbability, Is.EqualTo(20));
+            Assert.That(learningSession.Steps[2].Question.CorrectnessProbability, Is.EqualTo(30));
+        }
+    }
+}
