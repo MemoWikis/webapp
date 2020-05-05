da5fa331efa8ab398f8192f32453189c4423e563
dandor <daniel.majunke@gmx.de>
2020-05-05 13:21:53 +0200

    Add Stepping.cs

---
+++ src/TrueOrFalse/Domain/Knowledge/Learning/LearningSessionNew/Ops/Stepping.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrueOrFalse.Domain.Knowledge.Learning.LearningSessionNew.Ops
+{
+    class Stepping
+    {
+        private List<LearningSessionStepNew> LearningSessionSteps;
+
+      public Stepping( List<LearningSessionStepNew> learningSessionSteps)
+        {
+            LearningSessionSteps = learningSessionSteps;
+        }
+      
+        private  void SetAnswerLearningSessionStepAnswerNew(int questionId,bool isAnswerCorrect)
+        {
+            LearningSessionSteps.ForEach(lss => { if (lss.Question.Id == questionId) lss.Answered = isAnswerCorrect; }); 
+        }
+
+    }
+}
