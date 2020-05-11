2e9e4e7694e7523d113d80e70bcf93f218df47a1
dandor <daniel.majunke@gmx.de>
2020-05-11 11:51:35 +0200

    Class moved to Ops

---
+++ src/TrueOrFalse/Domain/Knowledge/Learning/LearningSessionNew/Ops/LearningSessionNew.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using FluentNHibernate.Utils;
+
+public class LearningSessionNew
+{
+    public IList<LearningSessionStepNew> Steps;
+    public LearningSessionConfig Config;
+    public int Pager;
+
+
+    public int CurrentIndex { get; private set; }
+    public LearningSessionStepNew CurrentStep => Steps[CurrentIndex];
+
+    public int UserId;
+    public bool IsLoggedIn; 
+
+
+    public LearningSessionNew(List<LearningSessionStepNew> learningSessionSteps, LearningSessionConfig config)
+    {
+        Steps = learningSessionSteps;
+        UserId = config.UserId; 
+        IsLoggedIn =  UserId != -1;
+    }
+
+    public void AddAnswer(Answer answer)
+    {
+        CurrentStep.AnswerState = answer.AnsweredCorrectly() ? AnswerStateNew.Correct : AnswerStateNew.False;
+        
+        if(Config.ReAddStepsToEnd() && answer.AnsweredCorrectly())
+            ReAddCurrentStepToEnd();
+    }
+
+    public void NextStep()
+    {
+        CurrentIndex++; 
+        //var question = Steps[CurrentIndex].Question;
+        //return new AnswerBody(question);
+    }
+
+    public void SkipStep()
+    {
+        CurrentStep.AnswerState = AnswerStateNew.Skipped;
+
+        if (Config.ReAddStepsToEnd())
+            ReAddCurrentStepToEnd();
+
+        CurrentIndex++;
+    }
+
+    private void ReAddCurrentStepToEnd()
+    {
+        var step = new LearningSessionStepNew(CurrentStep.Question);
+        Steps.Add(step);
+    }
+}
