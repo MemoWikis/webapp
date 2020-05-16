8f25806d192dde4cf9ad9d151a0f69e214714ada
dandor <daniel.majunke@gmx.de>
2020-05-16 11:36:45 +0200

    Code clean

--- src/TrueOrFalse/Domain/Knowledge/Learning/LearningSessionNew/HelperClasses/LearningSessionConfig.cs
+++ src/TrueOrFalse/Domain/Knowledge/Learning/LearningSessionNew/HelperClasses/LearningSessionConfig.cs
@@ -1,14 +1,18 @@
-﻿public class LearningSessionConfig
+﻿using System;
+
+[Serializable]
+public class LearningSessionConfig
 {
-    public int CategoryId;
-    public int MaxQuestions;
-    public bool OnlyWuwi;
-    public int UserId;
+    public int CategoryId { get; set; }
+    public int MaxQuestions = 10; 
+    public bool OnlyWuwi { get; set; }
+    public int UserId { get; set; }
+    public bool IsInTestmode { get; set; }
+    public bool IsInLearningTab { get; set; } = false;
 
     /// <summary>
-    /// User is not logge in
+    /// User is not logged in
     /// </summary>
-    public bool IsAnonymous()=> UserId == -1;
-
+    public bool IsAnonymous() => UserId == -1;
     public bool ReAddStepsToEnd() => !IsAnonymous();
 }
