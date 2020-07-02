58b836da7785efced98817b92e98f1481b2e8b46
Daniel-Majunke <daniel.majunke@gmx.de>
2020-07-02 13:05:32 +0200

    add test Create LearningSession

---
+++ src/TrueOrFalse.Tests/2 Domain/Question/LearnSession/Create_learning_session.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace TrueOrFalse.Tests._2_Domain.Question.LearnSession
+{
+    class Create_learning_session : BaseTest
+    {
+        [Test]
+        public void GetCorrectProbabilityQuestions()
+        {
+            ContextQuestion.PutQuestionsIntoMemoryCache(20);
+            var learningSession = ContextLearningSession.GetLearningSession(
+                new LearningSessionConfig
+                {
+                    UserId = 0,
+                    MaxQuestions = 5,
+                    MaxProbability = 50,
+                    MinProbability = 10
+                });
+
+            foreach (var step in learningSession.Steps)
+            {
+                Assert.That(step.Question.CorrectnessProbability, Is.LessThan(51));
+                Assert.That(step.Question.CorrectnessProbability, Is.GreaterThan(9));
+            }
+        }
+    }
+}
