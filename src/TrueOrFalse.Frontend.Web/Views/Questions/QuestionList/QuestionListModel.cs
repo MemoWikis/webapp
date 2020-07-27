ee57f891081f95070b9dd28797d63cc18b91bd20
Daniel-Majunke <daniel.majunke@gmx.de>
2020-07-27 10:44:21 +0200

    fix page counter

--- src/TrueOrFalse.Frontend.Web/Views/Questions/QuestionList/QuestionListModel.cs
+++ src/TrueOrFalse.Frontend.Web/Views/Questions/QuestionList/QuestionListModel.cs
@@ -14,17 +14,9 @@ public class QuestionListModel : BaseModel
     public QuestionListModel(int categoryId)
     {
         CategoryId = categoryId;
-        var questionList = GetAllQuestions(categoryId);
-        AllQuestionCount = questionList.Count();
+        AllQuestionCount = Sl.SessionUser.LearningSession.Steps.Count;
     }
 
-    public static IList<Question> GetAllQuestions(int categoryId)
-    {
-        
-        var category = Sl.CategoryRepo.GetByIdEager(categoryId); 
-        return category.GetAggregatedQuestionsFromMemoryCache();
-    }
- 
     public static List<QuestionListJson.Question> PopulateQuestionsOnPage(int categoryId, int currentPage, int itemCount, bool isLoggedIn,int questionsSort = 2)
     {
         var allQuestions = Sl.SessionUser.LearningSession.Steps.Select(q => q.Question);
@@ -33,19 +25,6 @@ public class QuestionListModel : BaseModel
         ConcurrentDictionary<int, QuestionValuation> userQuestionValuation = new ConcurrentDictionary<int, QuestionValuation>(); 
         if(user != null)
              userQuestionValuation = UserCache.GetItem(user.Id).QuestionValuations;
-
-        switch (questionsSort)
-        {
-            case (int)QuestionSort.Alphabetical:
-                 allQuestions = allQuestions.OrderBy(q => q.Text).ToList();
-                break;
-            case (int)QuestionSort.Random:
-            case (int)QuestionSort.Knowledge:
-                break;
-            case (int)QuestionSort.Probability:
-                allQuestions = allQuestions.OrderBy(q => q.CorrectnessProbability).ToList();
-                break;
-        }
 
         var questionsOfCurrentPage = allQuestions.Skip(itemCount * (currentPage - 1)).Take(itemCount).ToList();
         var newQuestionList = new List<QuestionListJson.Question>();
@@ -60,22 +39,14 @@ public class QuestionListModel : BaseModel
 
             question.CorrectnessProbability = q.CorrectnessProbability;
 
-            if (user != null)
+            if (userQuestionValuation.ContainsKey(q.Id) && user != null)
             {
-                
-                
-                if (userQuestionValuation.ContainsKey(q.Id))
-                {
-                    question.CorrectnessProbability = userQuestionValuation[q.Id].CorrectnessProbability;
-                    question.IsInWishknowledge = userQuestionValuation[q.Id].IsInWishKnowledge();
-                    question.HasPersonalAnswer = userQuestionValuation[q.Id].CorrectnessProbabilityAnswerCount > 0;
-                }
+                question.CorrectnessProbability = userQuestionValuation[q.Id].CorrectnessProbability;
+                question.IsInWishknowledge = userQuestionValuation[q.Id].IsInWishKnowledge();
+                question.HasPersonalAnswer = userQuestionValuation[q.Id].CorrectnessProbabilityAnswerCount > 0;
             }
-
             newQuestionList.Add(question);
         }
-
-        //Logg.r().Error("QuestionListModel/PopulateQuestionsOnPage, unknown sorting function");
         return newQuestionList;
     }
 }
