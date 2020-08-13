0aed741e1640b0e4e7bbdbcbe202ac6c5a32bf4c
tj-mai <mai.t.tung@gmail.com>
2020-08-13 11:06:09 +0200

    added ignored js files

---
+++ src/TrueOrFalse.Frontend.Web/Scripts/npm_builder/tiptap-build/index.js
@@ -0,0 +1,6 @@
+window.tiptapBuild = {
+    tiptap: require("tiptap"),
+    tiptapUtils: require("tiptap-utils"),
+    tiptapCommands: require("tiptap-commands"),
+    tiptapExtensions: require("tiptap-extensions"),
+};
