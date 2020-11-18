e8c35b3a76bce67d1bf1ed1446019a05043cfa3b
Daniel-Majunke <daniel.majunke@gmx.de>
2020-11-18 13:40:31 +0100

    add method GetChildren add condition for test

--- src/TrueOrFalse/Tools/Cache/UserWorld/UserEntityCache.cs
+++ src/TrueOrFalse/Tools/Cache/UserWorld/UserEntityCache.cs
@@ -1,18 +1,22 @@
 ﻿using System;
 using Seedworks.Web.State;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
 using TrueOrFalse.Tools.Cache.UserWorld;
 
 public class UserEntityCache : BaseCache
 {
-    private static int _rootCategoryId = 1;
+    private static int _rootCategoryId = 1726; //RootKategorie
     public const int ExpirationSpanInMinutes = 600;
 
     private static string CategoriesCacheKey(int userId) => "Categories_" + userId;
+    private static List<string> CategoriesCacheKeyList = new List<string>();
 
-    public static void Init()
+    public static void Init(bool isTest = false)
     {
-        var user = Sl.SessionUser.User;
+        _rootCategoryId = isTest ? 1 : _rootCategoryId;
+       var user = Sl.SessionUser.User;
 
         var cacheItem = new UserEntityCacheItem()
         {
@@ -20,22 +24,17 @@ public class UserEntityCache : BaseCache
             Categories = new ConcurrentDictionary<int, Category>(GraphService
                 .GetAllPersonelCategoriesWithRealtions(_rootCategoryId).ToConcurrentDictionary())
         };
-        Cache.Add(CategoriesCacheKey(user.Id), cacheItem.Categories, TimeSpan.FromMinutes(ExpirationSpanInMinutes),true);
+        var categoriesCacheKey = CategoriesCacheKey(user.Id); 
+        Cache.Add(categoriesCacheKey, cacheItem.Categories, TimeSpan.FromMinutes(ExpirationSpanInMinutes),true);
+        CategoriesCacheKeyList.Add(categoriesCacheKey);
     }
 
-    public static UserEntityCacheItem GetUserWorldItem(int userId)
+    public static Category GetCategory(int categoryId, int userId)
     {
-        var categories = Cache.Get<ConcurrentDictionary<int, Category>>(CategoriesCacheKey(userId));
-        var user = Sl.UserRepo.GetById(userId);
+        if (!CategoriesCacheKeyList.Contains(CategoriesCacheKey(userId)))
+            Init();
 
-        var result = new UserEntityCacheItem
-        {
-            User = user ?? Sl.UserRepo.GetById(userId),
-            Categories = categories ?? new ConcurrentDictionary<int, Category>(GraphService
-                .GetAllPersonelCategoriesWithRealtions(_rootCategoryId) 
-                .ToConcurrentDictionary())
-        };
-        return result;
+        return Cache.Get<ConcurrentDictionary<int, Category>>(CategoriesCacheKey(userId))[categoryId]; ;
     }
 
     public static ConcurrentDictionary<int, Category> GetCategories(int userId)
@@ -48,6 +47,18 @@ public class UserEntityCache : BaseCache
     {
         Cache.Remove(CategoriesCacheKey(userId));
     }
+
+    public static List<Category> GetChildren(int categoryId, int userId)
+    {
+        var category = GetCategory(categoryId,userId);
+
+        var allCategories = UserEntityCache.GetCategories(userId).Values.ToList();
+
+        return allCategories.SelectMany(c =>
+            c.CategoryRelations.Where(cr => cr.CategoryRelationType == CategoryRelationType.IsChildCategoryOf && cr.RelatedCategory.Id == category.Id)
+                .Select(cr => cr.Category)).ToList();
+    }
+
 }
 
 
