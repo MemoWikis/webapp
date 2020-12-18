a53286ecd7b2f0d1d8448d851791ed4e513d56aa
Daniel-Majunke <daniel.majunke@gmx.de>
2020-12-18 22:44:33 +0100

    Add all topics

--- src/TrueOrFalse/Tools/Update/Steps/UpdateToVs217.cs
+++ src/TrueOrFalse/Tools/Update/Steps/UpdateToVs217.cs
@@ -11,7 +11,7 @@ namespace TrueOrFalse.Updates
         {
             Sl.Resolve<ISession>()
                 .CreateSQLQuery(
-                    @"INSERT INTO Category (Id,Name,Content,DateCreated,DateModified,Type,Creator_id,SkipMigration)Values(1,'Alle Themen','[{\'TemplateName\':\'TopicNavigation\',\'Load\':\'689\'},{\'TemplateName\':\'TopicNavigation\',\'Load\':\'682\'},{\'TemplateName\':\'TopicNavigation\',\'Load\':\'709\'},{\'TemplateName\':\'TopicNavigation\',\'Load\':\'687\'}]','2020-12-18 09:11:59','2020-12-18 09:11:59',1,445,1)")
+                    @"INSERT INTO Category (Id,Name,Content,DateCreated,DateModified,Type,Creator_id,SkipMigration)Values(1,'Alle Themen','[{\'TemplateName\':\'TopicNavigation\',\'Load\':\'689\'},{\'TemplateName\':\'TopicNavigation\',\'Load\':\'682\'},{\'TemplateName\':\'TopicNavigation\',\'Load\':\'709\'},{\'TemplateName\':\'TopicNavigation\',\'Load\':\'687\'},{\'TemplateName\':\'TopicNavigation\',\'Load\':\'All\'}]','2020-12-18 09:11:59','2020-12-18 09:11:59',1,445,1)")
                 .ExecuteUpdate();
 
             var categories = Sl.CategoryRepo.GetAllEager();
