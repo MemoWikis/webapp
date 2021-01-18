3e9c915e37a34a0367776677afe3efeee553403a
tj-mai <mai.t.tung@gmail.com>
2021-01-18 12:38:58 +0100

    refactor

--- src/TrueOrFalse.Frontend.Web/Views/Categories/Detail/Partials/Segmentation/SegmentationModel.cs
+++ src/TrueOrFalse.Frontend.Web/Views/Categories/Detail/Partials/Segmentation/SegmentationModel.cs
@@ -10,7 +10,7 @@ public class SegmentationModel : BaseContentModule
     public string Text;
 
     public List<Category> CategoryList;
-    public List<Category> UnsortedCategoryList;
+    public List<Category> NotInSegmentCategoryList;
     public List<Segment> Segments;
 
     public SegmentationModel()
@@ -24,20 +24,20 @@ public class SegmentationModel : BaseContentModule
         var categoryList = UserCache.IsFiltered ? UserEntityCache.GetChildren(category.Id, UserId) : EntityCache.GetChildren(category.Id).ToList();
         CategoryList = categoryList.Where(c => c.Type.GetCategoryTypeGroup() == CategoryTypeGroup.Standard).ToList();
 
-        var sortedCategories = new List<Category>();
+        var inSegmentCategoryList = new List<Category>();
         if (!IsLoggedIn)
         {
             if (Segments != null)
                 foreach (var segment in Segments)
                 {
-                    var categoriesToAdd = segment.CategoryList.Where(c => !sortedCategories.Any(s => s.Id == c.Id));
+                    var categoriesToAdd = segment.CategoryList.Where(c => !inSegmentCategoryList.Any(s => s.Id == c.Id));
                     foreach (var c in categoriesToAdd)
-                       sortedCategories.Add(c);
-                }       
+                        inSegmentCategoryList.Add(c);
+                }
 
-            UnsortedCategoryList = categoryList.Where(c => !sortedCategories.Any(s => c.Id == s.Id)).ToList();
+            NotInSegmentCategoryList = categoryList.Where(c => !inSegmentCategoryList.Any(s => c.Id == s.Id)).ToList();
         } else
-            UnsortedCategoryList = categoryList;
+            NotInSegmentCategoryList = categoryList;
     }
 }
 
