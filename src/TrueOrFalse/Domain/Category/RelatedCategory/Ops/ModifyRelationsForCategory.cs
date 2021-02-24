15a4e340f14d247453846ef0c53bf8a1e540c712
Daniel-Majunke <daniel.majunke@gmx.de>
2021-02-24 11:24:03 +0100

    change ModifyRelationsForCategory and add tests

--- src/TrueOrFalse/Domain/Category/RelatedCategory/Ops/ModifyRelationsForCategory.cs
+++ src/TrueOrFalse/Domain/Category/RelatedCategory/Ops/ModifyRelationsForCategory.cs
@@ -22,13 +22,20 @@ public class ModifyRelationsForCategory
             .Except(existingRelationsOfType.Select(r => r.RelatedCategory))
             .Select(c => new CategoryRelation{Category = category, RelatedCategory = c, CategoryRelationType = relationType});
 
-        var relationsToRemove = existingRelationsOfType.Where(r => relatedCategories.All(c => c != r.RelatedCategory)).ToList();
+        var relationsToRemove =  existingRelationsOfType.ToConcurrentDictionary();
 
+        foreach (var categoryRelation in relatedCategories)
+            if (relationsToRemove.ContainsKey(categoryRelation.Id))
+            {
+                CategoryRelation cr; 
+                relationsToRemove.TryRemove(categoryRelation.Id, out cr);
+            }
+        
         foreach (var relation in relationsToAdd)
             category.CategoryRelations.Add(relation);
 
         foreach (var relation in relationsToRemove)
-            category.CategoryRelations.Remove(relation);
+            category.CategoryRelations.Remove(relation.Value);
     }
 
     public static void AddCategoryRelationOfType(Category category, Category relatedCategory, CategoryRelationType relationType)
