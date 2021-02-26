b7dd2707632737e7c34782116bb5fe0c8f59d9e4
tj-mai <mai.t.tung@gmail.com>
2021-02-26 13:08:49 +0100

    wip migrationtests

---
+++ src/TrueOrFalse.Tests/3 Infrastructure/TopicNavigationMigrationTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+
+[TestFixture]
+class TopicNavigationMigrationTest
+{
+    //[Test]
+    //public void RemoveTopicNavigation()
+    //{
+    //    var testString = "[{\"TemplateName\":\"TopicNavigation\"}]";
+    //    var testString2 = "[{\"TemplateName\":\"TopicNavigation\",\"Load\":\"All\"}]";
+    //    var expectedResult = "";
+
+    //    var tokens = Tokenizer.Run(testString);
+
+    //    var newString = "";
+    //    foreach (TemplateJson token in tokens)
+    //    {
+    //        if (token.TemplateName == "InlineText")
+    //            newString += token.OriginalJson;
+    //    }
+    //}
+
+    //[Test]
+    //public void RemoveTopicNavigationEndingWithInlineText()
+    //{
+    //    var testString = "[{\"TemplateName\":\"TopicNavigation\"},{ \"TemplateName\":\"InlineText\",\"Content\":\"<h2 id=\"weiterfuhrende-links\">Weiterführende Links</h2>\n<p><a href=\"https://de.wikipedia.org/wiki/Containervirtualisierung\">https://de.wikipedia.org/wiki/Containervirtualisierung</a></p>\n\"}]";
+    //    var expectedResult = "[{ \"TemplateName\":\"InlineText\",\"Content\":\"<h2 id=\"weiterfuhrende-links\">Weiterführende Links</h2>\n<p><a href=\"https://de.wikipedia.org/wiki/Containervirtualisierung\">https://de.wikipedia.org/wiki/Containervirtualisierung</a></p>\n\"}]";
+
+    //}
+}
+
---
+++ src/TrueOrFalse/Tools/Update/TemplateMigration/TopicNavigationMigration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMigration
+{
+    class TopicNavigationMigration
+    {
+        public void RemoveTopicNavigation(string categoryContent)
+        {
+
+        }
+    }
+}
