59b8f9ee14fdd638cfe04bc04923d7dcd9276cf5
Robert Mischke <robertmischke@gmail.com>
2021-03-01 01:30:53 +0100

    Revert "in order to debug errors reading the app.config.overwritten"
    This reverts commit a3895472509cdfca77b2f7a504b1ecbacfc16add.
    
    # Conflicts:
    #	src/TrueOrFalse/Infrastructure/Settings/OverwrittenConfig/OverwrittenConfig.cs

--- src/TrueOrFalse/Infrastructure/Settings/OverwrittenConfig/OverwrittenConfig.cs
+++ src/TrueOrFalse/Infrastructure/Settings/OverwrittenConfig/OverwrittenConfig.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.IO;
+using System.Reflection;
+using System.Web;
+using System.Web.Configuration;
 using System.Xml.Linq;
 using Seedworks.Web.State;
-using Serilog;
 
 public static class OverwrittenConfig
 {
+    
+
     public static string ValueString(string itemName)
     {
         var result = Value(itemName);
@@ -23,8 +27,6 @@ public static class OverwrittenConfig
         string filePath = ContextUtil.GetFilePath(
             ContextUtil.IsWebContext || ContextUtil.UseWebConfig ? "Web.overwritten.config" : "App.overwritten.config"
         );
-
-        Log.Information("{isWebContext} {filePath}", ContextUtil.IsWebContext, filePath);
 
         if (!File.Exists(filePath))
             return new OverwrittenConfigValueResult(false, null);
