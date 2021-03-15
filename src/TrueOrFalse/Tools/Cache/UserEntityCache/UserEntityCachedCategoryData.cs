f1608840054b1b14251ae99a17d9729429dc70bd
Daniel-Majunke <daniel.majunke@gmx.de>
2021-03-15 09:57:46 +0100

    Add UserEntityCachedCategoryData

---
+++ src/TrueOrFalse/Tools/Cache/UserEntityCache/UserEntityCachedCategoryData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin.Security.Provider;
+
+
+[Serializable]
+public class UserEntityCachedCategoryData
+{
+    public IList<UserEntityCacheCategoryItem> TotalAggregatedChildren { get; set; } = new List<UserEntityCacheCategoryItem>();
+    public IList<UserEntityCacheCategoryItem> Children { get; set; } = new List<UserEntityCacheCategoryItem>();
+
+    public UserEntityCachedCategoryData ToCachedCategoryData(CategoryCachedData categoryCachedData)
+    {
+        var uECCI = new UserEntityCacheCategoryItem();
+        TotalAggregatedChildren = uECCI.ToCacheCategoryItem( categoryCachedData.TotalAggregatedChildren)
+    }
+}
+
+
+
+
