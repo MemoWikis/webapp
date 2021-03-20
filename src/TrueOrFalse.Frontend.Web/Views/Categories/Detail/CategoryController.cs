83b8760d4ad9bbaccdfbd3a0d2edc9bf8dea8269
Daniel-Majunke <daniel.majunke@gmx.de>
2021-03-20 14:37:34 +0100

    Revert "Delete setRedirect"
    This reverts commit 7a7a78d6066ca1115af2312251307d50db92b2f1.

--- src/TrueOrFalse.Frontend.Web/Views/Categories/Detail/CategoryController.cs
+++ src/TrueOrFalse.Frontend.Web/Views/Categories/Detail/CategoryController.cs
@@ -140,7 +140,21 @@ public class CategoryController : BaseController
         return Redirect(Links.LearningSession(learningSession));
     }
 
-    [HttpPost]
+    [RedirectToErrorPage_IfNotLoggedIn]
+    public ActionResult StartLearningSessionForSets(List<int> setIds, string setListTitle)
+    {
+        var sets = Sl.SetRepo.GetByIds(setIds);
+        var questions = sets.SelectMany(s => s.Questions()).Distinct().ToList();
+
+        if (questions.Count == 0)
+            throw new Exception("Cannot start LearningSession with 0 questions.");
+        var config = new LearningSessionConfig();
+        var learningSession = LearningSessionCreator.ForAnonymous(config);
+
+        return Redirect(Links.LearningSession(learningSession));
+    }
+
+   [HttpPost]
     public string Tab(string tabName, int categoryId)
     {
         var categoryCacheItem = EntityCache.GetCategoryCacheItem(categoryId);
---
+++ src/TrueOrFalse/Domain/QuestionSet/Set.Persistence/SetRepo.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NHibernate;
+using NHibernate.Criterion;
+using NHibernate.Transform;
+
+public class SetRepo : RepositoryDbBase<Set>
+{
+    public SetRepo(
+        ISession session)
+        : base(session)
+    {
+    }
+
+    public IList<Set> GetByIds(List<int> setIds)
+    {
+        return GetByIds(setIds.ToArray());
+    }
+
+    public override IList<Set> GetByIds(params int[] setIds)
+    {
+        var resultTmp = base.GetByIds(setIds);
+        var result = new List<Set>();
+        for (int i = 0; i < setIds.Length; i++)
+        {
+            if (resultTmp.Any(c => c.Id == setIds[i]))
+                result.Add(resultTmp.First(c => c.Id == setIds[i]));
+        }
+        return result;
+    }
+
+    public IList<Set> GetByCreatorId(int creatorId)
+    {
+        return _session.QueryOver<Set>()
+            .Where(c => c.Creator.Id == creatorId)
+            .List<Set>();
+    }
+
+    public IList<Set> GetForCategory(int categoryId)
+    {
+        return _session.QueryOver<Set>()
+            .JoinQueryOver<Category>(q => q.Categories)
+            .Where(c => c.Id == categoryId)
+            .List<Set>();
+    }
+
+    public Set GetByIdEager(int setId)
+    {
+        //Question creatorAlias = null;
+
+        return _session.QueryOver<Set>()
+            .Where(set => set.Id == setId)
+            .Left.JoinQueryOver<QuestionInSet>(s => s.QuestionsInSet)
+            .Left.JoinQueryOver(s => s.Question)
+            .Left.JoinQueryOver<Category>(s => s.Categories)
+            .SingleOrDefault();
+    }
+
+    public IList<Set> GetByIdsEager(int[] setIds = null)
+    {
+        var query = _session.QueryOver<Set>();
+
+        if (setIds != null)
+            query = query.Where(Restrictions.In("Id", setIds));
+
+        return query.Left.JoinQueryOver<QuestionInSet>(s => s.QuestionsInSet)
+            .Left.JoinQueryOver(s => s.Question)
+            .Left.JoinQueryOver<Category>(s => s.Categories)
+            .List()
+            .GroupBy(s => s.Id)
+            .Select(s => s.First())
+            .ToList();
+    }
+
+    public IList<Set> GetAllEager() => GetByIdsEager();
+
+
+    public IEnumerable<Set> GetMostRecent_WithAtLeast3Questions(int amount)
+    {
+        string query = $@"SELECT s.* FROM questionset s
+            LEFT JOIN questioninset qs
+            ON s.Id = qs.Set_Id 
+            GROUP BY s.Id
+            Having Count(qs.Set_Id )  > 3
+            ORDER BY s.DateCreated DESC
+            LIMIT {amount}";
+
+        return _session
+            .CreateSQLQuery(query)
+            .AddEntity(typeof(Set))
+            .List<Set>();
+    }
+
+    public IEnumerable<TopSetResult> GetMostQuestions(int amount)
+    {
+        return _session.CreateSQLQuery(
+            "SELECT QCount, Set_id as SetId, Name, Text FROM " +
+            "(SELECT count(questioninset.Question_id) AS QCount, questioninset.Set_id " +
+            "FROM questioninset GROUP BY Set_id ORDER BY QCount DESC " +
+            "LIMIT "+ amount +") AS qis_r " +
+            "LEFT JOIN questionset ON qis_r.Set_id = questionset.Id")
+            .SetResultTransformer(Transformers.AliasToBean(typeof(TopSetResult)))
+            .List<TopSetResult>().ToList();
+    }
+
+    public override void Delete(Set set)
+    {
+        //if (Sl.R<SessionUser>().IsLoggedInUserOrAdmin(set.Creator.Id))
+        //    throw new InvalidAccessException(); //exception is thrown, even if admin (=creator of set) is logged in
+
+        base.Delete(set);
+        foreach (var category in set.Categories)
+        {
+            category.UpdateCountQuestionsAggregated();
+        }
+        Flush();
+    }
+
+    public int HowManyNewSetsCreatedSince(DateTime since)
+    {
+        return _session.QueryOver<Set>()
+            .Where(s => s.DateCreated > since)
+            .RowCount();
+    }
+
+    public int TotalSetCount()
+    {
+        return _session.QueryOver<Set>()
+            .RowCount();
+    }
+
+    public string GetYoutbeUrl(int setId) => 
+        _session.Query<Set>()
+            .Where(s => s.Id == setId)
+            .Select(s => s.VideoUrl)
+            .FirstOrDefault();
+}
