3b12b1cecfbe2e71d7f699aeadc2bef290743263
Daniel-Majunke <daniel.majunke@gmx.de>
2021-04-20 15:43:48 +0200

    code format

--- src/TrueOrFalse.Frontend.Web/Views/Api/CategoryApiModel.cs
+++ src/TrueOrFalse.Frontend.Web/Views/Api/CategoryApiModel.cs
@@ -1,26 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
-namespace TrueOrFalse.View.Web.Views.Api
+public class CategoryApiModel : BaseModel
 {
-    public class CategoryApiModel: BaseModel
+    public bool Pin(int categoryId) => Pin(categoryId.ToString());
+    public bool Pin(string categoryId)
     {
-        public bool Pin(int categoryId) => Pin(categoryId.ToString()); 
-        public  bool Pin(string categoryId)
-        {
-            if (_sessionUser.User == null)
-                return false;
+        if (_sessionUser.User == null)
+            return false;
 
-            CategoryInKnowledge.Pin(Convert.ToInt32(categoryId), _sessionUser.User);
-            if (UserCache.GetItem(_sessionUser.UserId).IsFiltered)
-            {
-                UserEntityCache.DeleteCacheForUser();
-                UserEntityCache.Init();
-            }
-            
-            return true;
+        CategoryInKnowledge.Pin(Convert.ToInt32(categoryId), _sessionUser.User);
+        if (UserCache.GetItem(_sessionUser.UserId).IsFiltered)
+        {
+            UserEntityCache.DeleteCacheForUser();
+            UserEntityCache.Init();
         }
+
+        return true;
     }
 }
