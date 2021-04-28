3265993049906f333063fb48c559db4b57b12351
Daniel-Majunke <daniel.majunke@gmx.de>
2021-04-28 14:22:12 +0200

    add CategoryDeleter to ServiceLocator

--- src/TrueOrFalse/Infrastructure/IoC/ServiceLocator.cs
+++ src/TrueOrFalse/Infrastructure/IoC/ServiceLocator.cs
@@ -24,6 +24,7 @@ public class Sl
     public static CategoryValuationRepo CategoryValuationRepo => R<CategoryValuationRepo>();
     public static CategoryViewRepo CategoryViewRepo => R<CategoryViewRepo>();
     public static CategoryChangeRepo CategoryChangeRepo => R<CategoryChangeRepo>();
+    public static CategoryDeleter CategoryDeleter => R<CategoryDeleter>();
 
     public static SetViewRepo SetViewRepo => R<SetViewRepo>();
     public static SetRepo SetRepo => R<SetRepo>();
