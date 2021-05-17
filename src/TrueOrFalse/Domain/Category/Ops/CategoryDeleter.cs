79fcbe078c9ff5eaed3ac58f1dae8bccd4810781
Daniel-Majunke <daniel.majunke@gmx.de>
2021-05-17 14:08:37 +0200

    fix delete Category

--- src/TrueOrFalse/Domain/Category/Ops/CategoryDeleter.cs
+++ src/TrueOrFalse/Domain/Category/Ops/CategoryDeleter.cs
@@ -21,6 +21,11 @@ public class CategoryDeleter : IRegisterAsInstancePerLifetime
             return;
         }
 
+        _session.CreateSQLQuery("DELETE FROM relatedcategoriestorelatedcategories where Related_id = " + category.Id).ExecuteUpdate();
+        _session.CreateSQLQuery("DELETE FROM relatedcategoriestorelatedcategories where Category_id = " + category.Id).ExecuteUpdate();
+        _session.CreateSQLQuery("DELETE FROM categories_to_questions where Category_id = " + category.Id).ExecuteUpdate();
+        _session.CreateSQLQuery("DELETE FROM categories_to_sets where Category_id = " + category.Id).ExecuteUpdate();
+
         if (!forSetMigration)
             ThrowIfNot_IsLoggedInUserOrAdmin.Run(category.Creator.Id);
 
@@ -40,9 +45,5 @@ public class CategoryDeleter : IRegisterAsInstancePerLifetime
         EntityCache.Remove(categoryCacheItem);
         UserCache.RemoveAllForCategory(category.Id);
 
-        _session.CreateSQLQuery("DELETE FROM relatedcategoriestorelatedcategories where Related_id = " + category.Id).ExecuteUpdate();
-        _session.CreateSQLQuery("DELETE FROM relatedcategoriestorelatedcategories where Category_id = " + category.Id).ExecuteUpdate();
-        _session.CreateSQLQuery("DELETE FROM categories_to_questions where Category_id = " + category.Id).ExecuteUpdate();
-        _session.CreateSQLQuery("DELETE FROM categories_to_sets where Category_id = " + category.Id).ExecuteUpdate();
     }
 }
