18060681535ecabb241a9233767bf500776fc519
Daniel-Majunke <daniel.majunke@gmx.de>
2021-05-25 10:13:34 +0200

    remove setMigration

--- src/TrueOrFalse/Domain/Category/Ops/CategoryDeleter.cs
+++ src/TrueOrFalse/Domain/Category/Ops/CategoryDeleter.cs
@@ -25,15 +25,11 @@ public class CategoryDeleter : IRegisterAsInstancePerLifetime
         _session.CreateSQLQuery("DELETE FROM categories_to_questions where Category_id = " + category.Id).ExecuteUpdate();
         _session.CreateSQLQuery("DELETE FROM categories_to_sets where Category_id = " + category.Id).ExecuteUpdate();
 
-        if (!forSetMigration)
-            ThrowIfNot_IsLoggedInUserOrAdmin.Run(category.Creator.Id);
 
-        Sl.UserActivityRepo.DeleteForCategory(category.Id);
+        ThrowIfNot_IsLoggedInUserOrAdmin.Run(category.Creator.Id);
 
-        if (forSetMigration)
-            Sl.CategoryRepo.DeleteWithoutFlush(category);
-        else
-            Sl.CategoryRepo.Delete(category);
+        Sl.UserActivityRepo.DeleteForCategory(category.Id); 
+        Sl.CategoryRepo.Delete(category);
 
         Sl.CategoryChangeRepo.AddDeleteEntry(category);
         Sl.CategoryValuationRepo.DeleteCategoryValuation(category.Id);
