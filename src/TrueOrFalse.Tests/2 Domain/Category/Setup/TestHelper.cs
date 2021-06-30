0c17c7a3b740c7ef1f1b376c076e553cb10d5ec3
Daniel-Majunke <daniel.majunke@gmx.de>
2021-06-30 10:33:34 +0200

    Add tests EditCategoryController

---
+++ src/TrueOrFalse.Tests/2 Domain/Category/Setup/TestHelper.cs
@@ -0,0 +1,21 @@
+﻿
+
+using System.Linq;
+
+class TestHelper
+    {
+        public static bool hasRelation(Category category, int categoryId, int relatedId, CategoryRelationType categoryRelationType)
+        {
+            return category.CategoryRelations.Any(c =>
+                c.Category.Id == categoryId && c.RelatedCategory.Id == relatedId &&
+                c.CategoryRelationType == categoryRelationType);
+        }
+
+        public static bool hasRelation(CategoryCacheItem category, int categoryId, int relatedId, CategoryRelationType categoryRelationType)
+        {
+            return category.CategoryRelations.Any(c =>
+                c.CategoryId == categoryId && c.RelatedCategoryId== relatedId &&
+                c.CategoryRelationType == categoryRelationType);
+        }
+}
+
