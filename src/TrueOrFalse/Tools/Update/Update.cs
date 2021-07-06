ec3e86d5115ae1377485b6114af565e829b441ff
Daniel-Majunke <daniel.majunke@gmx.de>
2021-07-06 15:21:05 +0200

    correct order update steps

--- src/TrueOrFalse/Tools/Update/Update.cs
+++ src/TrueOrFalse/Tools/Update/Update.cs
@@ -147,14 +147,14 @@
                 .Add(UpdateToVs214.Run)
                 .Add(UpdateToVs215.Run)
                 .Add(UpdateToVs216.Run)
+                .Add(UpdateToVs221.Run)
+                .Add(UpdateToVs219.Run)
+                .Add(UpdateToVs222.Run)
+                .Add(UpdateToVs225.Run)
                 .Add(UpdateToVs217.Run)
                 .Add(UpdateToVs218.Run)
-                .Add(UpdateToVs219.Run)
-                .Add(UpdateToVs221.Run)
-                .Add(UpdateToVs222.Run)
                 .Add(UpdateToVs223.Run)
                 .Add(UpdateToVs224.Run)
-                .Add(UpdateToVs225.Run)
                 .Add(UpdateToVs226.Run)
                 .Run();
         }
