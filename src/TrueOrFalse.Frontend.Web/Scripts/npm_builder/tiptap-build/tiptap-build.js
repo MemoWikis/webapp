441dde60c9ee8f7e09f8fa2037e908cce3fbf9c7
Jun <mai.t.tung@gmail.com>
2021-07-09 18:09:02 +0200

    bundling tiptap with webpack, build working tiptap core and starterkit

---
+++ src/TrueOrFalse.Frontend.Web/Scripts/npm_builder/tiptap-build/tiptap-build.js
@@ -0,0 +1,7 @@
+
+import {Editor, EditorContent} from '@tiptap/vue-2'
+import StarterKit from '@tiptap/starter-kit'
+
+window.Editor = Editor;
+window.EditorContent = EditorContent;
+window.StarterKit = StarterKit;
