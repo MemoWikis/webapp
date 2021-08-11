7b9bf312e191dd8555602280e635b75c0a499597
Robert Mischke <robertmischke@gmail.com>
2021-08-11 22:08:49 +0200

    removed unused set code

--- src/TrueOrFalse.Frontend.Web/App_Start/RouteConfig.cs
+++ src/TrueOrFalse.Frontend.Web/App_Start/RouteConfig.cs
@@ -117,7 +117,6 @@ namespace TrueOrFalse
             routes.MapRoute("ApiExport", "Api/Export/{action}", new { controller = "Export", action = "Export" });
             routes.MapRoute("ApiCategory", "Api/Category/{action}", new { controller = "CategoryApi" });
             routes.MapRoute("ApiCategoryStatistics", "Api/CategoryStatistics/{action}", new { controller = "CategoryStatisticsApi" });
-            routes.MapRoute("ApiSets", "Api/Sets/{action}", new { controller = "SetsApi" });
             routes.MapRoute("ApiUserFacebook", "Api/FacebookUsers/{action}", new { controller = "FacebookUsersApi" });
             routes.MapRoute("ApiUserGoogle", "Api/GoogleUsers/{action}", new { controller = "GoogleUsersApi" });
             routes.MapRoute("ApiQuestions", "Api/Questions/{action}", new { controller = "QuestionsApi" });
