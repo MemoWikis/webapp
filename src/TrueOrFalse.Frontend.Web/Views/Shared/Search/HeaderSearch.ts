90dd089252286db19df6266905d9b4b688866824
Jun <mai.t.tung@gmail.com>
2021-09-19 21:09:51 +0200

    resize test stage

--- src/TrueOrFalse.Frontend.Web/Views/Shared/Search/HeaderSearch.ts
+++ src/TrueOrFalse.Frontend.Web/Views/Shared/Search/HeaderSearch.ts
@@ -9,14 +9,16 @@
     },
     created() {
         this.init();
-
-        //var self = this;
-        //window.addEventListener("resize", self.init);
     },
-    //destroyed() {
-    //    var self = this;
-    //    window.removeEventListener("resize", self.init);
-    //},
+
+    mounted() {
+        var self = this;
+        window.addEventListener("resize", self.init);
+    },
+    destroyed() {
+        var self = this;
+        window.removeEventListener("resize", self.init);
+    },
     watch: {
         showSearch(val) {
             let logoContainer = $('#LogoContainer');
