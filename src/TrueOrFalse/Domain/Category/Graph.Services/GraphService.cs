e69fa8da42592d66fc6b360102f637ca982c7fbd
Daniel-Majunke <daniel.majunke@gmx.de>
2021-09-20 12:45:55 +0200

    rename and fix null reference Exception if get direct parents from UserEntityCache

--- src/TrueOrFalse/Domain/Category/Graph.Services/GraphService.cs
+++ src/TrueOrFalse/Domain/Category/Graph.Services/GraphService.cs
@@ -11,7 +11,7 @@ public class GraphService
 
     private static IList<CategoryCacheItem> GetAllParentsFromEntityCache(CategoryCacheItem category)
     {
-        var parentIds = GetDirectParents(category);
+        var parentIds = GetDirectParentIds(category);
         var allParents = new List<CategoryCacheItem>();
         var deletedIds = new Dictionary<int, int>();
 
@@ -24,7 +24,7 @@ public class GraphService
                 allParents.Add(parent);//Avoidance of circular references
 
                 deletedIds.Add(parentIds[0], parentIds[0]);
-                var currentParents = GetDirectParents(parent);
+                var currentParents = GetDirectParentIds(parent);
                 foreach (var currentParent in currentParents)
                 {
                     parentIds.Add(currentParent);
@@ -40,7 +40,10 @@ public class GraphService
        var userCache =  UserEntityCache.GetUserCache(userId);
 
        userCache.TryGetValue(category.Id, out var userEntityCacheItem);
-       var parentIds = GetDirectParents(userEntityCacheItem);
+       var parentIds=EntityCache.GetCategoryCacheItems(GetDirectParentIds(userEntityCacheItem))
+           .Where(cci => cci.IsInWishknowledge())
+           .Select(cci => cci.Id).ToList();
+
        var allParents = new List<CategoryCacheItem>();
        var deletedIds = new Dictionary<int, int>();
 
@@ -53,7 +56,7 @@ public class GraphService
                allParents.Add(parent);//Avoidance of circular references
 
                deletedIds.Add(parentIds[0], parentIds[0]);
-               var currentParents = GetDirectParents(parent);
+               var currentParents = GetDirectParentIds(parent);
                foreach (var currentParent in currentParents)
                {
                    parentIds.Add(currentParent);
@@ -64,7 +67,7 @@ public class GraphService
        return allParents;
     }
 
-    public static List<int> GetDirectParents(CategoryCacheItem category)
+    public static List<int> GetDirectParentIds(CategoryCacheItem category)
     {
         var relations = new List<int>(); 
         foreach (var relation in category.CategoryRelations)
