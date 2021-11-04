b8de36551a1ade28ded27c26089cf57066338ad9
Jun <mai.t.tung@gmail.com>
2021-11-04 08:47:29 +0100

    show name in breadcrumb, refactor

---
+++ src/TrueOrFalse.Frontend.Web/Views/Categories/Detail/Partials/BreadCrumbModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using TrueOrFalse.Web;
+
+public class BreadCrumbModel : BaseModel
+{
+    public User User { get; set; }
+    public string UserImage = "";
+    public string UserName = "";
+    public string ToolTipToHomepage = "Zur Startseite";
+    public BreadCrumbModel()
+    {
+        User = _sessionUser.User;
+        if (IsLoggedIn)
+        {
+            UserName = _sessionUser.User.Name;
+            var imageSetttings = new UserImageSettings(_sessionUser.User.Id);
+            UserImage = imageSetttings.GetUrl_30px_square(_sessionUser.User).Url;
+            ToolTipToHomepage = "Zu deinem Wiki";
+        }
+    }
+}
