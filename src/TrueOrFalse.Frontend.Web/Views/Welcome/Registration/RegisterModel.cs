19a9bd253f5a880c9a9db2272c9fdc74d17e40cb
Jun <mai.t.tung@gmail.com>
2021-12-07 12:04:36 +0100

    register page fixes for browser autofill recognition

--- src/TrueOrFalse.Frontend.Web/Views/Welcome/Registration/RegisterModel.cs
+++ src/TrueOrFalse.Frontend.Web/Views/Welcome/Registration/RegisterModel.cs
@@ -9,12 +9,12 @@ public class RegisterModel : BaseModel
     [Required(ErrorMessage="Wir benötigen einen Benutzernamen für dich.")]
     [StringLength(40, MinimumLength=2, ErrorMessage = "Minimum 2 Zeichen, Maximum 40 Zeichen.")]
     [DisplayName("Benutzername")]
-    public string Name { get; set; }
+    public string UserName { get; set; }
 
     [Required(ErrorMessage = "Wir benötigen deine E-Mail-Adresse.")]
     [RegularExpression(Regexes.Email, ErrorMessage = "Wir benötigen eine gültige E-Mail-Adresse.")]
     [DisplayName("E-Mail")]  
-    public string Email { get; set; }
+    public string Login { get; set; }
 
     [Required(ErrorMessage ="Ein Passwort ist Pflicht!")]
     [StringLength(40, MinimumLength = 5, ErrorMessage = "Mindestens 5 Zeichen und maximal 40 Zeichen.")]
