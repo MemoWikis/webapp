02bc7e5316d4d72287d5bfcd99bccbbde16baf72
Justin Schwab <justin-schwab@web.de>
2021-12-20 12:35:40 +0100

    StageOverlay fix

---
+++ src/TrueOrFalse.Frontend.Web/Scripts/header/MasterHeader.ts
@@ -0,0 +1,13 @@
+﻿function hideStageOverlay() {
+    document.getElementById('StageOverlay').classList.add('closedContainer');
+    sessionStorage.setItem('showStageOverlay', 'false');
+}
+function checkStageOverlay() {
+    var showStageOverlay = sessionStorage.getItem('showStageOverlay');
+
+    if (showStageOverlay == 'false') {
+        document.getElementById('StageOverlay').classList.add('closedContainer');
+    }
+}
+
+checkStageOverlay();
