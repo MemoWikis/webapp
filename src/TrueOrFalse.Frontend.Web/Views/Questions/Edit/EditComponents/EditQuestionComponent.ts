de29fd32f56f6747bcdf4e6b79a3664b58b9c939
Jun <mai.t.tung@gmail.com>
2022-04-07 03:25:21 +0200

    #339 minor

--- src/TrueOrFalse.Frontend.Web/Views/Questions/Edit/EditComponents/EditQuestionComponent.ts
+++ src/TrueOrFalse.Frontend.Web/Views/Questions/Edit/EditComponents/EditQuestionComponent.ts
@@ -406,7 +406,7 @@ var editQuestionComponent = Vue.component('edit-question-modal-component',
                                 Alerts.showSuccess({
                                     text: self.edit ? messages.success.question.saved : messages.success.question.created,
                                     customHtml: '<div class="session-config-error fade in col-xs-12"><span><b>Achtung: Die Frage wird nicht angezeigt.</b> Setze den Filter zurück, um alle Fragen anzuzeigen.</span></div>',
-                                    customBtn: '<div class="btn memo-button col-xs-4 btn-link" data-dismiss="modal" onclick="eventBus.$emit(\'reset-session-config\')">Filter zuruecksetzen</div>',
+                                    customBtn: '<div class="btn memo-button col-xs-4 btn-link" data-dismiss="modal" onclick="eventBus.$emit(\'reset-session-config\')">Filter zurücksetzen</div>',
                                 });
                         }
                         $('#EditQuestionModal').modal('hide');
