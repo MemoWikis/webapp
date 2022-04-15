196975979512d6d839be075eb3b873ce00603851
Robert Mischke <robertmischke@gmail.com>
2022-04-16 01:02:00 +0200

    removed unused code

--- src/TrueOrFalse.Frontend.Web/Views/VariousPublic/VariousPublicController.cs
+++ src/TrueOrFalse.Frontend.Web/Views/VariousPublic/VariousPublicController.cs
@@ -7,23 +7,12 @@ public class VariousPublicController : Controller
         return View(new BaseModel());
     }
 
-    [SetMainMenu(MainMenuEntry.None)]
-    public ActionResult MemuchoBeta()
-    {
-        return View(new BaseModel());
-    }
-
     public ActionResult TermsAndConditions()
     {
         return View(new BaseModel());
     }
 
     public ActionResult NotDoneYet()
-    {
-        return View(new BaseModel());
-    }
-
-    public ActionResult Qunit()
     {
         return View(new BaseModel());
     }
