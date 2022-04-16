3a6ae8c2b6983b228ed25b0c1bf25ecabb6e0f20
Robert Mischke <robertmischke@gmail.com>
2022-04-16 12:57:49 +0200

    restored parts of the welcome controller

---
+++ src/TrueOrFalse.Frontend.Web/Views/Welcome/WelcomeController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Net.Mail;
+using System.Web.Mvc;
+using TrueOrFalse.Frontend.Web.Code;
+using TrueOrFalse.Web;
+
+public class WelcomeController : BaseController
+{
+    [SetMainMenu(MainMenuEntry.None)]
+    [SetThemeMenu]
+    public ActionResult Welcome()
+    {
+        CategoryCacheItem category;
+        category = IsLoggedIn ? EntityCache.GetCategory(SessionUser.User.StartTopicId, getDataFromEntityCache: true) : RootCategory.Get; 
+
+        return Redirect(Links.CategoryDetail(category)); ;
+    }
+
+    public ActionResult Promoter()
+    {
+        return View(new BaseModel());
+    }
+
+    public ActionResult LogOn() => View();
+
+    public ActionResult Logout()
+    {
+        RemovePersistentLoginFromCookie.Run();
+        SessionUser.Logout();
+        return View(new BaseModel());
+    }
+
+    public ActionResult PasswordRecovery()
+    {
+        return View(new PasswordRecoveryModel());
+    }
+
+    [HttpPost]
+    public ActionResult PasswordRecovery(PasswordRecoveryModel model)
+    {
+        var result = Sl.Resolve<PasswordRecovery>().Run(model.Email);
+        if (result.TheEmailDoesNotExist)
+            model.Message = new ErrorMessage("Diese E-Mail-Adresse ist uns unbekannt.");
+        else if (result.Success)
+            model.Message = new SuccessMessage("Wir haben an " + model.Email + " eine E-Mail verschickt (überprüfe gegebenenfalls auch deinen Spam-Ordner). Klicke dort auf den Link, um ein neues Passwort zu setzen.");
+
+        return View(model);
+    }
+
+    [HttpGet]
+    public ActionResult PasswordReset(string id)
+    {
+        var result = PasswordResetPrepare.Run(id);
+        var model = new PasswordResetModel { TokenFound = result.Success, Token = id };
+
+        if (result.TokenOlderThan72h)
+        {
+            model.Message = new ErrorMessage("Der Link ist abgelaufen.");
+            return View(model);
+        }
+
+        if (!result.Success)
+            model.Message = new ErrorMessage("Der Link ist leider ungültig. Wenn du Probleme hast, schreibe uns einfach eine E-Mail an team@memucho.de.");
+
+        return View(model);
+    }
+
+    [HttpPost]
+    public ActionResult PasswordReset(PasswordResetModel model)
+    {
+        if (!ModelState.IsValid)
+        {
+            model.TokenFound = true;
+            model.Message = new ErrorMessage("Bitte überprüfe deine Eingaben.");
+            return View(model);
+        }
+
+        var result = PasswordResetPrepare.Run(model.Token);
+
+        var userRepo = Sl.Resolve<UserRepo>();
+        var user = userRepo.GetByEmail(result.Email);
+
+        if (user == null)
+            throw new Exception();
+
+        SetUserPassword.Run(model.NewPassword1, user);
+        userRepo.Update(user);
+
+        SessionUser.Login(user);
+
+        return RedirectToAction(Links.KnowledgeAction, Links.KnowledgeController, new {passwordSet = "true"});
+    }
+}
