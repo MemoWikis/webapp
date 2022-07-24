53570af1c32d844fb2ba2e1f33df874c3ea18288
Robert Mischke <robertmischke@gmail.com>
2022-07-24 20:00:05 +0200

    prevent (some) threading exception. code is far from thread safe

--- src/TrueOrFalse/Infrastructure/Persistence/Session/SqlDebugOutputInterceptor.cs
+++ src/TrueOrFalse/Infrastructure/Persistence/Session/SqlDebugOutputInterceptor.cs
@@ -14,15 +14,21 @@ public class SqlDebugOutputInterceptor : EmptyInterceptor
     private ISession _session;
     private readonly Dictionary<string, Stopwatch> _watches = new();
 
+    private static readonly object _addStopWatchLock = new();
+
     public override void SetSession(ISession session)
     {
-        base.SetSession(session);
-        _session = session;
-        Log.Information("NHibernate SetSession");
+        lock (_addStopWatchLock)
+        {
+            base.SetSession(session);
+            _session = session;
+            Log.Information("NHibernate SetSession");
 
-        var sessionKey = ((SessionImpl)_session).SessionId.ToString();
-        if (!_watches.ContainsKey(sessionKey))
-            _watches.Add(sessionKey, Stopwatch.StartNew());
+            var sessionKey = ((SessionImpl)_session).SessionId.ToString();
+
+            if (!_watches.ContainsKey(sessionKey))
+                _watches.Add(sessionKey, Stopwatch.StartNew());
+        }
     }
 
     public override SqlString OnPrepareStatement(SqlString sql)
@@ -31,28 +37,26 @@ public class SqlDebugOutputInterceptor : EmptyInterceptor
         StackTrace stackTrace = new StackTrace(true);
         var statistics = ((SessionImpl)_session).Statistics;
         var sessionId = ((SessionImpl)_session).SessionId.ToString();
-        Task.Run(() =>
+        
+        var stackFrames = stackTrace.GetFrames();
+        var sb = new StringBuilder();
+        foreach (var frame in stackFrames)
         {
-            var stackFrames = stackTrace.GetFrames();
-            var sb = new StringBuilder();
-            foreach (var frame in stackFrames)
-            {
-                var method = frame.GetMethod();
-                if (!method.Module.Name.Contains("TrueOrFalse"))
-                    continue;
+            var method = frame.GetMethod();
+            if (!method.Module.Name.Contains("TrueOrFalse"))
+                continue;
 
-                sb.AppendLine($"{method.DeclaringType}.{method.Name} in {frame.GetFileName()}:{frame.GetFileLineNumber()}");
-            }
+            sb.AppendLine($"{method.DeclaringType}.{method.Name} in {frame.GetFileName()}:{frame.GetFileLineNumber()}");
+        }
 
-            if (_session.IsOpen)
-                Log.Information("NHibernate before query: {sessionId} collections {collectionCount} entities {entityCount}",
-                    sessionId, statistics.CollectionCount, statistics.EntityCount);
+        if (_session.IsOpen)
+            Log.Information("NHibernate before query: {sessionId} collections {collectionCount} entities {entityCount}",
+                sessionId, statistics.CollectionCount, statistics.EntityCount);
 
-            if(_watches.ContainsKey(sessionId))
-                Log.Information("NHibernate before elapsed: {elapsed}", _watches[sessionId].Elapsed.TotalMilliseconds);
-            
-            Log.Information("NHibernate {sessionId} {sqlString} {stacktrace}", ((SessionImpl)_session).SessionId, sqlString, sb);
-        });
+        if(_watches.ContainsKey(sessionId))
+            Log.Information("NHibernate before elapsed: {elapsed}", _watches[sessionId].Elapsed.TotalMilliseconds);
+        
+        Log.Information("NHibernate {sessionId} {sqlString} {stacktrace}", ((SessionImpl)_session).SessionId, sqlString, sb);
 
         return base.OnPrepareStatement(sql);
     }
