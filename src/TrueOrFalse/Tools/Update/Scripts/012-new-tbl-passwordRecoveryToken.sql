ba8fc6673e5d6d2f9e9b8f93447f6f9209e62b72
Robert Mischke <robertmischke@gmail.com>
2022-07-28 02:09:51 +0200

    removed old updates and migrations

--- src/TrueOrFalse/Tools/Update/Scripts/012-new-tbl-passwordRecoveryToken.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE `PasswordRecoveryToken` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `Token` varchar(255) DEFAULT NULL,
-  `Email` varchar(255) DEFAULT NULL,
-  `DateModified` datetime DEFAULT NULL,
-  `DateCreated` datetime DEFAULT NULL,
-  PRIMARY KEY (`Id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
-
--- src/TrueOrFalse/Tools/Update/Scripts/027-new-tbl-setValuations.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE `setvaluation` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `UserId` int(11) DEFAULT NULL,
-  `SetId` int(11) DEFAULT NULL,
-  `RelevancePersonal` int(11) DEFAULT NULL,
-  `DateCreated` datetime DEFAULT NULL,
-  `DateModified` datetime DEFAULT NULL,
-  PRIMARY KEY (`Id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8
