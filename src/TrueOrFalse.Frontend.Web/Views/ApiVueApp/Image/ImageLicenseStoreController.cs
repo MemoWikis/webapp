f593e561163588d107dc1239f7215f203b89982e
Jun <mai.t.tung@gmail.com>
2023-01-09 16:03:21 +0100

    #440 fixed null reference in imageId

--- src/TrueOrFalse.Frontend.Web/Views/ApiVueApp/Image/ImageLicenseStoreController.cs
+++ src/TrueOrFalse.Frontend.Web/Views/ApiVueApp/Image/ImageLicenseStoreController.cs
@@ -63,7 +63,7 @@ public class ImageLicenseStoreController : BaseController
             return Json(new
             {
                 imageCanBeDisplayed = false
-            }, JsonRequestBehavior.AllowGet);
+            }, JsonRequestBehavior.AllowGet );
         }
     }
 
