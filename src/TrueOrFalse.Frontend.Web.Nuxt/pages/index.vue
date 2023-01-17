62546ee988fa0767ddc80e2f01c10f1de7d29c7c
Jun <mai.t.tung@gmail.com>
2023-01-17 23:22:13 +0100

    #440 reroute users to personal wiki, and guests to global wiki

--- src/TrueOrFalse.Frontend.Web.Nuxt/pages/index.vue
+++ src/TrueOrFalse.Frontend.Web.Nuxt/pages/index.vue
@@ -1,5 +1,13 @@
 <script lang="ts" setup>
 import { Page } from '~~/components/shared/pageEnum'
+import { useUserStore } from '~~/components/user/userStore'
+const userStore = useUserStore()
+
+if (userStore.isLoggedIn) {
+    navigateTo(`${userStore.personalWiki?.Name}/${userStore.personalWiki?.Id}`)
+} else {
+    navigateTo(`/Globales-Wiki/1`)
+}
 const emit = defineEmits(['setPage'])
 onBeforeMount(() => {
     emit('setPage', Page.Welcome)
