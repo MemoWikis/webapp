60cd697e499671d9856e593e20a211d560032edc
Daniel-Majunke <daniel.majunke@gmx.de>
2023-01-25 16:12:32 +0100

    # 460 Add create Category to meilisearch

--- src/TrueOrFalse/Domain/Category/Persistence/CategoryRepository.cs
+++ src/TrueOrFalse/Domain/Category/Persistence/CategoryRepository.cs
@@ -3,6 +3,7 @@ using NHibernate.Criterion;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using TrueOrFalse.Search;
 
 public class CategoryRepository : RepositoryDbBase<Category>
@@ -56,12 +57,13 @@ public class CategoryRepository : RepositoryDbBase<Category>
     /// <param name="category"></param>
     public override void Create(Category category)
     {
+
         foreach (var related in category.ParentCategories().Where(x => x.DateCreated == default(DateTime)))
             related.DateModified = related.DateCreated = DateTime.Now;
 
         base.Create(category);
         Flush();
-       
+
         UserActivityAdd.CreatedCategory(category);
         _solrSearchIndexCategory.Update(category);
 
@@ -82,6 +84,8 @@ public class CategoryRepository : RepositoryDbBase<Category>
         {
             Sl.CategoryChangeRepo.AddUpdateEntry(category, category.Creator?.Id ?? default, false, type: CategoryChangeType.Relations);
         }
+        var result = Task.Run(async ()=> await MeiliSearchDatabaseOperations.CategoryCreateAsync(category));
+
     }
 
     public void CreateOnlyDb(Category category)
@@ -159,7 +163,7 @@ public class CategoryRepository : RepositoryDbBase<Category>
     public void UpdateAuthors(Category category)
     {
         var sql = "UPDATE category " +
-                  "SET AuthorIds = " +"'" + category.AuthorIds + "'" +
+                  "SET AuthorIds = " + "'" + category.AuthorIds + "'" +
                   " WHERE Id = " + category.Id;
         _session.CreateSQLQuery(sql).ExecuteUpdate();
     }
