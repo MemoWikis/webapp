ac1d60ec254fce30fb5cea1914160b76174d1545
Daniel-Majunke <daniel.majunke@gmx.de>
2023-02-15 12:32:44 +0100

    code clean add yaml file

--- src/TrueOrFalse.Frontend.Web/Views/Questions/Answer/TopicToContinueLearningModel.cs
+++ src/TrueOrFalse.Frontend.Web/Views/Questions/Answer/TopicToContinueLearningModel.cs
@@ -6,7 +6,6 @@ using System.Web;
 public class TopicToContinueLearningModel
 {
     public bool IsLearningSession { get; }
-    public bool IsTestSession { get; }
     public ContentRecommendationResult ContentRecommendationResult { get; }
     public IList<CategoryCacheItem> Categories; 
     public TopicToContinueLearningModel(AnswerQuestionModel answerQuestionModel, IList<CategoryCacheItem> categoryList)
