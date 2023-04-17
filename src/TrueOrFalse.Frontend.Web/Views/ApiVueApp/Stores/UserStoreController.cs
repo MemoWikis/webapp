8e89438fdd6c12f59e2c7520e6177cd82acbf6e9
Jun <mai.t.tung@gmail.com>
2023-04-17 10:52:44 +0200

    nuxt: register fix

--- src/TrueOrFalse.Frontend.Web/Views/ApiVueApp/Stores/UserStoreController.cs
+++ src/TrueOrFalse.Frontend.Web/Views/ApiVueApp/Stores/UserStoreController.cs
@@ -1,5 +1,7 @@
 ﻿using System.Linq;
 using System.Web.Mvc;
+using Quartz;
+using Quartz.Impl;
 
 namespace VueApp;
 
@@ -61,8 +63,103 @@ public class UserStoreController : BaseController
     [AccessOnlyAsLoggedIn]
     [HttpPost]
     public void ResetPassword(string email) => Sl.Resolve<PasswordRecovery>().Run(email);
+
+    [HttpPost]
+    public JsonResult Register(RegisterJson json)
+    {
+        if (!IsEmailAddressAvailable.Yes(json.Email))
+            return Json(new
+            {
+                Data = new
+                {
+                    Success = false,
+                    Message = "emailInUse",
+                }
+            });
+
+        if (!global::IsUserNameAvailable.Yes(json.Name))
+            return Json(new
+            {
+                Data = new
+                {
+                    Success = false,
+                    Message = "userNameInUse",
+                }
+            });
+
+        var user = SetUser(json);
+
+        RegisterUser.Run(user);
+        ISchedulerFactory schedFact = new StdSchedulerFactory();
+        var x = schedFact.AllSchedulers;
+
+        SessionUser.Login(user);
+
+        var category = PersonalTopic.GetPersonalCategory(user);
+        category.Visibility = CategoryVisibility.Owner;
+        Sl.CategoryRepo.Create(category);
+        user.StartTopicId = category.Id;
+
+        Sl.UserRepo.Update(user);
+
+        var type = UserType.Anonymous;
+        if (SessionUser.IsLoggedIn)
+        {
+            if (SessionUser.User.IsGoogleUser)
+                type = UserType.Google;
+            else if (SessionUser.User.IsFacebookUser)
+                type = UserType.Facebook;
+            else type = UserType.Normal;
+        }
+
+        return Json(new
+        {
+            Success = true,
+            Message = "",
+            CurrentUser = new
+            {
+                IsLoggedIn = SessionUser.IsLoggedIn,
+                Id = SessionUser.UserId,
+                Name = SessionUser.IsLoggedIn ? SessionUser.User.Name : "",
+                IsAdmin = SessionUser.IsInstallationAdmin,
+                PersonalWikiId = SessionUser.IsLoggedIn ? SessionUser.User.StartTopicId : 1,
+                Type = type,
+                ImgUrl = SessionUser.IsLoggedIn
+                    ? new UserImageSettings(SessionUser.UserId).GetUrl_20px(SessionUser.User).Url
+                    : "",
+                Reputation = SessionUser.IsLoggedIn ? SessionUser.User.Reputation : 0,
+                ReputationPos = SessionUser.IsLoggedIn ? SessionUser.User.ReputationPos : 0,
+                PersonalWiki = new TopicController().GetTopicData(SessionUser.IsLoggedIn ? SessionUser.User.StartTopicId : 1)
+            }
+        });
+    }
+
+    private static User SetUser(RegisterJson json)
+    {
+        var user = new User();
+        user.EmailAddress = json.Email.TrimAndReplaceWhitespacesWithSingleSpace();
+        user.Name = json.Name.TrimAndReplaceWhitespacesWithSingleSpace();
+        SetUserPassword.Run(json.Password.Trim(), user);
+        return user;
+    }
+
+    private enum UserType
+    {
+        Normal,
+        Google,
+        Facebook,
+        Anonymous
+    }
 }
-    
+
+public class RegisterJson
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
+
+
 public class StateModel
 {
     public bool IsLoggedIn { get; set; }
