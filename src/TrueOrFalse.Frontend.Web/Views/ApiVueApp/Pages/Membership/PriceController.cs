f89e27565b9dda9725113268c832fdb104a44732
Daniel-Majunke <daniel.majunke@gmx.de>
2023-04-18 13:52:12 +0200

    CodeReview

--- src/TrueOrFalse.Frontend.Web/Views/ApiVueApp/Pages/Membership/PriceController.cs
+++ src/TrueOrFalse.Frontend.Web/Views/ApiVueApp/Pages/Membership/PriceController.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using System.Web.Http;
+﻿using System.Threading.Tasks;
 using System.Web.Mvc;
-using Stripe;
-using Stripe.Checkout;
 using TrueOrFalse.Stripe;
 
 namespace VueApp;
@@ -14,50 +9,13 @@ public class PriceController: BaseController
     [AccessOnlyAsLoggedIn]
     public async Task<JsonResult> CreateCheckoutSession(string priceId)
     {
-        var sessionUser = SessionUser.User;
-        var abologik = new SubscriptionLogic();
-        var customerId = "";
-        if (sessionUser.StripeId == null)
+        var sessionId = await new SubscriptionLogic().CreateStripeSession(priceId); 
+        if(sessionId.Equals("-1"))
         {
-            customerId = await abologik.CreateCustomer(sessionUser.Name, sessionUser.EmailAddress, sessionUser.Id);
-        }
-        else
-        {
-            customerId = sessionUser.StripeId;
+            return Json(new { success = false }); 
         }
 
-
-        var options = new SessionCreateOptions
-        {
-            PaymentMethodTypes = new List<string> { "card" },
-            Mode = "subscription",
-            LineItems = new List<SessionLineItemOptions>
-            {
-                new ()
-                {
-                    Price = priceId,
-                    Quantity = 1,
-                },
-            },
-            SuccessUrl = "https://example.com/success",
-            CancelUrl = "https://example.com/cancel",
-            Customer = customerId,
-        };
-
-        try
-        {
-            var sessionService = new SessionService();
-            var session = await sessionService.CreateAsync(options);
-
-            return Json(new { success = true,id = session.Id }); ;
-        }
-        catch (StripeException e)
-        {
-            return Json(new
-            {
-                success = false
-            });
-        };
+        return Json(new { success = false, id = sessionId });
     }
 }
 
