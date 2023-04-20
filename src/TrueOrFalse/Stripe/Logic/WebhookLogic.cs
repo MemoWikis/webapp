8c3763d67f883ef98e7aecb01bc321534c909d5f
Daniel-Majunke <daniel.majunke@gmx.de>
2023-04-20 09:54:20 +0200

    Code Review

--- src/TrueOrFalse/Stripe/Logic/WebhookLogic.cs
+++ src/TrueOrFalse/Stripe/Logic/WebhookLogic.cs
@@ -9,86 +9,109 @@ using System.Threading.Tasks;
 using System.Web.Mvc;
 using System.Web;
 
-namespace TrueOrFalse.Stripe.Logic
+namespace TrueOrFalse.Stripe.Logic;
+
+public class WebhookLogic
 {
-    public class WebhookLogic
+    public async Task<HttpStatusCodeResult> Create(HttpContextBase context, HttpRequestBase baseRequest)
     {
+        var eventAndStatus = await GetEvent(context, baseRequest);
+        var stripeEvent = eventAndStatus.stripeEvent;
+        var status = eventAndStatus.httpStatusCode;
 
+        if (stripeEvent == null)
+            return status;
 
-        public async Task<HttpStatusCodeResult> Create(HttpContextBase context, HttpRequestBase baseRequest)
+        if (stripeEvent.Type == Events.PaymentIntentSucceeded)
         {
-            var json = await new StreamReader(context.Request.InputStream).ReadToEndAsync();
-            var endpointSecret = Settings.WebhookKeyStripe;
-            try
-            {
-               
-                var signatureHeader = baseRequest.Headers["Stripe-Signature"];
+            ProcessSuccessfulPayment(stripeEvent);
+        }
+        else if (stripeEvent.Type == Events.CustomerSubscriptionDeleted)
+        {
+            ProcessDeletedPayment(stripeEvent);
+        }
+        else if (stripeEvent.Type == Events.PaymentMethodAttached)
+        {
+            var paymentMethod = stripeEvent.Data.Object as PaymentMethod;
+            Logg.r().Error($"The user paid with an incorrect payment method,  the CustomerId is {paymentMethod.CustomerId}.");
+        }
+        else
+        {
+            Logg.r().Error("Unhandled event type: {0}", stripeEvent.Type);
+        }
+        return new HttpStatusCodeResult(HttpStatusCode.OK);
+    }
 
-                var stripeEvent = EventUtility.ConstructEvent(json,
-                    signatureHeader, endpointSecret);
+    public (T paymentObject, User user) GetPaymentObjectAndUser<T>(Event stripeEvent) where T : class
+    {
+        var paymentObject = stripeEvent.Data.Object as T;
 
-                if (stripeEvent.Type == Events.PaymentIntentSucceeded)
-                {
-
-                    var paymentIntent = GetPaymentObjectAndUser<PaymentIntent>(stripeEvent);
-                    var user = paymentIntent.user; 
-                    if (user == null)
-                    {
-                        Logg.r().Error($"The user with the CustomerId:{paymentIntent.paymentObject.CustomerId}  was not found");
-                    }
-                    else
-                    {
-                        user.SubscriptionDuration = new DateTime(9999, 12, 31, 23, 59, 59);
-                        Sl.UserRepo.Update(user);
-                        Logg.r().Information(
-                            $"{user.Name} with userId: {user.Id}  has successfully subscribed to a plan.");
-                    }
-                }
-                else if (stripeEvent.Type == Events.CustomerSubscriptionDeleted)
-                {
-                    var paymentDeleted = GetPaymentObjectAndUser<Subscription>(stripeEvent);
-                    var user = paymentDeleted.user;
-                    user.SubscriptionDuration = paymentDeleted.paymentObject.CurrentPeriodEnd;
-                    Sl.UserRepo.Update(user);
-                    Logg.r().Information(
-                        $"{user.Name} with userId: {user.Id}  has deleted the plan.");
-                }
-                else if (stripeEvent.Type == Events.PaymentMethodAttached)
-                {
-                    var paymentMethod = stripeEvent.Data.Object as PaymentMethod;
-                }
-
-                else
-                {
-                    Console.WriteLine("Unhandled event type: {0}", stripeEvent.Type);
-                }
-
-                return new HttpStatusCodeResult(HttpStatusCode.OK);
-            }
-            catch (StripeException e)
-            {
-                Console.WriteLine("Error: {0}", e.Message);
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            catch (Exception e)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.InternalServerError);
-            }
+        if (paymentObject == null)
+        {
+            throw new ArgumentException("Invalid object type");
         }
 
-        public (T paymentObject, User user) GetPaymentObjectAndUser<T>(Event stripeEvent) where T : class
+        var customerId = ((dynamic)paymentObject).CustomerId;
+        var user = Sl.UserRepo.GetByStripeId(customerId);
+
+        return (paymentObject, user);
+    }
+
+    public async Task<(Event stripeEvent, HttpStatusCodeResult httpStatusCode)> GetEvent(HttpContextBase context, HttpRequestBase baseRequest)
+    {
+        var json = await new StreamReader(context.Request.InputStream).ReadToEndAsync();
+        var endpointSecret = Settings.WebhookKeyStripe;
+        try
         {
-            T paymentObject = stripeEvent.Data.Object as T;
+            var signatureHeader = baseRequest.Headers["Stripe-Signature"];
+            var stripeEvent = EventUtility.ConstructEvent(json,
+                signatureHeader, endpointSecret);
+            return (stripeEvent, new HttpStatusCodeResult(HttpStatusCode.OK));
+        }
+        catch (StripeException e)
+        {
+            Console.WriteLine("Error: {0}", e.Message);
+            return (null, new HttpStatusCodeResult(HttpStatusCode.BadRequest));
+        }
+        catch (Exception e)
+        {
+            return (null, new HttpStatusCodeResult(HttpStatusCode.InternalServerError));
+        }
+    }
 
-            if (paymentObject == null)
-            {
-                throw new ArgumentException("Invalid object type");
-            }
+    public void ProcessSuccessfulPayment(Event stripeEvent)
+    {
+        var paymentIntent = GetPaymentObjectAndUser<PaymentIntent>(stripeEvent);
+        var user = paymentIntent.user;
+        if (user == null)
+        {
+            Logg.r().Error(
+                $"The user with the CustomerId:{paymentIntent.paymentObject.CustomerId}  was not found");
+        }
+        else
+        {
+            user.SubscriptionDuration = new DateTime(9999, 12, 31, 23, 59, 59);
+            Sl.UserRepo.Update(user);
+            Logg.r().Information(
+                $"{user.Name} with userId: {user.Id}  has successfully subscribed to a plan.");
+        }
+    }
 
-            var customerId = ((dynamic)paymentObject).CustomerId;
-            var user = Sl.UserRepo.GetByStripeId(customerId);
-
-            return (paymentObject,user);
+    public void ProcessDeletedPayment(Event stripeEvent)
+    {
+        var paymentDeleted = GetPaymentObjectAndUser<Subscription>(stripeEvent);
+        var user = paymentDeleted.user;
+        if (user != null)
+        {
+            Logg.r().Error(
+                $"The user with the CustomerId:{paymentDeleted.paymentObject.CustomerId}  was not found");
+        }
+        else
+        {
+            user.SubscriptionDuration = paymentDeleted.paymentObject.CurrentPeriodEnd;
+            Sl.UserRepo.Update(user);
+            Logg.r().Information(
+                $"{user.Name} with userId: {user.Id}  has deleted the plan.");
         }
     }
 }
