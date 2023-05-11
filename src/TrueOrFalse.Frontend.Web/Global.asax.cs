871979f6a937ae6015fccf5e412d1bb48b425b07
Daniel-Majunke <daniel.majunke@gmx.de>
2023-05-11 10:50:43 +0200

    Code Review and Remove get failed stripe requests #480

--- src/TrueOrFalse.Frontend.Web/Global.asax.cs
+++ src/TrueOrFalse.Frontend.Web/Global.asax.cs
@@ -13,123 +13,133 @@ using Serilog;
 using StackExchange.Profiling;
 using Stripe;
 using TrueOrFalse.Infrastructure;
-using TrueOrFalse.Stripe.Logic;
+using TrueOrFalse.Tools;
 using TrueOrFalse.Updates;
 using TrueOrFalse.Utilities.ScheduledJobs;
 using TrueOrFalse.View;
-using TrueOrFalse.Tools;
 using TrueOrFalse.Web.JavascriptView;
 
-namespace TrueOrFalse.Frontend.Web
+namespace TrueOrFalse.Frontend.Web;
+
+public class Global : HttpApplication
 {
-    public class Global : HttpApplication
-    {        
-       
-        protected void Application_Start()
-        {
-            Logg.r().Information("=== Application Start (start) ===============================");
-            StripeConfiguration.ApiKey = Settings.SecurityKeyStripe;
-            IgnoreLog.Initialize();
-            InitializeAutofac();
-            
-            Sl.Resolve<Update>().Run();
-
-            AreaRegistration.RegisterAllAreas();
-
-            BundleConfig.RegisterBundles(BundleTable.Bundles); 
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-
-            ViewEngines.Engines.Clear();
-            ViewEngines.Engines.Add(new JavaScriptViewEngine());
-            ViewEngines.Engines.Add(new PartialSubDirectoriesViewEngine());
-
-            if (!Settings.DisableAllJobs())
-                JobScheduler.Start();
-
-            if (Settings.InitEntityCacheViaJobScheduler())
-            {
-                JobScheduler.StartImmediately_RefreshEntityCache();//Is a lot faster (for unknown reasons) than direct init but bears the risk of EntityCache not being filled before first request
-            }
-            else
-            {
-                EntityCache.Init();
-            }
-            new WebhookLogic().FetchFailedWebhookRequests();
-            Sl.Resolve<ISession>().Close();
-            Logg.r().Information("=== Application Start (end) ===============================");
-        }
-
-        protected void Application_Stop()
-        {
-            JobScheduler.Shutdown();
-            Logg.r().Information("=== Application Stop ===============================");
-        }
-
-        private void Application_BeginRequest(object source, EventArgs e)
-        {
-            Thread.CurrentThread.CurrentCulture = new CultureInfo("de-DE");
-            Thread.CurrentThread.CurrentUICulture = new CultureInfo("de-DE");
+    protected void Application_EndRequest(object source, EventArgs e)
+    {
 #if DEBUG
-            if (Settings.DebugEnableMiniProfiler())
-                MiniProfiler.StartNew();
+        //if (Settings.DebugEnableMiniProfiler())
+        //MiniProfiler.Current.Stop();
 
-            var app = (HttpApplication)source;
-            var uriObject = app.Context.Request.Url;
+        var app = (HttpApplication)source;
+        var uriObject = app.Context.Request.Url;
 
-            if (!uriObject.AbsolutePath.Contains("."))
-            {
-                app.Context.Items.Add("requestStopwatch", Stopwatch.StartNew());
-                Log.Information("=== Start Request: {pathAndQuery} ==", uriObject.PathAndQuery);
-            }
+        if (!uriObject.AbsolutePath.Contains("."))
+        {
+            var elapsed = ((Stopwatch)app.Context.Items["requestStopwatch"]).Elapsed;
+            Log.Information("=== End Request: {pathAndQuery} {elapsed}==", uriObject.PathAndQuery, elapsed);
+        }
 #endif
+    }
+
+    protected void Application_Error(object sender, EventArgs e)
+    {
+        var exception = Server.GetLastError();
+        if (exception == null)
+        {
+            return;
         }
 
-        protected void Application_EndRequest(object source, EventArgs e)
+        var code = exception is HttpException ? (exception as HttpException).GetHttpCode() : 500;
+
+        if (code != 404)
         {
+            Logg.Error(exception);
+        }
+
+        if (!Request.IsLocal)
+        {
+            Response.Redirect(string.Format("~/Fehler/{0}", code), true);
+        }
+    }
+
+    protected void Application_Start()
+    {
+        Logg.r().Information("=== Application Start (start) ===============================");
+        StripeConfiguration.ApiKey = Settings.SecurityKeyStripe;
+        IgnoreLog.Initialize();
+        InitializeAutofac();
+
+        Sl.Resolve<Update>().Run();
+
+        AreaRegistration.RegisterAllAreas();
+
+        BundleConfig.RegisterBundles(BundleTable.Bundles);
+        RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+        ViewEngines.Engines.Clear();
+        ViewEngines.Engines.Add(new JavaScriptViewEngine());
+        ViewEngines.Engines.Add(new PartialSubDirectoriesViewEngine());
+
+        if (!Settings.DisableAllJobs())
+        {
+            JobScheduler.Start();
+        }
+
+        if (Settings.InitEntityCacheViaJobScheduler())
+        {
+            JobScheduler
+                .StartImmediately_RefreshEntityCache(); //Is a lot faster (for unknown reasons) than direct init but bears the risk of EntityCache not being filled before first request
+        }
+        else
+        {
+            EntityCache.Init();
+        }
+
+        Sl.Resolve<ISession>().Close();
+        Logg.r().Information("=== Application Start (end) ===============================");
+    }
+
+    protected void Application_Stop()
+    {
+        JobScheduler.Shutdown();
+        Logg.r().Information("=== Application Stop ===============================");
+    }
+
+    protected void Session_Start()
+    {
+        if (!SessionUser.IsLoggedIn)
+        {
+            LoginFromCookie.Run();
+        }
+    }
+
+    private void Application_BeginRequest(object source, EventArgs e)
+    {
+        Thread.CurrentThread.CurrentCulture = new CultureInfo("de-DE");
+        Thread.CurrentThread.CurrentUICulture = new CultureInfo("de-DE");
 #if DEBUG
-            //if (Settings.DebugEnableMiniProfiler())
-                //MiniProfiler.Current.Stop();
+        if (Settings.DebugEnableMiniProfiler())
+        {
+            MiniProfiler.StartNew();
+        }
 
-            var app = (HttpApplication)source;
-            var uriObject = app.Context.Request.Url;
+        var app = (HttpApplication)source;
+        var uriObject = app.Context.Request.Url;
 
-            if (!uriObject.AbsolutePath.Contains("."))
-            {
-                var elapsed = ((Stopwatch)app.Context.Items["requestStopwatch"]).Elapsed;
-                Log.Information("=== End Request: {pathAndQuery} {elapsed}==", uriObject.PathAndQuery, elapsed);
-            }
+        if (!uriObject.AbsolutePath.Contains("."))
+        {
+            app.Context.Items.Add("requestStopwatch", Stopwatch.StartNew());
+            Log.Information("=== Start Request: {pathAndQuery} ==", uriObject.PathAndQuery);
+        }
 #endif
-        }
+    }
 
-        private void InitializeAutofac()
-        {
-            DependencyResolver.SetResolver(
-                new AutofacDependencyResolver(
-                    AutofacWebInitializer.Run(
-                        registerForAspNet: true, 
-                        assembly:Assembly.GetExecutingAssembly()
-                    )));
-        }
-
-        protected void Session_Start()
-        {
-            if(!SessionUser.IsLoggedIn)
-                LoginFromCookie.Run();
-        }
-
-        protected void Application_Error(object sender, EventArgs e)
-        {
-            var exception = Server.GetLastError();
-            if (exception == null)
-                return;
-
-            var code = (exception is HttpException) ? (exception as HttpException).GetHttpCode() : 500;
-
-            if (code != 404)
-                Logg.Error(exception);
-
-            if(!Request.IsLocal)
-                Response.Redirect(string.Format("~/Fehler/{0}", code), true);
-        }
+    private void InitializeAutofac()
+    {
+        DependencyResolver.SetResolver(
+            new AutofacDependencyResolver(
+                AutofacWebInitializer.Run(
+                    true,
+                    Assembly.GetExecutingAssembly()
+                )));
     }
 }
