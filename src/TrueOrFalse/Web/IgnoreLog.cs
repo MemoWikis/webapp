8092855cb5fb402fb9ae0b4b6bd0a195a15ed3a6
Daniel-Majunke <daniel.majunke@gmx.de>
2023-05-12 12:13:15 +0200

    Resharper Code clean

--- src/TrueOrFalse/Web/IgnoreLog.cs
+++ src/TrueOrFalse/Web/IgnoreLog.cs
@@ -1,67 +1,74 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Web;
 using static System.String;
 
-namespace TrueOrFalse.Tools
+namespace TrueOrFalse.Tools;
+
+public class IgnoreLog
 {
-    public class IgnoreLog
+    private static IEnumerable<string> _crawlers;
+
+    public static bool ContainsCrawlerInHeader(string header)
     {
-        private static IEnumerable<string> _crawlers;
-
-        public static IEnumerable<string> GetCrawlers()
+        if (IsNullOrEmpty(header))
         {
-            if (_crawlers == null) 
-                Initialize();
-
-            return _crawlers;
-        }
-
-        public static void Initialize()
-        {
-            lock ("3fb23623-caed-48fc-6e86-c595b4c0820c")
-            {
-                if (!File.Exists(PathTo.Log_Ignore()))
-                {
-                    Logg.r().Warning($"Ignore.log is not available- {PathTo.Log_Ignore()}");
-                    _crawlers = new List<string>();
-                }
-
-                var lines = File.ReadAllLines(PathTo.Log_Ignore());
-
-                _crawlers = lines
-                    .Select(line => line.Trim())
-                    .Where(line => line.Length > 0)
-                    .Select(line =>
-                    {
-                        var parts = line.Split(':');
-                        if (parts.Length == 2)
-                            return parts[1].ToLower().Trim();
-                        return "";
-                    })
-                    .Where(crawlerName => !IsNullOrEmpty(crawlerName));
-            }
-        }
-
-        public static bool ContainsCrawlerInHeader(string header)
-        {
-            if (IsNullOrEmpty(header))
-                return false;
-
-            foreach (var crawlerName in GetCrawlers())
-            {
-                if (header.ToLower().IndexOf(crawlerName.Trim()) != -1)
-                    return true;
-            }
             return false;
         }
-        public static void LoadNewList()
+
+        foreach (var crawlerName in GetCrawlers())
         {
-            _crawlers = null;
+            if (header.ToLower().IndexOf(crawlerName.Trim()) != -1)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static IEnumerable<string> GetCrawlers()
+    {
+        if (_crawlers == null)
+        {
             Initialize();
         }
+
+        return _crawlers;
+    }
+
+    public static void Initialize()
+    {
+        lock ("3fb23623-caed-48fc-6e86-c595b4c0820c")
+        {
+            if (!File.Exists(PathTo.Log_Ignore()))
+            {
+                Logg.r().Warning($"Ignore.log is not available- {PathTo.Log_Ignore()}");
+                _crawlers = new List<string>();
+            }
+
+            var lines = File.ReadAllLines(PathTo.Log_Ignore());
+
+            _crawlers = lines
+                .Select(line => line.Trim())
+                .Where(line => line.Length > 0)
+                .Select(line =>
+                {
+                    var parts = line.Split(':');
+                    if (parts.Length == 2)
+                    {
+                        return parts[1].ToLower().Trim();
+                    }
+
+                    return "";
+                })
+                .Where(crawlerName => !IsNullOrEmpty(crawlerName));
+        }
+    }
+
+    public static void LoadNewList()
+    {
+        _crawlers = null;
+        Initialize();
     }
 }
-
