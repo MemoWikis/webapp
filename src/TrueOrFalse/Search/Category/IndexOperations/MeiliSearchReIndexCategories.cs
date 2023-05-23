50a87c3fc887a5c77e5d2ff7448a19f0f1ada67c
Daniel-Majunke <daniel.majunke@gmx.de>
2023-05-23 13:17:37 +0200

    Code clean  Resharper

--- src/TrueOrFalse/Search/Category/IndexOperations/MeiliSearchReIndexCategories.cs
+++ src/TrueOrFalse/Search/Category/IndexOperations/MeiliSearchReIndexCategories.cs
@@ -1,39 +1,35 @@
-﻿using AutofacContrib.SolrNet;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using Meilisearch;
 
-namespace TrueOrFalse.Search
+namespace TrueOrFalse.Search;
+
+public class MeiliSearchReIndexCategories : IRegisterAsInstancePerLifetime
+
 {
-    public class MeiliSearchReIndexCategories : IRegisterAsInstancePerLifetime
-
+    public MeiliSearchReIndexCategories()
     {
-        public MeilisearchClient _client { get; }
-        public MeiliSearchReIndexCategories()
+        _client = new MeilisearchClient(MeiliSearchKonstanten.Url, MeiliSearchKonstanten.MasterKey);
+    }
+
+    public MeilisearchClient _client { get; }
+
+    public async Task Go()
+    {
+        await _client.DeleteIndexAsync(MeiliSearchKonstanten.Categories);
+        var allCateogoriesFromDb = Sl.CategoryRepo.GetAll();
+
+        var meiliSearchCategories = allCateogoriesFromDb.Select(c => new MeiliSearchCategoryMap
         {
-            _client = new MeilisearchClient(MeiliSearchKonstanten.Url, MeiliSearchKonstanten.MasterKey);
-        }
+            Id = c.Id,
+            Name = c.Name,
+            CreatorId = c.Creator == null ? -1 : c.Creator.Id,
+            DateCreated = c.DateCreated,
+            Description = c.Description,
+            QuestionCount = c.CountQuestions
+        });
 
-        public async Task Go()
-        {
-            await _client.DeleteIndexAsync(MeiliSearchKonstanten.Categories);
-            var allCateogoriesFromDb = Sl.CategoryRepo.GetAll();
-
-            var meiliSearchCategories = allCateogoriesFromDb.Select(c => new MeiliSearchCategoryMap
-            {
-                Id = c.Id,
-                Name = c.Name,
-                CreatorId =  c.Creator == null ? -1 : c.Creator.Id,
-                DateCreated = c.DateCreated,
-                Description = c.Description,
-                QuestionCount = c.CountQuestions
-            });
-
-            var index = _client.Index(MeiliSearchKonstanten.Categories);
-            await index.AddDocumentsAsync(meiliSearchCategories);
-        }
+        var index = _client.Index(MeiliSearchKonstanten.Categories);
+        await index.AddDocumentsAsync(meiliSearchCategories);
     }
 }
