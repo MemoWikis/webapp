db9137c2c780bf0d03f21c3bdd95f86d2501734a
Daniel-Majunke <daniel.majunke@gmx.de>
2023-05-30 10:36:20 +0200

    delete foreighn key

--- src/TrueOrFalse/Tools/Update/Steps/UpdateToVs261.cs
+++ src/TrueOrFalse/Tools/Update/Steps/UpdateToVs261.cs
@@ -6,14 +6,19 @@ internal class UpdateToVs261
 {
     public static void Run()
     {
-        //Sl.Resolve<ISession>()
-        //    .CreateSQLQuery(
-        //        @"ALTER TABLE learningsession DROP FOREIGN KEY FK946A748A5C428B0;"
-        //    ).ExecuteUpdate();
+        Sl.Resolve<ISession>()
+            .CreateSQLQuery(
+                @"ALTER TABLE learningsession DROP FOREIGN KEY FK946A748A5C428B0;"
+            ).ExecuteUpdate();
 
         Sl.Resolve<ISession>()
             .CreateSQLQuery(
                 @"ALTER TABLE useractivity DROP FOREIGN KEY Set_id_FK;"
+            ).ExecuteUpdate();
+
+        Sl.Resolve<ISession>()
+            .CreateSQLQuery(
+                @"ALTER TABLE questionset DROP FOREIGN KEY FKFFF30CD2DE1D1D36;"
             ).ExecuteUpdate();
 
         Sl.Resolve<ISession>()
