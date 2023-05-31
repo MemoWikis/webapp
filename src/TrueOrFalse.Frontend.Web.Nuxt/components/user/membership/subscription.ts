1938e837db557cc0c0250389e4c9d8dffb20062d
Daniel-Majunke <daniel.majunke@gmx.de>
2023-05-31 12:25:21 +0200

    change subscription for price cards

--- src/TrueOrFalse.Frontend.Web.Nuxt/components/user/membership/subscription.ts
+++ src/TrueOrFalse.Frontend.Web.Nuxt/components/user/membership/subscription.ts
@@ -14,36 +14,35 @@ export enum Type {
     Organisation
 }
 
-
 export const plans: { basic: Plan, plus: Plan, team: Plan, organisation: Plan } = {
     basic: {
         name: 'Kostenlos',
         price: 0,
         priceLabel: 'Kostenlos für immer!',
-        description: ['Für einzelne Personen, die schnell und unkompliziert mit dem Lernen beginnen wollen.', 'Erstelle Dein persönliches Wiki und verwalte erste persönliche Dokumente und kleine Lerneinheiten.'],
-        list: ['<b>Unbeschränkt</b> öffentliche Inhalte (ansehen, erstellen und lernen)', '<b>10</b> private Themen', '<b>50</b> private Fragen', 'max <b>150</b> Fragen im Wunschwissen'],
+        description: ['Du kannst alles machen. Nur die Anzahl der privaten Inhalte ist begrenzt.'],
+        list: ['Unbeschränkt öffentliche Inhalte (ansehen, erstellen und lernen)', '10 private Themen', '20 private Fragen', 'max 500 Fragen im Wunschwissen'],
     },
     plus: {
         name: 'Plus',
         price: 3,
         priceLabel: 'pro Monat',
-        description: ['Für einzelne Personen, die täglich Lernen und sich Wissen zu einer Vielzahl an Themen aneignen wollen.', 'Verwalten Sie unbegrenzt private Inhalte.'],
-        list: ['<b>Unbeschränkt</b> private Themen', '<b>Unbeschränkt</b> private Fragen', '<b>Unbeschränktes</b> Wunschwissen', 'Private Inhalte teilen', 'Gruppen Verwalten'],
+        description: ['Du unterstützt die Weiterentwicklung! Uneingeschränkt private Inhalte!'],
+        list: ['Du unterstützt uns :-)','Unbeschränkt private Themen', 'Unbeschränkt private Fragen', 'Unbeschränktes Wunschwissen'],
     },
     team: {
         name: 'Team',
         price: 7,
         priceLabel: 'pro Monat & Nutzer',
-        description: ['Für Lehrende und Lerngruppen, die sich gemeinsam Themen erarbeiten und gemeinsam Lernen wollen.', 'Verwalten Sie Lerngruppen und unbegrenzt Inhalte.'],
+        description: ['Für Teams, die eine zentrale Addministration und private Inhalte benötigen.'],
         listLabel: 'Alles aus Plus und zusätzlich:',
         list: ['Private Inhalte im Team bearbeiten', 'Teams verwalten'],
     },
     organisation: {
         name: 'Organisation',
         price: 'Auf Anfrage',
-        priceLabel: 'Individuelle Preise. <br/> Bitte Kontaktieren Sie uns für mehr Informationen.',
+        priceLabel: 'Individuelle Preise. Bitte kontaktiert uns für mehr Informationen.',
         description: ['Für Unternehmen und Institutionen, die zusätzliche Unterstützung benötigen.', 'Bringen Sie Ihr Team auf den neuesten Stand.'],
         listLabel: 'Alles aus Team, plus:',
-        list: ['Individuelle Anbindung an Infrastruktur', 'Installation on premise oder gehostet'],
+        list: ['SSO / AD-Integration', 'Eigener Server / Hosting in Eurem Rechenzentrium','Customizing'],
     }
 }
