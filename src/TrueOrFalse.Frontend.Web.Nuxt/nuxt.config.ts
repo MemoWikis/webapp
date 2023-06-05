4d9e43c3da2c1cc57b9b319e7decb0fe35b502a0
Jule Misdom <julianemisdom@gmail.com>
2023-06-05 10:52:38 +0200

    Fix nuxt.config

--- src/TrueOrFalse.Frontend.Web.Nuxt/nuxt.config.ts
+++ src/TrueOrFalse.Frontend.Web.Nuxt/nuxt.config.ts
@@ -5,7 +5,8 @@ export default defineNuxtConfig({
         routeRules: {
             '/seqlog': { proxy: process.env.NUXT_SEQ_RAW_URL ? process.env.NUXT_SEQ_RAW_URL : 'http://localhost:5341/api/events/raw' },
             '/Images/**': { proxy: process.env.NUXT_PUBLIC_SERVER_BASE ? `${process.env.NUXT_PUBLIC_SERVER_BASE}/Images/**` : 'http://localhost/Images/**' },
-        }
+        },
+        preset: 'node-cluster'
     },
     runtimeConfig: {
         seqServerApiKey: "",
@@ -29,9 +30,6 @@ export default defineNuxtConfig({
         '@pinia/nuxt',
         '@nuxtjs/device',
     ],
-    nitro: {
-        preset: 'node-cluster'
-    },
     css: [
         '@fortawesome/fontawesome-svg-core/styles.css',
         '~/assets/bootstrap/bootstrap.less',
