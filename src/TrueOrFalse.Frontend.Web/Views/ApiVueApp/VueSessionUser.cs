05ac95a8302e83465efefd28906434bc50a72c15
Daniel-Majunke <daniel.majunke@gmx.de>
2023-06-06 07:19:31 +0200

    change SessionUserlegacy to SessionUser #497

--- src/TrueOrFalse.Frontend.Web/Views/ApiVueApp/VueSessionUser.cs
+++ src/TrueOrFalse.Frontend.Web/Views/ApiVueApp/VueSessionUser.cs
@@ -9,21 +9,27 @@ namespace VueApp;
 public class VueSessionUser : IRegisterAsInstancePerLifetime
 {
     private readonly HttpContext _httpContext;
-
+    private readonly SessionUser _sessionUser; 
     public VueSessionUser(HttpContext httpContext)
     {
         _httpContext = httpContext;
+        if (httpContext == null)
+        {
+            Logg.r().Error("httpcontext is Empty VueSessionUser");
+            throw new NullReferenceException("httpcontext is Empty VueSessionUser"); 
+        }
+        _sessionUser = new SessionUser(httpContext);
     }
     public dynamic GetCurrentUserData()
     {
         var type = UserType.Anonymous;
-        if (SessionUserLegacy.IsLoggedIn)
+        if (_sessionUser.IsLoggedIn)
         {
-            if (SessionUserLegacy.User.IsGoogleUser)
+            if (_sessionUser.User.IsGoogleUser)
             {
                 type = UserType.Google;
             }
-            else if (SessionUserLegacy.User.IsFacebookUser)
+            else if (_sessionUser.User.IsFacebookUser)
             {
                 type = UserType.Facebook;
             }
@@ -32,24 +38,24 @@ public class VueSessionUser : IRegisterAsInstancePerLifetime
                 type = UserType.Normal;
             }
 
-            var activityPoints = SessionUserLegacy.User.ActivityPoints;
-            var activityLevel = SessionUserLegacy.User.ActivityLevel;
-            var subscriptionDate = SessionUserLegacy.User.EndDate;
+            var activityPoints = _sessionUser.User.ActivityPoints;
+            var activityLevel = _sessionUser.User.ActivityLevel;
+            var subscriptionDate = _sessionUser.User.EndDate;
             var settings = new JsonSerializerSettings { DateFormatString = "yyyy-MM-ddTHH:mm:ss.fffZ" };
             var json = JsonConvert.SerializeObject(DateTime.Now, settings);
             return new
             {
-                SessionUserLegacy.IsLoggedIn,
-                Id = SessionUserLegacy.UserId,
-                SessionUserLegacy.User.Name,
-                Email = SessionUserLegacy.User.EmailAddress,
-                IsAdmin = SessionUserLegacy.IsInstallationAdmin,
-                PersonalWikiId = SessionUserLegacy.User.StartTopicId,
+                _sessionUser.IsLoggedIn,
+                Id = _sessionUser.UserId,
+                _sessionUser.User.Name,
+                Email = _sessionUser.User.EmailAddress,
+                IsAdmin = _sessionUser.IsInstallationAdmin,
+                PersonalWikiId = _sessionUser.User.StartTopicId,
                 Type = type,
-                ImgUrl = new UserImageSettings(SessionUserLegacy.UserId).GetUrl_50px(SessionUserLegacy.User).Url,
-                SessionUserLegacy.User.Reputation,
-                SessionUserLegacy.User.ReputationPos,
-                PersonalWiki = new TopicControllerLogic(new SessionUser(_httpContext)).GetTopicData(SessionUserLegacy.User.StartTopicId),
+                ImgUrl = new UserImageSettings(_sessionUser.UserId).GetUrl_50px(_sessionUser.User).Url,
+                _sessionUser.User.Reputation,
+                _sessionUser.User.ReputationPos,
+                PersonalWiki = new TopicControllerLogic(_sessionUser).GetTopicData(_sessionUser.User.StartTopicId),
                 ActivityPoints = new
                 {
                     points = activityPoints,
@@ -61,12 +67,12 @@ public class VueSessionUser : IRegisterAsInstancePerLifetime
                         ? 0
                         : 100 * activityPoints / UserLevelCalculator.GetUpperLevelBound(activityLevel)
                 },
-                UnreadMessagesCount = Sl.Resolve<GetUnreadMessageCount>().Run(SessionUserLegacy.UserId),
-                SubscriptionType = SessionUserLegacy.User.EndDate > DateTime.Now
+                UnreadMessagesCount = Sl.Resolve<GetUnreadMessageCount>().Run(_sessionUser.UserId),
+                SubscriptionType = _sessionUser.User.EndDate > DateTime.Now
                     ? SubscriptionType.Plus
                     : SubscriptionType.Basic,
                 EndDate = subscriptionDate?.ToString("yyyy-MM-ddTHH:mm:ss.fffZ"),
-                SubscriptionStartDate = SessionUserLegacy.User.SubscriptionStartDate?.ToString("yyyy-MM-ddTHH:mm:ss.fffZ"),
+                SubscriptionStartDate = _sessionUser.User.SubscriptionStartDate?.ToString("yyyy-MM-ddTHH:mm:ss.fffZ"),
                 IsSubscriptionCanceled = subscriptionDate is
                 {
                     Year: < 9999
@@ -74,7 +80,7 @@ public class VueSessionUser : IRegisterAsInstancePerLifetime
             };
         }
 
-        var userLevel = UserLevelCalculator.GetLevel(SessionUserLegacy.GetTotalActivityPoints());
+        var userLevel = UserLevelCalculator.GetLevel(_sessionUser.GetTotalActivityPoints());
 
         return new
         {
@@ -87,10 +93,10 @@ public class VueSessionUser : IRegisterAsInstancePerLifetime
             ImgUrl = "",
             Reputation = 0,
             ReputationPos = 0,
-            PersonalWiki = new TopicControllerLogic(new SessionUser(_httpContext)).GetTopicData(RootCategory.RootCategoryId),
+            PersonalWiki = new TopicControllerLogic(_sessionUser).GetTopicData(RootCategory.RootCategoryId),
             ActivityPoints = new
             {
-                points = SessionUserLegacy.GetTotalActivityPoints(),
+                points = _sessionUser.GetTotalActivityPoints(),
                 level = userLevel,
                 levelUp = false,
                 activityPointsTillNextLevel = UserLevelCalculator.GetUpperLevelBound(userLevel)
