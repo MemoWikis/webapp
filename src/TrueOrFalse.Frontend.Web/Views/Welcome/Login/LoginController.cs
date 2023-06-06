bf13a7108600f0a8d31d162541d83825f8d1d26d
Daniel-Majunke <daniel.majunke@gmx.de>
2023-06-06 14:25:02 +0200

    Delete LoginModel #497

--- src/TrueOrFalse.Frontend.Web/Views/Welcome/Login/LoginController.cs
+++ src/TrueOrFalse.Frontend.Web/Views/Welcome/Login/LoginController.cs
@@ -1,5 +1,4 @@
 ﻿using System.Web.Mvc;
-using TrueOrFalse.Frontend.Web.Code;
 
 public class MVCLoginController : BaseController
 {
@@ -16,37 +15,4 @@ public class MVCLoginController : BaseController
     public JsonResult IsEmailAvailable(string selectedEmail) =>
         new JsonResult { Data = new { isAvailable = IsEmailAddressAvailable.Yes(selectedEmail) } };
 
-    [HttpPost]
-    public JsonResult Login(LoginModel loginModel)
-    {
-        var credentialsAreValid = R<CredentialsAreValid>();
-
-        if (credentialsAreValid.Yes(loginModel.EmailAddress, loginModel.Password))
-        {
-
-            if (loginModel.PersistentLogin)
-            {
-                WritePersistentLoginToCookie.Run(credentialsAreValid.User.Id);
-            }
-
-            SessionUserLegacy.Login(credentialsAreValid.User);
-
-            TransferActivityPoints.FromSessionToUser();
-            Sl.UserRepo.UpdateActivityPointsData();
-
-            return Json(new
-            {
-                Success = true,
-            });
-        }
-
-        return Json(new
-        {
-            Success = false,
-            Message = "Du konntest nicht eingeloggt werden. Bitte überprüfe deine E-Mail-Adresse und das Passwort."
-        });
-    }
-
-    public string LoginModal() => 
-        ViewRenderer.RenderPartialView("~/Views/Welcome/Login/Login.ascx", new LoginModel(), ControllerContext);
 }
