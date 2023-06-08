aedfe68ac864ece7c0bf282abb9decdeca3bef16
Daniel-Majunke <daniel.majunke@gmx.de>
2023-06-08 12:38:33 +0200

    Remove unused Code #497

--- src/TrueOrFalse.Frontend.Web/Code/BaseModel.cs
+++ src/TrueOrFalse.Frontend.Web/Code/BaseModel.cs
@@ -1,45 +1,10 @@
 ﻿public class BaseModel : BaseResolve
 {
-    public SponsorModel SponsorModel
-    {
-        get
-        {
-            if (_sponsorModel != null)
-            {
-                return _sponsorModel;
-            }
-
-            _sponsorModel = new SponsorModel();
-            return _sponsorModel;
-        }
-
-        set => _sponsorModel = value;
-    }
-
-    private SponsorModel _sponsorModel;
-    protected SessionUiData _sessionUiData => Resolve<SessionUiData>();
-
-    public bool IsLoggedIn => SessionUserLegacy.IsLoggedIn;
-    public bool IsInstallationAdmin => SessionUserLegacy.IsInstallationAdmin;
-
     public int UserId => SessionUserLegacy.UserId;
-
     public UserMenu UserMenu;
-    
-    public SidebarModel SidebarModel = new SidebarModel();
-
-    public TopNavMenu TopNavMenu = new TopNavMenu();
-
     public BaseModel()
     {
         var sessionUiData = Resolve<SessionUiData>();
         UserMenu = sessionUiData.UserMenu;
     }
-
-    public string UserMenuActive(UserMenuEntry userMenuEntry)
-    {
-        return  UserMenu.UserMenuActive(userMenuEntry);
-    }
-
-    public bool ShowSidebar = false;
 }
