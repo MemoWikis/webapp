497af00ccf4aafeea7404e2b91ff7f5d0e4fc872
Jun <mai.t.tung@gmail.com>
2023-06-26 12:47:23 +0200

    #506 extracted AllTopicsHistory

--- src/TrueOrFalse.Frontend.Web/Views/ApiVueApp/Pages/History/Topic/AllTopicsOverviewController.cs
+++ src/TrueOrFalse.Frontend.Web/Views/ApiVueApp/Pages/History/Topic/AllTopicsOverviewController.cs
@@ -2,48 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
-using NHibernate;
 using TrueOrFalse.Web;
 
 namespace VueApp;
 
 public class HistoryTopicAllTopicsOverviewController : BaseController
 {
-    private IOrderedEnumerable<CategoryChange> _orderedTopicChangesOnPage;
+    private readonly AllTopicsHistory _allTopicsHistory;
+
+    public HistoryTopicAllTopicsOverviewController(AllTopicsHistory allTopicsHistory)
+    {
+        _allTopicsHistory = allTopicsHistory;
+    }
 
     [HttpGet]
     public JsonResult Get(int page)
     {
-
         const int revisionsToShow = 100;
-        var revisionsToSkip = (page - 1) * revisionsToShow;
-        var query = $@"SELECT * FROM CategoryChange cc ORDER BY cc.DateCreated DESC LIMIT {revisionsToSkip},{revisionsToShow}";
-        _orderedTopicChangesOnPage = Sl.R<ISession>().CreateSQLQuery(query).AddEntity(typeof(CategoryChange)).List<CategoryChange>().OrderBy(c => c.Id);
-            
-        var days = _orderedTopicChangesOnPage
-            .Where(ChangeVisibilityCheck)
-            .GroupBy(change => change.DateCreated.Date)
-            .OrderByDescending(group => group.Key)
-            .Select(group => GetDay(
-                group.Key,
-                group.OrderByDescending(g => g.DateCreated).ToList()))
-            .ToArray();
-
+        var days = GetDays(page, revisionsToShow);
 
         return Json(days, JsonRequestBehavior.AllowGet);
     }
 
-    private bool ChangeVisibilityCheck(CategoryChange change)
+    private Day[] GetDays(int page, int revisionsToShow)
     {
-        return change.Category != null &&
-               change.Category.Id > 0 && 
-               PermissionCheck.CanView(change.Category) &&
-               PermissionCheck.CanView(change.Category.Creator.Id, change.GetCategoryChangeData().Visibility);
+        return _allTopicsHistory.GetDays(page, revisionsToShow).Select(group => GetDay(
+                @group.Key,
+                @group.OrderByDescending(g => g.DateCreated).ToList()))
+            .ToArray();
     }
 
-    public Day GetDay(DateTime date, IList<CategoryChange> topicChanges)
+    private Day GetDay(DateTime date, IList<CategoryChange> topicChanges)
     {
-
         var day = new Day
         {
             date = date.ToString("dd.MM.yyyy"),
@@ -67,7 +57,9 @@ public class HistoryTopicAllTopicsOverviewController : BaseController
     {
         if (change.AuthorId < 1)
             return null;
+
         var author = SessionUserCache.GetItem(change.AuthorId);
+
         return new Author
         {
             id = author.Id,
@@ -77,7 +69,7 @@ public class HistoryTopicAllTopicsOverviewController : BaseController
 
     public Change SetChange(CategoryChange topicChange)
     {
-        
+
         var change = new Change
         {
             topicId = topicChange.Category.Id,
@@ -91,33 +83,33 @@ public class HistoryTopicAllTopicsOverviewController : BaseController
 
         if (topicChange.Type == CategoryChangeType.Relations)
         {
-
             var previousChange = Sl.CategoryChangeRepo.GetForTopic(topicChange.Category.Id).OrderBy(c => c.Id).LastOrDefault(c => c.Id < topicChange.Id);
-            if (previousChange != null)
+            
+            if (previousChange == null) 
+                return change;
+
+            var previousRelations = CategoryEditData_V2.CreateFromJson(previousChange.Data).CategoryRelations;
+            var currentRelations = CategoryEditData_V2.CreateFromJson(topicChange.Data).CategoryRelations;
+
+            if (previousRelations.Count > currentRelations.Count)
             {
-                var previousRelations = CategoryEditData_V2.CreateFromJson(previousChange.Data).CategoryRelations;
-                var currentRelations = CategoryEditData_V2.CreateFromJson(topicChange.Data).CategoryRelations;
+                change.relationAdded = false;
+                var lastRelationDifference = previousRelations.Except(currentRelations).LastOrDefault();
 
-                if (previousRelations.Count > currentRelations.Count)
-                {
-                    change.relationAdded = false;
-                    var lastRelationDifference = previousRelations.Except(currentRelations).LastOrDefault();
+                if (PermissionCheck.CanViewCategory(lastRelationDifference.RelatedCategoryId) && lastRelationDifference.CategoryId == topicChange.Category.Id)
+                    change = GetAffectedTopicData(change, lastRelationDifference.RelatedCategoryId);
+                else if (PermissionCheck.CanViewCategory(lastRelationDifference.CategoryId))
+                    change = GetAffectedTopicData(change, lastRelationDifference.CategoryId);
+            }
+            else if (previousRelations.Count < currentRelations.Count)
+            {
+                change.relationAdded = true;
+                var lastRelationDifference = currentRelations.Except(previousRelations).LastOrDefault();
 
-                    if (PermissionCheck.CanViewCategory(lastRelationDifference.RelatedCategoryId) && lastRelationDifference.CategoryId == topicChange.Category.Id)
-                        change = GetAffectedTopicData(change, lastRelationDifference.RelatedCategoryId);
-                    else if (PermissionCheck.CanViewCategory(lastRelationDifference.CategoryId))
-                        change = GetAffectedTopicData(change, lastRelationDifference.CategoryId);
-                }
-                else if (previousRelations.Count < currentRelations.Count)
-                {
-                    change.relationAdded = true;
-                    var lastRelationDifference = currentRelations.Except(previousRelations).LastOrDefault();
-
-                    if (PermissionCheck.CanViewCategory(lastRelationDifference.RelatedCategoryId) && lastRelationDifference.CategoryId == topicChange.Category.Id)
-                        change = GetAffectedTopicData(change, lastRelationDifference.RelatedCategoryId);
-                    else if (PermissionCheck.CanViewCategory(lastRelationDifference.CategoryId))
-                        change = GetAffectedTopicData(change, lastRelationDifference.CategoryId);
-                }
+                if (PermissionCheck.CanViewCategory(lastRelationDifference.RelatedCategoryId) && lastRelationDifference.CategoryId == topicChange.Category.Id)
+                    change = GetAffectedTopicData(change, lastRelationDifference.RelatedCategoryId);
+                else if (PermissionCheck.CanViewCategory(lastRelationDifference.CategoryId))
+                    change = GetAffectedTopicData(change, lastRelationDifference.CategoryId);
             }
         }
 
@@ -133,6 +125,7 @@ public class HistoryTopicAllTopicsOverviewController : BaseController
 
         return change;
     }
+
 
     public class Day
     {
---
+++ src/TrueOrFalse/Domain/Category/History/AllTopicsHistory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NHibernate;
+using TrueOrFalse.Web;
+
+public class AllTopicsHistory : IRegisterAsInstancePerLifetime
+{
+    public IOrderedEnumerable<IGrouping<DateTime, CategoryChange>> GetDays(int page, int revisionsToShow)
+    {
+        var revisionsToSkip = (page - 1) * revisionsToShow;
+        var query =
+            $@"SELECT * FROM CategoryChange cc ORDER BY cc.DateCreated DESC LIMIT {revisionsToSkip},{revisionsToShow}";
+        var orderedTopicChangesOnPage = Sl.R<ISession>().CreateSQLQuery(query).AddEntity(typeof(CategoryChange))
+            .List<CategoryChange>().OrderBy(c => c.Id);
+
+        var days = orderedTopicChangesOnPage
+            .Where(ChangeVisibilityCheck)
+            .GroupBy(change => change.DateCreated.Date)
+            .OrderByDescending(group => @group.Key);
+
+        return days;
+    }
+    
+
+    private bool ChangeVisibilityCheck(CategoryChange change)
+    {
+        return change.Category != null && 
+               change.Category.Id > 0 &&
+               PermissionCheck.CanView(change.Category) &&
+               PermissionCheck.CanView(change.Category.Creator.Id, change.GetCategoryChangeData().Visibility);
+    }
+
+}
+
