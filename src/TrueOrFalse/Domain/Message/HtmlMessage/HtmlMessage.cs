921045d6b45c7e03285fba546d5340595b0002b4
Daniel-Majunke <daniel.majunke@gmx.de>
2023-08-14 14:46:22 +0200

    Remove unused Code and change HtmMessage #497

--- src/TrueOrFalse/Domain/Message/HtmlMessage/HtmlMessage.cs
+++ src/TrueOrFalse/Domain/Message/HtmlMessage/HtmlMessage.cs
@@ -1,33 +1,38 @@
-﻿
+﻿using RazorLight;
 
-using System.Net.Mail;
-using RazorLight;
-
-public class HtmlMessage
+public static class HtmlMessage
 {
-    public static void Send(MailMessage2 mailMessage, 
+
+    private static RazorLightEngine? _engine;
+
+    static HtmlMessage()
+    {
+        _engine = new RazorLightEngineBuilder()
+            .UseFileSystemProject(PathTo.EmailTemplate())
+            .UseMemoryCachingProvider()
+            .Build();
+    }
+    public static async Task Send(MailMessage2 mailMessage, 
         string messageTitle, 
         JobQueueRepo jobQueueRepo,
         UserReadingRepo userReadingRepo, 
         string signOutMessage, 
         string utmSource)
     {
-        var parsedTemplate = Razor.Parse(
-            File.ReadAllText(PathTo.EmailTemplate()),
-            new HtmlMessageModel
-            {
-                UserName = mailMessage.UserName,
-                MessageTitle = messageTitle,
-                Content = mailMessage.Body,
-                SignOutMessage = signOutMessage,
-                UtmSource = utmSource,
-                UtmCampaign = null
-            });
+        string result = await _engine?.CompileRenderAsync("EmailTemplate.cshtml", new HtmlMessageModel
+        {
+            UserName = mailMessage.UserName,
+            MessageTitle = messageTitle,
+            Content = mailMessage.Body,
+            SignOutMessage = signOutMessage,
+            UtmSource = utmSource,
+            UtmCampaign = null
+        });
 
-        mailMessage.Body = parsedTemplate;
+        mailMessage.Body = result;
         mailMessage.IsBodyHtml = true;
 
-        SendEmail.Run(mailMessage,jobQueueRepo, userReadingRepo);
+        SendEmail.Run(mailMessage, jobQueueRepo, userReadingRepo);
     }
 
     public static async Task SendAsync(
@@ -38,12 +43,7 @@ public class HtmlMessage
         string signOutMessage,
         string utmSource)
     {
-        var engine = new RazorLightEngineBuilder()
-            .UseFileSystemProject(PathTo.EmailTemplate())
-            .UseMemoryCachingProvider()
-            .Build();
-
-        string result = await engine.CompileRenderAsync("EmailTemplate.cshtml", new HtmlMessageModel
+        string result = await _engine?.CompileRenderAsync("EmailTemplate.cshtml", new HtmlMessageModel
         {
             UserName = mailMessage.UserName,
             MessageTitle = messageTitle,
