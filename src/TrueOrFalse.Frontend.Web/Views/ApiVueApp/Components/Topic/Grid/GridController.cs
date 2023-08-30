3f76c62f6cf941a92cc543f3f63c0cac5b2c0f02
Jun <mai.t.tung@gmail.com>
2023-08-30 13:48:56 +0200

    #189 added grid logic and controller

---
+++ src/TrueOrFalse.Frontend.Web/Views/ApiVueApp/Components/Topic/Grid/GridController.cs
@@ -0,0 +1,34 @@
+﻿using System.Web.Mvc;
+
+namespace VueApp;
+[SessionState(System.Web.SessionState.SessionStateBehavior.ReadOnly)]
+public class GridController : BaseController
+{
+    private readonly PermissionCheck _permissionCheck;
+    private readonly GridItemLogic _gridItemLogic;
+
+    public GridController(PermissionCheck permissionCheck,SessionUser sessionUser, GridItemLogic gridItemLogic) :base(sessionUser)
+    {
+        _permissionCheck = permissionCheck;
+        _gridItemLogic = gridItemLogic;
+        _sessionUser = sessionUser;
+    }
+
+    [HttpGet]
+    public JsonResult GetItem(int id)
+    {
+        var topic = EntityCache.GetCategory(id);
+        if (!_permissionCheck.CanView(topic))
+            return Json(new RequestResult { success = false, messageKey = FrontendMessageKeys.Error.Category.MissingRights }, JsonRequestBehavior.AllowGet);
+
+
+        var gridItem = _gridItemLogic.BuildGridTopicItem(topic);
+        return Json(new RequestResult { success = true, data = gridItem }, JsonRequestBehavior.AllowGet);
+    }
+}
+
+
+
+
+
+
