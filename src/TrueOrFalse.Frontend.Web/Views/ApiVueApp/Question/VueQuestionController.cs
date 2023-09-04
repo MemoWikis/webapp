d5207c82a7372b4cc100bb2480b2b40bbb8969b8
Jun <mai.t.tung@gmail.com>
2023-09-04 23:08:04 +0200

    #521 wip landingpage

--- src/TrueOrFalse.Frontend.Web/Views/ApiVueApp/Question/VueQuestionController.cs
+++ src/TrueOrFalse.Frontend.Web/Views/ApiVueApp/Question/VueQuestionController.cs
@@ -65,7 +65,7 @@ public class VueQuestionController : BaseController
                 renderedQuestionTextExtended = q.TextExtended != null ? MarkdownMarkdig.ToHtml(q.TextExtended) : "",
                 description = q.Description,
                 hasTopics = q.Categories.Any(),
-                primaryTopicUrl = "/" + UriSanitizer.Run(primaryTopic?.Name) + "/" + primaryTopic?.Id,
+                primaryTopicId = primaryTopic?.Id,
                 primaryTopicName = primaryTopic?.Name,
                 solution = q.Solution,
 
