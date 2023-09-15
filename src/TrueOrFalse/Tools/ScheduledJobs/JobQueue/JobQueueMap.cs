a18768caa4894d56923e911758354a261dd63278
Daniel-Majunke <daniel.majunke@gmx.de>
2023-09-15 08:18:37 +0200

    fix JobqueueData #497

--- src/TrueOrFalse/Tools/ScheduledJobs/JobQueue/JobQueueMap.cs
+++ src/TrueOrFalse/Tools/ScheduledJobs/JobQueue/JobQueueMap.cs
@@ -5,7 +5,7 @@ public class JobQueueMap : ClassMap<JobQueue>
     public JobQueueMap()
     {
         Id(x => x.Id);
-        Map(x => x.JobQueueType);
+        Map(x => x.JobQueueType).CustomType<JobQueueType>();
         Map(x => x.JobContent);
         Map(x => x.Priority);
     }
