9a75a698efcabf48649512bf99284ba2c8a0d175
Robert Mischke <robertmischke@gmail.com>
2023-09-16 23:20:58 +0200

    #497 removed unused folder/project

--- src/TrueOrFalse.Frontend.Web1/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TrueOrFalse.Frontend.Web1.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
