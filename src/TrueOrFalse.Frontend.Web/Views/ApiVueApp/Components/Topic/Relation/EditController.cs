29f171f790debd45cee971b5d665182775670d5d
Daniel-Majunke <daniel.majunke@gmx.de>
2023-10-04 13:53:20 +0200

    fix QuickCreate for Category #497

--- src/TrueOrFalse.Frontend.Web/Views/ApiVueApp/Components/Topic/Relation/EditController.cs
+++ src/TrueOrFalse.Frontend.Web/Views/ApiVueApp/Components/Topic/Relation/EditController.cs
@@ -1,75 +1,94 @@
 ﻿using System.Threading.Tasks;
+using HelperClassesControllers;
 using Microsoft.AspNetCore.Mvc;
 
-namespace VueApp;
-
-public class TopicRelationEditController : Controller
+namespace VueApp
 {
-    private readonly SessionUser _sessionUser;
-    private readonly EditControllerLogic _editControllerLogic;
 
-    public TopicRelationEditController(IGlobalSearch search,
-        SessionUser sessionUser,
-        EditControllerLogic editControllerLogic) 
+    public class TopicRelationEditController : Controller
     {
-        _sessionUser = sessionUser;
-        _editControllerLogic = editControllerLogic;
+        private readonly SessionUser _sessionUser;
+        private readonly EditControllerLogic _editControllerLogic;
+
+        public TopicRelationEditController(IGlobalSearch search,
+            SessionUser sessionUser,
+            EditControllerLogic editControllerLogic)
+        {
+            _sessionUser = sessionUser;
+            _editControllerLogic = editControllerLogic;
+        }
+
+        [HttpPost]
+        public JsonResult ValidateName([FromBody] NameHelper nameHelper)
+        {
+            var data = _editControllerLogic.ValidateName(nameHelper.Name);
+            return Json(data);
+        }
+
+        [AccessOnlyAsLoggedIn]
+        [HttpPost]
+        public JsonResult QuickCreate([FromBody] QuickCreatHelper quickCreatHelper)
+        {
+            var data = _editControllerLogic.QuickCreate(quickCreatHelper.Name,
+                    quickCreatHelper.ParentTopicId, 
+                    _sessionUser);
+
+            return Json(data);
+        }
+
+        [AccessOnlyAsLoggedIn]
+        [HttpPost]
+        public async Task<JsonResult> SearchTopic(string term, int[] topicIdsToFilter = null)
+        {
+            var data = _editControllerLogic.SearchTopic(term, topicIdsToFilter);
+            return Json(data);
+        }
+
+        [AccessOnlyAsLoggedIn]
+        [HttpPost]
+        public async Task<JsonResult> SearchTopicInPersonalWiki(string term, int[] topicIdsToFilter = null)
+        {
+            var data = _editControllerLogic.SearchTopicInPersonalWiki(term, topicIdsToFilter);
+            return Json(data);
+        }
+
+        [AccessOnlyAsLoggedIn]
+        [HttpPost]
+        public JsonResult MoveChild(int childId, int parentIdToRemove, int parentIdToAdd)
+        {
+            var data = _editControllerLogic.MoveChild(childId, parentIdToRemove, parentIdToAdd);
+            return Json(data);
+        }
+
+        [AccessOnlyAsLoggedIn]
+        [HttpPost]
+        public JsonResult AddChild(int childId, int parentId, int parentIdToRemove = -1,
+            bool addIdToWikiHistory = false)
+        {
+            var data = _editControllerLogic.AddChild(childId, parentId, parentIdToRemove, addIdToWikiHistory);
+            return Json(data);
+        }
+
+        [AccessOnlyAsLoggedIn]
+        [HttpPost]
+        public JsonResult RemoveParent(int parentIdToRemove, int childId, int[] affectedParentIdsByMove = null)
+        {
+            var data = _editControllerLogic.RemoveParent(parentIdToRemove, childId, affectedParentIdsByMove);
+            return Json(data);
+        }
+    }
+}
+
+namespace HelperClassesControllers
+{
+    public class NameHelper
+    {
+        public string Name { get; set; }
     }
 
-    [HttpPost]
-    public JsonResult ValidateName(string name)
+    public class QuickCreatHelper
     {
-        var data = _editControllerLogic.ValidateName(name);
-        return Json(data);
-    }
-
-    [AccessOnlyAsLoggedIn]
-    [HttpPost]
-    public JsonResult QuickCreate(string name, int parentTopicId)
-    {
-        var data = _editControllerLogic            
-            .QuickCreate(name, parentTopicId,_sessionUser); 
-
-        return Json(data);
-    }
-
-    [AccessOnlyAsLoggedIn]
-    [HttpPost]
-    public async Task<JsonResult> SearchTopic(string term, int[] topicIdsToFilter = null)
-    {
-        var data = _editControllerLogic.SearchTopic(term, topicIdsToFilter);
-        return Json(data);
-    }
-
-    [AccessOnlyAsLoggedIn]
-    [HttpPost]
-    public async Task<JsonResult> SearchTopicInPersonalWiki(string term, int[] topicIdsToFilter = null)
-    {
-        var data = _editControllerLogic.SearchTopicInPersonalWiki(term, topicIdsToFilter);
-        return Json(data);
-    }
-
-    [AccessOnlyAsLoggedIn]
-    [HttpPost]
-    public JsonResult MoveChild(int childId, int parentIdToRemove, int parentIdToAdd)
-    {
-        var data = _editControllerLogic.MoveChild(childId,parentIdToRemove,parentIdToAdd);
-        return Json(data);
-    }
-
-    [AccessOnlyAsLoggedIn]
-    [HttpPost]
-    public JsonResult AddChild(int childId, int parentId, int parentIdToRemove = -1, bool addIdToWikiHistory = false)
-    {
-        var data = _editControllerLogic.AddChild(childId, parentId, parentIdToRemove, addIdToWikiHistory);
-        return Json(data);
-    }
-
-    [AccessOnlyAsLoggedIn]
-    [HttpPost]
-    public JsonResult RemoveParent(int parentIdToRemove, int childId, int[] affectedParentIdsByMove = null)
-    {
-        var data = _editControllerLogic.RemoveParent(parentIdToRemove,childId,affectedParentIdsByMove);
-        return Json(data);
+        public string Name { get; set; }
+        public int ParentTopicId { get; set; } 
     }
 }
