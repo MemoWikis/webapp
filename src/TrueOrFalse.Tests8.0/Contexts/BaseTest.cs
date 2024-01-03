48b533f0c846901eda36acd81c1c94fd5e24477d
Daniel-Majunke <daniel.majunke@gmx.de>
2024-01-03 11:38:58 +0100

    change BaseTest to Net8.0 #670

--- src/TrueOrFalse.Tests8.0/Contexts/BaseTest.cs
+++ src/TrueOrFalse.Tests8.0/Contexts/BaseTest.cs
@@ -1,23 +1,28 @@
 ﻿using Autofac;
 using FakeItEasy;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
-using NHibernate;
 using Seedworks.Web.State;
 using TrueOrFalse;
 using TrueOrFalse.Infrastructure;
-using TrueOrFalse.Utilities.ScheduledJobs;
+using ISession = NHibernate.ISession;
 
 [TestFixture]
 public class BaseTest
 {
     private static IContainer _container;
     protected ILifetimeScope LifetimeScope;
+    private static User _sessionUser => new User
+    {
+        Name = "SessionUser",
+        Id = 1
+    };
     static BaseTest()
     {
-        #if DEBUG
-//            NHibernateProfiler.Initialize();
-        #endif
+#if DEBUG
+        //            NHibernateProfiler.Initialize();
+#endif
 
     }
 
@@ -25,7 +30,7 @@ public class BaseTest
     public void SetUp()
     {
         Settings.Initialize(CreateConfiguration());
-        SessionFactory.BuildTestConfiguration(); 
+        SessionFactory.BuildTestConfiguration();
         // CleanEmailsFromPickupDirectory.Run();
         InitializeContainer();
         LifetimeScope = _container.BeginLifetimeScope();
@@ -40,6 +45,7 @@ public class BaseTest
         var initilizer = Resolve<EntityCacheInitializer>();
         initilizer.Init(" (started in unit test) ");
         DateTimeX.ResetOffset();
+        SetSessionUserInDatabase(_sessionUser);
     }
 
     [TearDown]
@@ -58,30 +64,58 @@ public class BaseTest
         BuildContainer();
         ServiceLocator.Init(_container);
         SessionFactory.TruncateAllTables();
-        
     }
 
     private static void BuildContainer()
     {
+        _container = AutofacWebInitializer.GetTestContainer(SetWebHostEnvironment(), SetHttpContextAccessor());
+    }
+
+    private static IWebHostEnvironment SetWebHostEnvironment()
+    {
         var fakeWebHostEnvironment = A.Fake<IWebHostEnvironment>();
         App.Environment = fakeWebHostEnvironment;
         A.CallTo(() => fakeWebHostEnvironment.EnvironmentName).Returns("TestEnvironment");
-        _container = AutofacWebInitializer.GetContainer(fakeWebHostEnvironment); 
+        return fakeWebHostEnvironment;
+    }
+    private static IHttpContextAccessor SetHttpContextAccessor()
+    {
+        var httpContextAccessor = A.Fake<IHttpContextAccessor>();
+        var httpContext = A.Fake<HttpContext>();
+        var session = A.Fake<Microsoft.AspNetCore.Http.ISession>();
+        A.CallTo(() => httpContextAccessor.HttpContext).Returns(httpContext);
+        A.CallTo(() => httpContext.Session).Returns(session);
+
+        SetSessionValues(session); 
+
+        return httpContextAccessor;
     }
 
+    private static void SetSessionValues(Microsoft.AspNetCore.Http.ISession session)
+    {
+        byte[] userIdBytes = BitConverter.GetBytes(1);
+        if (BitConverter.IsLittleEndian)
+        {
+            Array.Reverse(userIdBytes);
+        }
+
+        A.CallTo(() => session.TryGetValue("userId", out userIdBytes)).Returns(true);
+    }
+
+    private static void SetSessionUserInDatabase(User user)
+    {
+        ContextUser.New(R<UserWritingRepo>())
+            .Add(_sessionUser)
+            .Persist();
+    }
 
     private IConfiguration CreateConfiguration()
     {
         var configurationBuilder = new ConfigurationBuilder();
-        configurationBuilder .AddJsonFile("appsettings.Test.json"); // Pfad zur Konfigurationsdatei
+        configurationBuilder.AddJsonFile("appsettings.Test.json");
         return configurationBuilder.Build();
     }
-   
-
-    // Jetzt können Sie Ihre Initialize-Methode mit dieser Konfiguration aufrufen
-   
 
     public static T Resolve<T>() where T : notnull => _container.Resolve<T>();
-
     public static T R<T>() where T : notnull => _container.Resolve<T>();
 }
