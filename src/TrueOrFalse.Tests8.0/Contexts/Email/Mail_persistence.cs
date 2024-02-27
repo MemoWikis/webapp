0749153cce7b6be8c4554cbbd4a5e2dcf10adcc9
Daniel-Majunke <daniel.majunke@gmx.de>
2024-02-27 19:01:53 +0100

    Refactoring: Moving Code from Controller to Business Logic and add Test

--- src/TrueOrFalse.Tests8.0/Contexts/Email/Mail_persistence.cs
+++ src/TrueOrFalse.Tests8.0/Contexts/Email/Mail_persistence.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net.Mail;
-using System.Threading;
-using NHibernate.Criterion;
-using NUnit.Framework;
+﻿using System.Net.Mail;
 using TrueOrFalse.Utilities.ScheduledJobs;
 
 
--- src/TrueOrFalse/Domain/Category/Ops/CategoryDeleter.cs
+++ src/TrueOrFalse/Domain/Category/Ops/CategoryDeleter.cs
@@ -8,9 +8,12 @@ public class CategoryDeleter(
     CategoryChangeRepo _categoryChangeRepo,
     CategoryValuationWritingRepo _categoryValuationWritingRepo,
     PermissionCheck _permissionCheck,
-    SessionUserCache _sessionUserCache)
+    SessionUserCache _sessionUserCache,
+    CrumbtrailService crumbtrailService,
+    CategoryRepository categoryRepo)
     : IRegisterAsInstancePerLifetime
 {
+
     ///todo:(DaMa)  Revise: Wrong place for SQL commands.
     public HasDeleted Run(Category category, int userId, bool isTestCase = false)
     {
@@ -45,6 +48,58 @@ public class CategoryDeleter(
 
         hasDeleted.DeletedSuccessful = true;
         return hasDeleted;
+    }
+
+    public RequestResult DeleteTopic(int id)
+    {
+        var redirectParent = GetRedirectTopic(id);
+        var topic = categoryRepo.GetById(id);
+        if (topic == null)
+            throw new Exception("Category couldn't be deleted. Category with specified Id cannot be found.");
+
+        var parentIds =
+            EntityCache.GetCategory(id).ParentCategories().Select(c => c.Id)
+                .ToList(); //if the parents are fetched directly from the category there is a problem with the flush
+        var parentTopics = categoryRepo.GetByIds(parentIds);
+
+        var hasDeleted = Run(topic, _sessionUser.UserId);
+        foreach (var parent in parentTopics)
+        {
+            _categoryChangeRepo.AddUpdateEntry(categoryRepo, parent, _sessionUser.UserId, false);
+        }
+
+        return new RequestResult
+        {
+            success = true,
+            data = new
+            {
+                hasChildren = hasDeleted.HasChildren,
+                isNotCreatorOrAdmin = hasDeleted.IsNotCreatorOrAdmin,
+                success = hasDeleted.DeletedSuccessful,
+                redirectParent = redirectParent
+            }
+        };
+    }
+
+
+    private dynamic GetRedirectTopic(int id)
+    {
+        var topic = EntityCache.GetCategory(id);
+        var currentWiki = EntityCache.GetCategory(_sessionUser.CurrentWikiId);
+        var lastBreadcrumbItem = crumbtrailService.BuildCrumbtrail(topic, currentWiki).Items.LastOrDefault();
+
+        if (lastBreadcrumbItem != null)
+            return new
+            {
+                name = lastBreadcrumbItem.Category.Name,
+                id = lastBreadcrumbItem.Category.Id
+            };
+
+        return new
+        {
+            name = currentWiki.Name,
+            id = currentWiki.Id
+        };
     }
 
     public class HasDeleted
