ecfff8ff4eae591e4a2a45f4831025dadbca1be9
Daniel-Majunke <daniel.majunke@gmx.de>
2024-03-07 18:42:21 +0100

    remove unused code

--- src/TrueOrFalse.Frontend.Web/Views/ApiVueApp/Stores/UserStoreController.cs
+++ src/TrueOrFalse.Frontend.Web/Views/ApiVueApp/Stores/UserStoreController.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Infrastructure;
 using TrueOrFalse.Domain.User;
 
 namespace VueApp;
