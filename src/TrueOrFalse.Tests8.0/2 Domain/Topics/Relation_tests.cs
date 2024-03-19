1bf86fa9a491c9e88a2bda6d38140531a9937106
Jun <mai.t.tung@gmail.com>
2024-03-19 15:53:03 +0100

    #189 wip test, added add child test

--- src/TrueOrFalse.Tests8.0/2 Domain/Topics/Relation_tests.cs
+++ src/TrueOrFalse.Tests8.0/2 Domain/Topics/Relation_tests.cs
@@ -1,5 +1,7 @@
 ﻿
 
+using System.Diagnostics;
+
 class Relation_tests : BaseTest
 {
     [Test]
@@ -7,14 +9,33 @@ class Relation_tests : BaseTest
     {
         var context = ContextCategory.New();
 
-        context.Add("RootElement").Persist();
+        context.Add("root").Persist();
 
         context
-            .Add("Sub1")
+            .Add("sub1")
+            .Add("sub2")
             .Persist();
 
+        var root = context.All.ByName("root");
+        var sub1 = context.All.ByName("sub1");
+        var sub2 = context.All.ByName("sub2");
 
+        context.AddChild(root, sub1);
+        context.AddChild(root, sub2);
 
+        RecycleContainerAndEntityCache();
+
+        var entityCacheInitializer = R<EntityCacheInitializer>();
+        entityCacheInitializer.Init();
+
+        var cachedRoot = EntityCache.GetCategory(root);
+        Assert.That(cachedRoot.ChildRelations.Count, Is.EqualTo(2));
+        Assert.AreEqual(cachedRoot.ChildRelations[0].PreviousId, null);
+
+        Assert.AreEqual(cachedRoot.ChildRelations[0].NextId, sub2.Id);
+        Assert.AreEqual(cachedRoot.ChildRelations[1].PreviousId, sub1.Id);
+
+        Assert.AreEqual(cachedRoot.ChildRelations[1].NextId, null);
 
     }
 
