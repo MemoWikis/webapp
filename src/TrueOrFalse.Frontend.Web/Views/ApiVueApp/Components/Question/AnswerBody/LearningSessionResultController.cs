d7b26edee59e30ad0b4868f2f15cb2c61ad9ea91
Daniel-Majunke <daniel.majunke@gmx.de>
2024-04-04 09:34:43 +0200

    #670 outsourced logic from LearningSessionResultController to LearningSessionResultService

--- src/TrueOrFalse.Frontend.Web/Views/ApiVueApp/Components/Question/AnswerBody/LearningSessionResultController.cs
+++ src/TrueOrFalse.Frontend.Web/Views/ApiVueApp/Components/Question/AnswerBody/LearningSessionResultController.cs
@@ -1,85 +1,10 @@
-﻿using System.Linq;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Infrastructure;
+﻿using Microsoft.AspNetCore.Mvc;
 
-public class VueLearningSessionResultController
-    : BaseController
+public class VueLearningSessionResultController(LearningSessionResultService _learningSessionResultService) : Controller
 {
-    private readonly LearningSessionCache _learningSessionCache;
-    private readonly ImageMetaDataReadingRepo _imageMetaDataReadingRepo;
-    private readonly IActionContextAccessor _actionContextAccessor;
-    private readonly IHttpContextAccessor _httpContextAccessor;
-    private readonly IWebHostEnvironment _webHostEnvironment;
-    private readonly QuestionReadingRepo _questionReadingRepo;
-
-    public VueLearningSessionResultController(LearningSessionCache learningSessionCache,
-        ImageMetaDataReadingRepo imageMetaDataReadingRepo,
-        IActionContextAccessor actionContextAccessor,
-        IHttpContextAccessor httpContextAccessor,
-        IWebHostEnvironment webHostEnvironment,
-        QuestionReadingRepo questionReadingRepo, SessionUser sessionUser) : base(sessionUser)
-    {
-        _learningSessionCache = learningSessionCache;
-        _imageMetaDataReadingRepo = imageMetaDataReadingRepo;
-        _actionContextAccessor = actionContextAccessor;
-        _httpContextAccessor = httpContextAccessor;
-        _webHostEnvironment = webHostEnvironment;
-        _questionReadingRepo = questionReadingRepo;
-    }
     [HttpGet]
-    public JsonResult Get()
+    public LearningSessionResultService.LearningSessionResult Get()
     {
-        var learningSession = _learningSessionCache.GetLearningSession();
-        var model = new LearningSessionResultModel(learningSession, _httpContextAccessor, _webHostEnvironment);
-        var questions = model.AnsweredStepsGrouped.Where(g => g.First().Question.Id != 0).Select(g =>
-        {
-            var question = g.First().Question;
-            return new {
-                    correctAnswerHtml = GetQuestionSolution.Run(question).GetCorrectAnswerAsHtml(),
-                    id = question.Id,
-                    imgUrl = GetQuestionImageFrontendData.Run(question,
-                        _imageMetaDataReadingRepo, 
-                        _httpContextAccessor, 
-                        _webHostEnvironment,
-                        _questionReadingRepo)
-                        .GetImageUrl(128, true).Url,
-                    title = question.GetShortTitle(),
-                    steps = g.Select(s => new {
-                        answerState = s.AnswerState,
-                        answerAsHtml = Question.AnswersAsHtml(s.Answer, question.SolutionType)
-                    }).ToArray(),
-                };
-        }).ToArray();
-
-        return Json(new
-        {
-            uniqueQuestionCount = model.NumberUniqueQuestions,
-            correct = new
-            {
-                percentage = model.NumberCorrectPercentage,
-                count = model.NumberCorrectAnswers
-            },
-            correctAfterRepetition = new
-            {
-                percentage = model.NumberCorrectAfterRepetitionPercentage,
-                count = model.NumberCorrectAfterRepetitionAnswers
-            },
-            wrong = new
-            {
-                percentage = model.NumberWrongAnswersPercentage,
-                count = model.NumberWrongAnswers
-            },
-            notAnswered = new
-            {
-                percentage = model.NumberNotAnsweredPercentage,
-                count = model.NumberNotAnswered
-            },
-            topicName = learningSession.Config.Category.Name,
-            topicId = learningSession.Config.Category.Id,
-            inWuwi = learningSession.Config.InWuwi,
-            questions = questions
-        });
+      return _learningSessionResultService.GetLearningSessionResult();
     }
 }
---
+++ src/TrueOrFalse/Domain/Knowledge/Learning/LearningSessionNew/Ops/LearningSessionResultModel.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+public class LearningSessionResultModel
+{
+    public LearningSession LearningSession;
+    private readonly IHttpContextAccessor _httpContextAccessor;
+    private readonly IWebHostEnvironment _webHostEnvironment;
+    public int NumberSteps;
+    public int NumberUniqueQuestions;
+    public int NumberCorrectAnswers; //answered correctly at first try
+    public int NumberCorrectAfterRepetitionAnswers; 
+    public int NumberWrongAnswers;
+    public int NumberNotAnswered;
+    public int NumberCorrectPercentage;
+    public int NumberCorrectAfterRepetitionPercentage;
+    public int NumberWrongAnswersPercentage;
+    public int NumberNotAnsweredPercentage;
+
+    public IEnumerable<IGrouping<int, LearningSessionStep>> AnsweredStepsGrouped;
+    public bool ShowSummaryText;
+    public int PercentageAverageRightAnswers;
+
+    public LearningSessionResultModel(LearningSession learningSession,
+        IHttpContextAccessor httpContextAccessor,
+        IWebHostEnvironment webHostEnvironment,
+        bool isInTestMode = false)
+    {
+        ShowSummaryText = !isInTestMode;
+        LearningSession = learningSession;
+        _httpContextAccessor = httpContextAccessor;
+        _webHostEnvironment = webHostEnvironment;
+        NumberSteps = LearningSession.Steps.Count();
+        var numberQuestions = LearningSession.Steps.Count(s => s.AnswerState == AnswerState.Unanswered || s.AnswerState == AnswerState.Skipped);
+        PercentageAverageRightAnswers = (int)Math.Round(LearningSession.Steps.Sum(s => s.Question.CorrectnessProbability) / (float)numberQuestions);
+
+        if (NumberSteps > 0)
+        {
+            AnsweredStepsGrouped = LearningSession.Steps.GroupBy(d => d.Question.Id);
+
+            NumberUniqueQuestions = AnsweredStepsGrouped.Count();
+
+            NumberCorrectAnswers = AnsweredStepsGrouped.Count(
+                g => g.First().AnswerState != AnswerState.Unanswered
+                && g.First().AnswerState == AnswerState.Correct);
+
+            NumberCorrectAfterRepetitionAnswers = AnsweredStepsGrouped.Count(
+                g => g.Last().AnswerState != AnswerState.Unanswered 
+                && g.Count() > 1 && g.Last().AnswerState == AnswerState.Correct);
+
+            NumberNotAnswered = AnsweredStepsGrouped.Count(g => g.All(a => a.AnswerState == AnswerState.Unanswered || a.AnswerState == AnswerState.Skipped));
+
+            NumberWrongAnswers = NumberUniqueQuestions - NumberNotAnswered - NumberCorrectAnswers - NumberCorrectAfterRepetitionAnswers;
+            
+            if (NumberWrongAnswers < 0)
+                Logg.r.Error("Answered questions (wrong+skipped+...) don't add up at LearningSessionResult for LearningSession");
+
+            PercentageShares.FromAbsoluteShares(
+                new List<ValueWithResultAction>
+                {
+                    new() {AbsoluteValue = NumberCorrectAnswers, ActionForPercentage = p => NumberCorrectPercentage = p},
+                    new() {AbsoluteValue = NumberCorrectAfterRepetitionAnswers, ActionForPercentage = p => NumberCorrectAfterRepetitionPercentage = p},
+                    new() {AbsoluteValue = NumberWrongAnswers, ActionForPercentage = p => NumberWrongAnswersPercentage = p},
+                    new() {AbsoluteValue = NumberNotAnswered, ActionForPercentage = p => NumberNotAnsweredPercentage = p},
+                });
+        }
+    }
+}
