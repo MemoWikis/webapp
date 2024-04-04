e9923d73a2a2676960ee631622272d14648a46b5
Daniel-Majunke <daniel.majunke@gmx.de>
2024-04-04 14:51:37 +0200

    #670 BreadCrumController reverse

--- src/TrueOrFalse.Frontend.Web/Views/ApiVueApp/Components/Header/BreadcrumbController.cs
+++ src/TrueOrFalse.Frontend.Web/Views/ApiVueApp/Components/Header/BreadcrumbController.cs
@@ -1,13 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using TrueOrFalse.Domain;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace VueApp;
-public class BreadcrumbController(SessionUser _sessionUser, BreadcrumbCreator _breadcrumbCreator) : Controller
+public class BreadcrumbController(
+    SessionUser _sessionUser,
+    CrumbtrailService _crumbtrailService) : Controller
 {
+    public readonly record struct GetBreadcrumbParam(int wikiId, int currentCategoryId);
+
     [HttpPost]
-    public Breadcrumb GetBreadcrumb([FromBody] BreadcrumbCreator.GetBreadcrumbParam param)
+    public Breadcrumb GetBreadcrumb([FromBody] GetBreadcrumbParam param)
     {
-        return _breadcrumbCreator.GetBreadcrumb(param); 
+        var wikiId = param.wikiId;
+        int currentCategoryId = param.currentCategoryId;
+
+        var defaultWikiId = _sessionUser.IsLoggedIn ? _sessionUser.User.StartTopicId : 1;
+        _sessionUser.SetWikiId(wikiId != 0 ? wikiId : defaultWikiId);
+        var category = EntityCache.GetCategory(currentCategoryId);
+        var currentWiki = _crumbtrailService.GetWiki(category, _sessionUser);
+        _sessionUser.SetWikiId(currentWiki);
+
+        var breadcrumb = _crumbtrailService.BuildCrumbtrail(category, currentWiki);
+
+        return GetBreadcrumbItems(breadcrumb, currentWiki);
     }
 
     [HttpGet]
@@ -19,5 +35,68 @@ public class BreadcrumbController(SessionUser _sessionUser, BreadcrumbCreator _b
             Name = topic.Name,
             Id = topic.Id
         };
+    }
+
+    private Breadcrumb GetBreadcrumbItems(Crumbtrail breadcrumb, CategoryCacheItem currentWiki)
+    {
+        var breadcrumbItems = new List<BreadcrumbItem>();
+
+        foreach (var item in breadcrumb.Items)
+        {
+            if (item.Category.Id != breadcrumb.Root.Category.Id)
+                breadcrumbItems.Add(new BreadcrumbItem
+                {
+                    Name = item.Text,
+                    Id = item.Category.Id
+                });
+        }
+
+        var personalWiki = new BreadcrumbItem();
+        if (_sessionUser.IsLoggedIn)
+        {
+            var personalWikiId = _sessionUser.User.StartTopicId;
+            personalWiki.Id = personalWikiId;
+            personalWiki.Name = EntityCache.GetCategory(personalWikiId).Name;
+        }
+        else
+        {
+            personalWiki.Id = RootCategory.RootCategoryId;
+            personalWiki.Name = RootCategory.Get.Name;
+        }
+
+        return new Breadcrumb
+        {
+            NewWikiId = currentWiki.Id,
+            Items = breadcrumbItems,
+            PersonalWiki = personalWiki,
+            RootTopic = new BreadcrumbItem
+            {
+                Name = breadcrumb.Root.Text,
+                Id = breadcrumb.Root.Category.Id
+            },
+            CurrentTopic = new BreadcrumbItem
+            {
+                Name = breadcrumb.Current.Text,
+                Id = breadcrumb.Current.Category.Id
+            },
+            BreadcrumbHasGlobalWiki = breadcrumb.Items.Any(c => c.Category.Id == RootCategory.RootCategoryId),
+            IsInPersonalWiki = _sessionUser.IsLoggedIn ? _sessionUser.User.StartTopicId == breadcrumb.Root.Category.Id : RootCategory.RootCategoryId == breadcrumb.Root.Category.Id
+        };
+    }
+
+    public class Breadcrumb
+    {
+        public int NewWikiId { get; set; }
+        public BreadcrumbItem PersonalWiki { get; set; }
+        public List<BreadcrumbItem> Items { get; set; }
+        public BreadcrumbItem RootTopic { get; set; }
+        public BreadcrumbItem CurrentTopic { get; set; }
+        public bool BreadcrumbHasGlobalWiki { get; set; }
+        public bool IsInPersonalWiki { get; set; }
+    }
+    public record struct BreadcrumbItem
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
     }
 }
