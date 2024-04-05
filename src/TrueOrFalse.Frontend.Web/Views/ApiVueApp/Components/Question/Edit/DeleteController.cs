d4079a71d24f431ad8fbd330bdb0746fd03b18ea
Daniel-Majunke <daniel.majunke@gmx.de>
2024-04-05 10:24:36 +0200

    #670 change return value in DeleteController

--- src/TrueOrFalse.Frontend.Web/Views/ApiVueApp/Components/Question/Edit/DeleteController.cs
+++ src/TrueOrFalse.Frontend.Web/Views/ApiVueApp/Components/Question/Edit/DeleteController.cs
@@ -1,21 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
 namespace VueApp;
-public class QuestionEditDeleteController : BaseController
+
+public class QuestionEditDeleteController(
+    QuestionReadingRepo _questionReadingRepo,
+    QuestionDelete _questionDelete,
+    LearningSessionCache _learningSessionCache)
+    : Controller
 {
-    private readonly QuestionReadingRepo _questionReadingRepo;
-    private readonly QuestionDelete _questionDelete;
-    private readonly LearningSessionCache _learningSessionCache;
-
-    public QuestionEditDeleteController(QuestionReadingRepo questionReadingRepo,
-        QuestionDelete questionDelete,
-        LearningSessionCache learningSessionCache, SessionUser sessionUser) : base(sessionUser)
-    {
-        _questionReadingRepo = questionReadingRepo;
-        _questionDelete = questionDelete;
-        _learningSessionCache = learningSessionCache;
-    }
-
     public record struct DeleteDetailsJson(
         string QuestionTitle,
         int TotalAnswers,
@@ -24,19 +15,19 @@ public class QuestionEditDeleteController : BaseController
         bool HasRights);
 
     [HttpGet]
-    public JsonResult DeleteDetails([FromRoute] int id)
+    public DeleteDetailsJson DeleteDetails([FromRoute] int id)
     {
         var question = _questionReadingRepo.GetById(id);
         var canBeDeleted = _questionDelete.CanBeDeleted(question.Creator.Id, question);
 
-        return Json(new DeleteDetailsJson(
+        return new DeleteDetailsJson(
 
             QuestionTitle: question.Text.TruncateAtWord(90),
             TotalAnswers: question.TotalAnswers(),
             CanNotBeDeleted: !canBeDeleted.Yes,
             WuwiCount: canBeDeleted.WuwiCount,
             HasRights: canBeDeleted.HasRights
-        ));
+        );
     }
 
     public record struct DeleteJson(
@@ -46,16 +37,16 @@ public class QuestionEditDeleteController : BaseController
 
     [AccessOnlyAsLoggedIn]
     [HttpPost]
-    public JsonResult Delete([FromRoute] int id)
+    public DeleteJson Delete([FromRoute] int id)
     {
         var updatedLearningSessionResult = _learningSessionCache.RemoveQuestionFromLearningSession(id);
 
         _questionDelete.Run(id);
 
-        return Json(new DeleteJson(
+        return new DeleteJson(
             ReloadAnswerBody: updatedLearningSessionResult.reloadAnswerBody,
             SessionIndex: updatedLearningSessionResult.sessionIndex,
             id = id
-        ));
+        );
     }
 }
--- src/TrueOrFalse.Frontend.Web/Views/ApiVueApp/Components/Search/SearchController.cs
+++ src/TrueOrFalse.Frontend.Web/Views/ApiVueApp/Components/Search/SearchController.cs
@@ -7,30 +7,27 @@ using TrueOrFalse.Frontend.Web.Code;
 
 namespace VueApp
 {
-    public class SearchController : BaseController
+    public class SearchController(IGlobalSearch _search,
+        SessionUser _sessionUser,
+        PermissionCheck _permissionCheck,
+        ImageMetaDataReadingRepo _imageMetaDataReadingRepo,
+        IHttpContextAccessor _httpContextAccessor,
+        QuestionReadingRepo _questionReadingRepo) : Controller
     {
-        private readonly PermissionCheck _permissionCheck;
-        private readonly ImageMetaDataReadingRepo _imageMetaDataReadingRepo;
-        private readonly IHttpContextAccessor _httpContextAccessor;
-        private readonly QuestionReadingRepo _questionReadingRepo;
-        private readonly IGlobalSearch _search;
 
-        public SearchController(IGlobalSearch search,
-            SessionUser sessionUser,
-            PermissionCheck permissionCheck,
-            ImageMetaDataReadingRepo imageMetaDataReadingRepo,
-            IHttpContextAccessor httpContextAccessor,
-            QuestionReadingRepo questionReadingRepo) :base(sessionUser)
-        {
-            _search = search;
-            _permissionCheck = permissionCheck;
-            _imageMetaDataReadingRepo = imageMetaDataReadingRepo;
-            _httpContextAccessor = httpContextAccessor;
-            _questionReadingRepo = questionReadingRepo;
-        }
         public readonly record struct SearchAllJson(string term);
+
+        public readonly record struct AllJson(
+           List<SearchTopicItem> Topics,
+            int TopicCount,
+            List<SearchQuestionItem> Questions,
+            int QuestionCount,
+            List<SearchUserItem> Users,
+            int UserCount,
+            string UserSearchUrl);
+
         [HttpPost]
-        public async Task<JsonResult> All([FromBody] SearchAllJson json)
+        public async Task<AllJson> All([FromBody] SearchAllJson json)
         {
             var topicItems = new List<SearchTopicItem>();
             var questionItems = new List<SearchQuestionItem>();
@@ -42,37 +39,37 @@ namespace VueApp
                 _questionReadingRepo);
 
             if (elements.Categories.Any())
-                searchHelper.AddTopicItems(topicItems, elements, _permissionCheck, UserId);
+                searchHelper.AddTopicItems(topicItems, elements, _permissionCheck, _sessionUser.UserId);
 
             if (elements.Questions.Any())
                 searchHelper.AddQuestionItems(questionItems, elements, _permissionCheck, _questionReadingRepo);
 
             if (elements.Users.Any())
                 searchHelper.AddUserItems(userItems, elements);
-            var result = Json(new
-            {
-                topics = topicItems,
-                topicCount = elements.CategoriesResultCount,
-                questions = questionItems,
-                questionCount = elements.QuestionsResultCount,
-                users = userItems,
-                userCount = elements.UsersResultCount,
-                userSearchUrl = Links.UsersSearch(json.term)
-            });
+            var result = new AllJson(
+            
+                Topics: topicItems,
+                TopicCount: elements.CategoriesResultCount,
+                Questions: questionItems,
+                QuestionCount: elements.QuestionsResultCount,
+                Users: userItems,
+                UserCount: elements.UsersResultCount,
+                UserSearchUrl: Links.UsersSearch(json.term)
+            );
 
             return result;
         }
         public readonly record struct SearchTopicJson(string term, int[] topicIdsToFilter);
         [HttpPost]
         public async Task<JsonResult> Topic([FromBody] SearchTopicJson json)
-        { 
+        {
             var items = new List<SearchTopicItem>();
             var elements = await _search.GoAllCategories(json.term, json.topicIdsToFilter);
 
             if (elements.Categories.Any())
                 new SearchHelper(_imageMetaDataReadingRepo,
                     _httpContextAccessor,
-                    _questionReadingRepo).AddTopicItems(items, elements, _permissionCheck, UserId);
+                    _questionReadingRepo).AddTopicItems(items, elements, _permissionCheck, _sessionUser.UserId);
 
             return Json(new
             {
@@ -94,7 +91,7 @@ namespace VueApp
                     .AddTopicItems(items,
                         elements,
                         _permissionCheck,
-                        UserId);
+                        _sessionUser.UserId);
 
             var wikiChildren = GraphService.Descendants(_sessionUser.User.StartTopicId);
             items = items.Where(i => wikiChildren.Any(c => c.Id == i.Id)).ToList();
@@ -126,7 +123,7 @@ namespace VueApp
                     var c = EntityCache.GetCategory(categoryId);
                     recentlyUsedRelationTargetTopicIds.Add(new SearchHelper(_imageMetaDataReadingRepo,
                         _httpContextAccessor,
-                        _questionReadingRepo).FillSearchCategoryItem(c, UserId));
+                        _questionReadingRepo).FillSearchCategoryItem(c, _sessionUser.UserId));
                 }
             }
 
@@ -138,7 +135,7 @@ namespace VueApp
                 personalWiki = new SearchHelper(_imageMetaDataReadingRepo,
                         _httpContextAccessor,
                         _questionReadingRepo)
-                    .FillSearchCategoryItem(personalWiki, UserId),
+                    .FillSearchCategoryItem(personalWiki, _sessionUser.UserId),
                 addToWikiHistory = recentlyUsedRelationTargetTopicIds.ToArray()
             });
         }
