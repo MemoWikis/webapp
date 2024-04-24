b597d7c1f66075fca54fa27e32d67546ec57e11b
Jun <mai.t.tung@gmail.com>
2024-04-24 16:43:05 +0200

    minor

--- src/TrueOrFalse.Frontend.Web.Nuxt/components/author/icon.vue
+++ src/TrueOrFalse.Frontend.Web.Nuxt/components/author/icon.vue
@@ -9,19 +9,25 @@ const props = defineProps<Props>()
 </script>
 
 <template>
-    <LazyNuxtLink :to="`/Nutzer/${props.author.name}/${props.author.id}`" v-tooltip="props.author.name" class="header-autho"
-        v-if="props.author.id > 0">
+    <LazyNuxtLink :to="`/Nutzer/${props.author.name}/${props.author.id}`" v-tooltip="props.author.name"
+        class="header-autho" v-if="props.author.id > 0">
         <Image :src="props.author.imgUrl" :format="ImageFormat.Author" class="header-author-icon"
             :alt="`${props.author.name}'s profile picture'`" />
     </LazyNuxtLink>
 </template>
 
-<style lang="less" scoped>
+<style lang="less">
 .header-author-icon {
     height: 20px;
     width: 20px;
     min-height: 20px;
     min-width: 20px;
     margin: 0 4px;
+    max-height: 20px;
+    max-width: 20px;
+
+    .img {
+        object-fit: cover;
+    }
 }
 </style>
