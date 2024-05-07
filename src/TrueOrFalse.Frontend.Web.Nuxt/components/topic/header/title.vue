ba9bb439263a649d7cd438f72ace001fc5880f06
Daniel-Majunke <daniel.majunke@gmx.de>
2024-05-07 13:47:06 +0200

    #769 change mobile author grouping and handling

--- src/TrueOrFalse.Frontend.Web.Nuxt/components/topic/header/title.vue
+++ src/TrueOrFalse.Frontend.Web.Nuxt/components/topic/header/title.vue
@@ -8,8 +8,23 @@ import { ImageFormat } from '~~/components/image/imageFormatEnum'
 const topicStore = useTopicStore()
 const tabsStore = useTabsStore()
 const textArea = ref()
-const firstAuthor = computed(() => topicStore.authors[0]);
-const lastAuthors = computed(() => topicStore.authors.length > 1 ? topicStore.authors.slice(1, topicStore.authors.length + 1) : [] as Author[])
+
+const mobileFirstAuthor = ref<Author>()
+const firstAuthors = computed(() => {
+    if (isMobile) {
+        mobileFirstAuthor.value = topicStore.authors[0]
+        return [] as Author[]
+    }
+    else
+        return topicStore.authors.length <= 4 ? topicStore.authors : topicStore.authors.slice(0, 4)
+})
+
+const groupedAuthors = computed(() => {
+    if (isMobile)
+        return topicStore.authors
+    else
+        return topicStore.authors.length > 4 ? topicStore.authors.slice(4, topicStore.authors.length + 1) : [] as Author[]
+})
 
 function resize() {
     let element = textArea.value as VueElement
@@ -60,9 +75,10 @@ async function scrollToChildTopics() {
         s.scrollIntoView({ behavior: 'smooth' })
 }
 
-const { isDesktopOrTablet, isMobile } = useDevice()
+const { isMobile } = useDevice()
 
 const topic = useState<Topic>('topic')
+
 </script>
 
 <template>
@@ -115,31 +131,39 @@ const topic = useState<Topic>('topic')
             <div class="topic-detail-spacer"
                 v-if="topicStore.views > 0 && (topicStore.childTopicCount > 0 || topicStore.parentTopicCount > 0)">
             </div>
+
             <div v-if="topicStore.views > 0" class="topic-detail">
                 <font-awesome-icon icon="fa-solid fa-eye" />
                 <div class="topic-detail-label">{{ topicStore.views }}</div>
             </div>
-            <!-- <div v-if="isMobile" class="topic-detail-flex-breaker"></div> -->
-            <div v-if="isDesktopOrTablet && (topicStore.views > 0 || (topicStore.childTopicCount > 0 || topicStore.parentTopicCount > 0))"
-                class="topic-detail-spacer">
+
+            <div v-if="topicStore.views > 0 ||
+                    (topicStore.childTopicCount > 0 || topicStore.parentTopicCount > 0)" class="topic-detail-spacer">
             </div>
 
-            <div class="topic-detail">
-                <LazyNuxtLink if="author.Id > 0" :to="$urlHelper.getUserUrl(firstAuthor.name, firstAuthor.id)"
-                    v-tooltip="firstAuthor.name">
-                    <Image :src="firstAuthor.imgUrl" :format="ImageFormat.Author" class="header-author-icon"
-                        :alt="`${firstAuthor.name}'s profile picture'`" />
+            <template v-for="author in firstAuthors">
+                <LazyNuxtLink v-if="author.id > 0" :to="$urlHelper.getUserUrl(author.name, author.id)"
+                    v-tooltip="author.name" class="header-author-icon-link">
+                    <Image :src="author.imgUrl" :format="ImageFormat.Author" class="header-author-icon"
+                        :alt="`${author.name}'s profile picture'`" />
                 </LazyNuxtLink>
-            </div>
+            </template>
+
             <VDropdown :distance="6">
-                <button v-show="(lastAuthors.length > 1)" class="additional-authors-btn"
-                    :class="{ 'long': lastAuthors.length > 9 }">
-                    <div v-if="lastAuthors.length > 1">
-                        +{{ lastAuthors.length }}
-                    </div>
-                </button>
+                <div v-if="isMobile && groupedAuthors.length == 1 && mobileFirstAuthor && mobileFirstAuthor.id > 0"
+                    :to="$urlHelper.getUserUrl(mobileFirstAuthor.name, mobileFirstAuthor.id)"
+                    class="header-author-icon-link">
+                    <Image :src="mobileFirstAuthor.imgUrl" :format="ImageFormat.Author" class="header-author-icon"
+                        :alt="`${mobileFirstAuthor.name}'s profile picture'`" />
+                </div>
+                <div v-else-if="groupedAuthors.length > 1" class="additional-authors-btn"
+                    :class="{ 'long': groupedAuthors.length > 9 }">
+                    <span>
+                        +{{ groupedAuthors.length }}
+                    </span>
+                </div>
                 <template #popper>
-                    <template v-for="author in lastAuthors">
+                    <template v-for="author in groupedAuthors">
                         <LazyNuxtLink class="dropdown-row" v-if="author.id > 0"
                             :to="$urlHelper.getUserUrl(author.name, author.id)">
                             <div class="dropdown-icon">
@@ -150,6 +174,8 @@ const topic = useState<Topic>('topic')
                     </template>
                 </template>
             </VDropdown>
+
+
         </div>
     </div>
 </template>
@@ -234,16 +260,6 @@ const topic = useState<Topic>('topic')
         align-items: center;
         min-height: 21px;
 
-        &.is-mobile {
-
-            .topic-detail,
-            .topic-detail-spacer {
-                margin-bottom: 8px;
-            }
-        }
-
-
-
         .header-author-icon-link {
             margin-right: 8px;
         }
@@ -276,10 +292,6 @@ const topic = useState<Topic>('topic')
 
                 transition: color ease-in-out 0.2s;
             }
-        }
-
-        .author-first {
-            margin-bottom: 0px;
         }
 
         .topic-detail-spacer {
