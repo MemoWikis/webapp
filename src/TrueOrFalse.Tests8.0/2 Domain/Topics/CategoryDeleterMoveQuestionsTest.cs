d448df4f807c3e82b0304cb8559f511423b2b26e
Daniel-Majunke <daniel.majunke@gmx.de>
2024-06-04 12:10:51 +0200

    #278 add CategoryDeleterMoveQuestionsTest

--- src/TrueOrFalse.Tests8.0/2 Domain/Topics/CategoryDeleterMoveQuestionsTest.cs
+++ src/TrueOrFalse.Tests8.0/2 Domain/Topics/CategoryDeleterMoveQuestionsTest.cs
@@ -1,4 +1,5 @@
-﻿using ISession = NHibernate.ISession;
+﻿using static TopicDataManager;
+using ISession = NHibernate.ISession;
 
 namespace TrueOrFalse.Tests8._0._2_Domain.Topics
 {
@@ -36,65 +37,74 @@ namespace TrueOrFalse.Tests8._0._2_Domain.Topics
                 R<CategoryRepository>(),
                 true);
 
-            var childFromDatabase = R<CategoryRepository>().GetByIdEager(child.Id);
-            questionContext.AddQuestion("Frage1", creator: creator, category: childFromDatabase);
+            questionContext.AddQuestion("Frage1", creator: creator, categories: new List<Category> { child });
             var categoryDeleter = R<CategoryDeleter>();
             //Act
-            var requestResult = categoryDeleter.DeleteTopic(child.Id, parent.Id);
+            categoryDeleter.DeleteTopic(child.Id, parent.Id);
+            RecycleContainerAndEntityCache();
+            var parentFromDb = R<CategoryRepository>().GetByIdEager(parent.Id);
+            var questionFromDb = R<QuestionReadingRepo>().GetById(questionContext.All.First().Id);
+            var parentFromCache = EntityCache.GetCategory(parentFromDb.Id);
+            var questionFromCache = EntityCache.GetQuestionById(questionFromDb.Id);
 
+            var categoryChange = R<CategoryChangeRepo>().GetForCategory(parent.Id);
+            var questionChange = R<QuestionChangeRepo>().GetByQuestionId(questionFromDb.Id);
 
             //Assert
-            Assert.IsNotNull(requestResult);
-            Assert.IsTrue(requestResult.Success);
-            Assert.IsFalse(requestResult.HasChildren);
-            Assert.IsFalse(requestResult.IsNotCreatorOrAdmin);
-            Assert.That(parent.Id, Is.EqualTo(requestResult.RedirectParent.Id));
+            Assert.IsNotNull(parentFromDb);
+            Assert.AreEqual(CategoryChangeType.Create, categoryChange.First().Type);
+            Assert.AreEqual(CategoryChangeType.Relations, categoryChange[1].Type);
+            Assert.AreEqual(CategoryChangeType.Update, categoryChange.Last().Type);
+            Assert.NotNull(questionChange);
+            Assert.AreEqual(QuestionChangeType.Create, questionChange.Type);
+
+            Assert.AreEqual(parentFromDb.CountQuestions, 1);
+            Assert.AreEqual(parentFromDb.Id, questionFromDb.Categories.First().Id);
+            Assert.AreEqual(questionFromDb.Categories.Count(), 1);
+
+            Assert.IsNotNull(parentFromCache);
+            Assert.AreEqual(parentFromCache.CountQuestions, 1);
+            Assert.AreEqual(parentFromCache.Id, questionFromDb.Categories.First().Id);
+            Assert.AreEqual(questionFromCache.Categories.Count(), 1);
         }
 
-        //[Test]
-        //public void DeleteCategoryWithQuestionTest()
-        //{
-        //    var contextTopic = ContextCategory.New();
-        //    var parentName = "parent name";
-        //    var childName = "child name";
-        //    var sessionUser = R<SessionUser>();
-        //    var creator = new User { Id = sessionUser.UserId };
+        [Test]
+        public void MoveQuestionNoParent()
+        {
+            var contextTopic = ContextCategory.New();
 
-        //    var parent = contextTopic.Add(
-        //            parentName,
-        //            CategoryType.Standard,
-        //            creator)
-        //        .GetTopicByName(parentName);
+            var sessionUser = R<SessionUser>();
+            var creator = new User { Id = sessionUser.UserId };
 
-        //    var child = contextTopic.Add(childName,
-        //            CategoryType.Standard,
-        //            creator)
-        //        .GetTopicByName(childName);
 
-        //    contextTopic.Persist();
-        //    contextTopic.AddChild(parent, child);
+            var child = contextTopic.Add("child",
+                    CategoryType.Standard,
+                    creator)
+                .GetTopicByName("child");
 
-        //    var categoryRepo = R<CategoryRepository>();
+            contextTopic.Persist();
 
-        //    var questionContext = ContextQuestion.New(
-        //        R<QuestionWritingRepo>(),
-        //        R<AnswerRepo>(),
-        //        R<AnswerQuestion>(),
-        //        R<UserWritingRepo>(),
-        //        categoryRepo,
-        //        true);
 
-        //    questionContext.AddQuestion("Frage1", creator: creator, categories: new List<Category> { parent });
+            var categoryRepo = R<CategoryRepository>();
 
-        //    var childCacheItem = EntityCache.GetCategory(child.Id);
+            var questionContext = ContextQuestion.New(
+                R<QuestionWritingRepo>(),
+                R<AnswerRepo>(),
+                R<AnswerQuestion>(),
+                R<UserWritingRepo>(),
+                categoryRepo,
+                true);
 
-        //    var modifyRelationsForCategory =
-        //        new ModifyRelationsForCategory(categoryRepo, R<CategoryRelationRepo>());
 
-        //    ModifyRelationsEntityCacheAndDb.RemoveRelationsForCategoryDeleter(childCacheItem, creator.Id,
-        //        modifyRelationsForCategory);
+            questionContext.AddQuestion("Frage1", creator: creator, categories: new List<Category> { child });
+            var parentId = 0;
+            RecycleContainerAndEntityCache();
 
-        //    categoryRepo.Delete(child);
-        //}
+            var exception = Assert.Throws<NullReferenceException>(() =>
+            {
+                R<CategoryDeleter>().DeleteTopic(child.Id, parentId);
+            });
+            Assert.AreEqual(exception.Message, "parent is null");
+        }
     }
 }
