130e055fbb4a2a822ecc0b4731bbb09d86f236bd
Daniel-Majunke <daniel.majunke@gmx.de>
2024-06-10 15:53:58 +0200

    #634 Redis Health check

--- src/TrueOrFalse.Frontend.Web/Program.cs
+++ src/TrueOrFalse.Frontend.Web/Program.cs
@@ -44,10 +44,11 @@ builder.Services.AddHttpContextAccessor();
 Settings.Initialize(builder.Configuration);
 
 if (Settings.UseRedisSession)
-    builder.Services.AddStackExchangeRedisCache(options =>
-    {
-        options.Configuration = Settings.RedisUrl;
-    });
+{
+    builder.Services.AddStackExchangeRedisCache(options => { options.Configuration = Settings.RedisUrl; });
+    var redisUrl = string.IsNullOrWhiteSpace(Settings.RedisUrl) ? "localhost: 6379" : Settings.RedisUrl;
+    RedisConnectionMonitor.CreateConnection(builder, redisUrl);
+}
 
 builder.Services.AddSession(options =>
 {
