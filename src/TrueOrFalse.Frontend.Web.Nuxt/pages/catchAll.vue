62c17b24a97bb715854ec4c79fdf21e0312e063f
Jun <mai.t.tung@gmail.com>
2024-06-25 17:18:52 +0200

    #881 minor

--- src/TrueOrFalse.Frontend.Web.Nuxt/pages/catchAll.vue
+++ src/TrueOrFalse.Frontend.Web.Nuxt/pages/catchAll.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 import { messages } from '~/components/alert/messages'
-throw createError({ statusCode: 404, statusMessage: messages.route.notFound })
+throw createError({ statusCode: 404, statusMessage: messages.error.route.notFound })
 </script>
 <template>
     <div></div>
