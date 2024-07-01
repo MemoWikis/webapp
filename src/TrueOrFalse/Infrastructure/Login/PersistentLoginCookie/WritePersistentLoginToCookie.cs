70917d7ba722b0e89fb0d6d58d406acb6fa6ad88
Jun <mai.t.tung@gmail.com>
2024-07-01 14:59:51 +0200

    #927 minor

--- src/TrueOrFalse/Infrastructure/Login/PersistentLoginCookie/WritePersistentLoginToCookie.cs
+++ src/TrueOrFalse/Infrastructure/Login/PersistentLoginCookie/WritePersistentLoginToCookie.cs
@@ -10,7 +10,7 @@ public class WritePersistentLoginToCookie
             HttpOnly = true,
             Secure = Settings.Environment != "develop", // Set to true if using HTTPS
             SameSite = SameSiteMode.Lax,
-            Expires = DateTimeOffset.UtcNow.AddDays(30)
+            Expires = DateTimeOffset.UtcNow.AddDays(180)
         };
         var loginGuid = CreatePersistentLogin.Run(userId, persistentLoginRepo);
         httpContext.Response.Cookies.Append(PersistentLoginCookie.Key, userId + "-x-" + loginGuid, cookieOptions);
@@ -20,7 +20,7 @@ public class WritePersistentLoginToCookie
     public static CookieResult Run(int userId, PersistentLoginRepo persistentLoginRepo)
     {
         var loginGuid = CreatePersistentLogin.Run(userId, persistentLoginRepo);
-        var expiryDate = DateTimeOffset.UtcNow.AddDays(30);
+        var expiryDate = DateTimeOffset.UtcNow.AddDays(180);
 
         return new CookieResult(userId + "-x-" + loginGuid, expiryDate);
     }
