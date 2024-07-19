5c50c525fdc381258bd85d4fcd11d52c147abbf5
Jun <mai.t.tung@gmail.com>
2024-07-19 12:21:08 +0200

    #954 minor

--- src/TrueOrFalse.Frontend.Web.Nuxt/app.vue
+++ src/TrueOrFalse.Frontend.Web.Nuxt/app.vue
@@ -183,7 +183,7 @@ useHead(() => ({
 	meta: [
 		{
 			name: 'viewport',
-			content: `width=device-width, initial-scale=1.0, interactive-widget=resizes-content}`
+			content: `width=device-width, initial-scale=1.0, interactive-widget=resizes-content`
 		},
 	]
 }))
