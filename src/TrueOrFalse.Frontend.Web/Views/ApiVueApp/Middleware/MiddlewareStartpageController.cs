ef894115c2af5e642d757e20e9c7b1f3f93b3c93
Daniel-Majunke <daniel.majunke@gmx.de>
2024-08-13 10:14:16 +0200

    #980 rename

---
+++ src/TrueOrFalse.Frontend.Web/Views/ApiVueApp/Middleware/MiddlewareStartpageController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class MiddlewareStartpageController(SessionUser _sessionUser) : Controller
+{
+    public readonly record struct TinyTopic(int Id, string Name);
+
+    [HttpGet]
+    public TinyTopic Get()
+    {
+        var topic = _sessionUser.IsLoggedIn
+            ? EntityCache.GetCategory(_sessionUser.User.StartTopicId)
+            : RootCategory.Get;
+        return new TinyTopic { Name = topic.Name, Id = topic.Id };
+    }
+}
