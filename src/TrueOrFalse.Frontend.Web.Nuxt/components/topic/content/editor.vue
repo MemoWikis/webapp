69d3f8469f6747bcd252672afd5a561b1b137279
Jun <mai.t.tung@gmail.com>
2024-08-20 16:51:05 +0200

    #973 recreate editor if not authorised, and destroy editor

--- src/TrueOrFalse.Frontend.Web.Nuxt/components/topic/content/editor.vue
+++ src/TrueOrFalse.Frontend.Web.Nuxt/components/topic/content/editor.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { useEditor, EditorContent, JSONContent } from '@tiptap/vue-3'
+import { useEditor, Editor, EditorContent, JSONContent } from '@tiptap/vue-3'
 import StarterKit from '@tiptap/starter-kit'
 import Link from '@tiptap/extension-link'
 import Placeholder from '@tiptap/extension-placeholder'
@@ -41,120 +41,133 @@ const userStore = useUserStore()
 const doc = new Y.Doc() // Initialize Y.Doc for shared editing
 
 const providerContentLoaded = ref(false)
-const provider = ref<TiptapCollabProvider | null>(null)
+// const provider = ref<TiptapCollabProvider | null>(null)
 
-if (userStore.isLoggedIn) {
-    provider.value = new TiptapCollabProvider({
-        baseUrl: "ws://localhost:3010/collaboration",
-        name: 'ydoc-' + topicStore.id,
-        token: userStore.collaborationToken,
-        preserveConnection: false,
-        document: doc,
-        onSynced() {
-            if (!doc.getMap('config').get('initialContentLoaded') && editor.value) {
-                doc.getMap('config').set('initialContentLoaded', true)
-                editor.value.commands.setContent(topicStore.initialContent)
-                providerContentLoaded.value = true
-            }
-        },
-        onAuthenticationFailed: ({ reason }) => {
-            loadCollab.value = false
-        },
-    })
+const provider = shallowRef<TiptapCollabProvider>()
+const editor = shallowRef<Editor>()
+const loadCollab = ref(true)
 
-    // new IndexeddbPersistence(`document-${topicStore.id}`, doc)
-}
-
-const loadCollab = ref(userStore.isLoggedIn)
-
-const editor = useEditor({
-    content: topicStore.initialContent,
-    extensions: [
-        StarterKit.configure({
-            heading: false,
-            codeBlock: false,
-            history: false,
-        }),
-        CustomHeading.configure({
-            levels: [2, 3, 4],
-            HTMLAttributes: {
-                class: 'heading',
+const recreate = () => {
+    console.log('recreated', userStore.isLoggedIn, loadCollab.value)
+    if (userStore.isLoggedIn && loadCollab.value) {
+        provider.value = new TiptapCollabProvider({
+            baseUrl: "ws://localhost:3010/collaboration",
+            name: 'ydoc-' + topicStore.id,
+            token: userStore.collaborationToken,
+            preserveConnection: false,
+            document: doc,
+            onAuthenticated() {
+                new IndexeddbPersistence(`document-${topicStore.id}`, doc)
             },
-        }),
-        Link.configure({
-            HTMLAttributes: {
-                rel: 'noopener noreferrer nofollow'
+            onAuthenticationFailed: ({ reason }) => {
+                loadCollab.value = false
+                recreate()
             },
-            openOnClick: true,
-        }),
-        Placeholder.configure({
-            emptyEditorClass: 'is-editor-empty',
-            emptyNodeClass: 'is-empty',
-            placeholder: 'Klicke hier um zu tippen ...',
-            showOnlyWhenEditable: true,
-            showOnlyCurrent: true,
-        }),
-        Underline,
-        ImageResize.configure({
-            inline: true,
-            allowBase64: true,
-        }),
-        CodeBlockLowlight.configure({
-            lowlight,
-        }),
-        TaskList,
-        TaskItem.configure({
-            nested: true,
-        }),
-        Indent,
-        ...(userStore.isLoggedIn && loadCollab.value) ? [
-            Collaboration.configure({
-                document: doc
+            onSynced() {
+                if (!doc.getMap('config').get('initialContentLoaded') && editor.value) {
+                    doc.getMap('config').set('initialContentLoaded', true)
+                    editor.value.commands.setContent(topicStore.initialContent)
+                    providerContentLoaded.value = true
+                }
+            },
+        })
+    }
+
+    editor.value?.destroy()
+
+    editor.value = new Editor({
+        content: topicStore.initialContent,
+        extensions: [
+            StarterKit.configure({
+                heading: false,
+                codeBlock: false,
+                history: false,
             }),
-            CollaborationCursor.configure({
-                provider: provider.value,
-                user: {
-                    name: userStore.name,
-                    color: getRandomColor(),
-                    avatar: userStore.imgUrl
+            CustomHeading.configure({
+                levels: [2, 3, 4],
+                HTMLAttributes: {
+                    class: 'heading',
                 },
-            })
-        ] : [History]
-    ],
-    onUpdate({ editor }) {
-        topicStore.contentHasChanged = true
-        if (editor.isEmpty)
-            topicStore.content = ''
-        else
-            topicStore.content = editor.getHTML()
+            }),
+            Link.configure({
+                HTMLAttributes: {
+                    rel: 'noopener noreferrer nofollow'
+                },
+                openOnClick: true,
+            }),
+            Placeholder.configure({
+                emptyEditorClass: 'is-editor-empty',
+                emptyNodeClass: 'is-empty',
+                placeholder: 'Klicke hier um zu tippen ...',
+                showOnlyWhenEditable: true,
+                showOnlyCurrent: true,
+            }),
+            Underline,
+            ImageResize.configure({
+                inline: true,
+                allowBase64: true,
+            }),
+            CodeBlockLowlight.configure({
+                lowlight,
+            }),
+            TaskList,
+            TaskItem.configure({
+                nested: true,
+            }),
+            Indent,
+            ...(userStore.isLoggedIn && loadCollab.value) ? [
+                Collaboration.configure({
+                    document: doc
+                }),
+                CollaborationCursor.configure({
+                    provider: provider.value,
+                    user: {
+                        name: userStore.name,
+                        color: getRandomColor(),
+                        avatar: userStore.imgUrl
+                    },
+                })
+            ] : [History]
+        ],
+        onUpdate({ editor }) {
+            topicStore.contentHasChanged = true
+            if (editor.isEmpty)
+                topicStore.content = ''
+            else
+                topicStore.content = editor.getHTML()
 
-        const contentArray: JSONContent[] | undefined = editor.getJSON().content
-        if (contentArray)
-            outlineStore.setHeadings(contentArray)
+            const contentArray: JSONContent[] | undefined = editor.getJSON().content
+            if (contentArray)
+                outlineStore.setHeadings(contentArray)
 
-        if (editor.isActive('heading'))
-            updateHeadingIds()
+            if (editor.isActive('heading'))
+                updateHeadingIds()
 
-        updateCursorIndex()
-    },
-    editorProps: {
-        handlePaste: (view, pos, event) => {
-            const firstNode = event.content.firstChild
-            if (firstNode != null && firstNode.type.name == 'image') {
-                if (!isEmpty(firstNode.attrs)) {
-                    let src = firstNode.attrs.src;
-                    if (src.length > 1048576 && src.startsWith('data:image')) {
-                        alertStore.openAlert(AlertType.Error, { text: messages.error.image.tooBig })
-                        return true
+            updateCursorIndex()
+        },
+        editorProps: {
+            handlePaste: (view, pos, event) => {
+                const firstNode = event.content.firstChild
+                if (firstNode != null && firstNode.type.name == 'image') {
+                    if (!isEmpty(firstNode.attrs)) {
+                        let src = firstNode.attrs.src;
+                        if (src.length > 1048576 && src.startsWith('data:image')) {
+                            alertStore.openAlert(AlertType.Error, { text: messages.error.image.tooBig })
+                            return true
+                        }
                     }
                 }
-            }
 
+            },
+            attributes: {
+                id: 'InlineEdit',
+            }
         },
-        attributes: {
-            id: 'InlineEdit',
-        }
-    },
+    })
+}
+
+onMounted(() => {
+    recreate()
 })
 
 function setHeadings() {
@@ -217,12 +230,17 @@ function updateCursorIndex() {
     outlineStore.nodeIndex = nodeIndex
 }
 
+onBeforeUnmount(() => {
+    provider.value?.destroy()
+    editor.value?.destroy()
+})
+
 </script>
 
 <template>
     <template v-if="editor">
         <EditorMenuBar :editor="editor" :heading="true" :is-topic-content="true" />
-        <editor-content :editor="editor" class="col-xs-12" ref="editorRef" />
+        <editor-content :editor="editor" class="col-xs-12" />
     </template>
 </template>
 
