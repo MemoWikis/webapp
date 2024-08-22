d9c012deeb44d9259db9de4d51a9647e70ab92f9
Jun <mai.t.tung@gmail.com>
2024-08-22 17:28:38 +0200

    #1010 wip

--- src/TrueOrFalse.Frontend.Web.Nuxt/middleware/01.renew-cookie.global.ts
+++ src/TrueOrFalse.Frontend.Web.Nuxt/middleware/01.renew-cookie.global.ts
@@ -2,7 +2,6 @@
 export default defineNuxtRouteMiddleware(async (to, from) => {
     const nuxtApp = useNuxtApp()
     if (import.meta.client && nuxtApp.isHydrating && nuxtApp.payload.serverRendered) {
-
         await $api<void>('/apiVue/MiddlewareRefreshCookie/Get',
             {
                 credentials: 'include',
@@ -11,5 +10,18 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
                     nuxtApp.$logger.error('renew-cookie.global.ts: error', [{ response: context.response, host: context.request }])
                 },
             })
+        try {
+            // await $api<void>('/apiVue/MiddlewareRefreshCookie/Get',
+            // {
+            //     credentials: 'include',
+            //     mode: 'cors',
+            //     onResponseError(context) {
+            //         nuxtApp.$logger.error('renew-cookie.global.ts: error', [{ response: context.response, host: context.request }])
+            //     },
+            // })
+        } catch (e) {
+
+        }
+
     }
 })
