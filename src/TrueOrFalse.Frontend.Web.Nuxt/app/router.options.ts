dfec6e5ea0769c6d811c5378e7d106b9a6dc098f
Jun <mai.t.tung@gmail.com>
2024-08-22 16:16:19 +0200

    #1010 added breadcrumb title, changed name overview to metrics

--- src/TrueOrFalse.Frontend.Web.Nuxt/app/router.options.ts
+++ src/TrueOrFalse.Frontend.Web.Nuxt/app/router.options.ts
@@ -203,10 +203,10 @@ export default <RouterConfig>{
             component: () => import('~/pages/catchAll.vue'),
             hidden: true
         },
-         {
-            name: 'overview',
-            path: '/Gesamtdaten',
-            component: () => import('~/pages/overview.vue')
+        {
+            name: 'metrics',
+            path: '/Metrik',
+            component: () => import('~/pages/metrics.vue')
         },
     ],
 }   
