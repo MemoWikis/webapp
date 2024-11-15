9ba06bdc3f268778cf665a7aa83e403bb2df6fc2
Jun <mai.t.tung@gmail.com>
2024-11-15 15:54:57 +0100

    #1128 wip

---
+++ src/TrueOrFalse.Frontend.Web.Nuxt/components/sidesheet/section.vue
@@ -0,0 +1,73 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+    <div class="sidesheet-section">
+        <div class="header">
+            <slot name="header"></slot>
+        </div>
+        <div class="content">
+            <slot name="content"></slot>
+        </div>
+        <div class="slot">
+            <slot name="footer"></slot>
+        </div>
+
+    </div>
+</template>
+
+<style lang="less" scoped>
+@import (reference) '~~/assets/includes/imports.less';
+@memo-grey-lightest: #f9f9f9;
+
+.sidesheet-section {
+    padding-bottom: 20px;
+
+    .header,
+    .content,
+    .slot {
+        padding: 0 10px;
+    }
+
+    .header {
+        :slotted(h4) {
+            color: @memo-grey-darker;
+        }
+
+        :slotted(.header-title) {
+            padding: 8px 0p;
+        }
+    }
+
+    .content {
+        :slotted(a) {
+            display: block;
+            text-decoration: none;
+            color: @memo-grey-dark;
+
+            .link {
+                border-radius: 24px;
+                padding: 8px 16px;
+                background: @memo-grey-lightest;
+                color: @memo-grey-dark;
+                text-overflow: ellipsis;
+                overflow: hidden;
+                white-space: nowrap;
+
+                &:hover {
+                    filter: brightness(0.95);
+                }
+
+                &:active {
+                    filter: brightness(0.9);
+                }
+            }
+        }
+
+
+
+
+    }
+}
+</style>
--- src/TrueOrFalse.Frontend.Web.Nuxt/components/sidesheet/sidesheet.vue
+++ src/TrueOrFalse.Frontend.Web.Nuxt/components/sidesheet/sidesheet.vue
@@ -1,6 +1,9 @@
 <script lang="ts" setup>
 
 import { debounce } from 'underscore'
+import { TinyPageModel, usePageStore } from '../page/pageStore'
+
+const pageStore = usePageStore()
 const windowWidth = ref(0)
 
 const resize = () => {
@@ -13,15 +16,17 @@ const resize = () => {
 const handleWidth = (newWidth: number) => {
     if (newWidth < 901) {
         hidden.value = true
-        collapse.value = true
+        collapsed.value = true
+        previouslyCollapsed.value = true
     }
     else if (newWidth < 1701 && newWidth > 900) {
         hidden.value = false
-        collapse.value = true
+        collapsed.value = true
+        previouslyCollapsed.value = true
     } else {
         hidden.value = false
-
-        collapse.value = false
+        collapsed.value = false
+        previouslyCollapsed.value = false
     }
 }
 
@@ -34,9 +39,8 @@ onMounted(() => {
 
 })
 
-const collapse = ref(false)
+const collapsed = ref(false)
 const hidden = ref(false)
-
 
 watch(windowWidth, (oldWidth, newWidth) => {
 
@@ -47,29 +51,152 @@ watch(windowWidth, (oldWidth, newWidth) => {
 
 }, { immediate: true })
 
+const mockData = ref(
+    {
+        title: 'Favourites',
+        links: [
+            {
+                title: 'Home',
+                url: '/'
+            },
+            {
+                title: 'About',
+                url: '/Ueber-uns/1876'
+            },
+            {
+                title: 'Contact',
+                url: '/Impressum'
+            }
+        ]
+    }
+)
+
+const previouslyCollapsed = ref(false)
+const recentPages = ref<TinyPageModel[]>()
+
+const handleRecentPage = () => {
+    const tinyPageModel = {
+        id: pageStore.id,
+        name: pageStore.name,
+        imgUrl: pageStore.imgUrl
+    } as TinyPageModel
+
+    if (recentPages.value) {
+        recentPages.value = recentPages.value.filter((page) => page.id !== tinyPageModel.id)
+
+        if (recentPages.value.length > 5) {
+            recentPages.value.pop()
+        }
+
+        recentPages.value.unshift(tinyPageModel)
+    } else {
+        recentPages.value = [tinyPageModel]
+    }
+
+}
+watch(() => pageStore.id, (id) => {
+    if (id) {
+        handleRecentPage()
+    }
+})
+
+onMounted(() => {
+    handleRecentPage()
+})
+
+const { $urlHelper } = useNuxtApp()
+
 </script>
 <template>
-    <div id="SideSheet" :class="{ 'collapsed': collapse, 'hide': hidden }">
-        <div class="sidesheet">
-            <slot></slot>
-        </div>
+    <div id="SideSheet" :class="{ 'collapsed': collapsed, 'hide': hidden }" @mouseover="collapsed = false" @mouseleave="collapsed = previouslyCollapsed">
+        <perfect-scrollbar>
+
+            <SidesheetSection>
+                <template #header>
+                    <h4>
+                        <font-awesome-icon :icon="['fas', 'star']" class="header-icon" />
+                        <div v-show="!hidden" :class="{ 'smol': collapsed }" class="header-title">
+                            {{ mockData.title }}
+
+                        </div>
+
+                    </h4>
+                </template>
+
+                <template #content>
+                    <NuxtLink v-for="link in mockData.links" :to="link.url">
+                        <div class="link">
+                            {{ link.title }}
+                        </div>
+                    </NuxtLink>
+                </template>
+            </SidesheetSection>
+
+            <SidesheetSection>
+                <template #header>
+                    <h4>
+                        <font-awesome-icon :icon="['fas', 'clock-rotate-left']" class="header-icon" />
+                        <div v-show="!hidden" :class="{ 'smol': collapsed }" class="header-title">
+                            Recent
+                        </div>
+
+                    </h4>
+                </template>
+
+                <template #content>
+                    <NuxtLink v-for="recentPage in recentPages" :to="$urlHelper.getPageUrl(recentPage.name, recentPage.id)">
+                        <div class="link">
+                            {{ recentPage.name }}
+                        </div>
+                    </NuxtLink>
+                </template>
+            </SidesheetSection>
+
+        </perfect-scrollbar>
     </div>
 </template>
 
 <style lang="less" scoped>
 @import (reference) '~~/assets/includes/imports.less';
+@memo-grey-lightest: #f9f9f9;
 
 #SideSheet {
-    // height: calc(100vh - 60px);
-    height: 100vh;
-    background: @memo-grey-lighter;
+    height: 100%;
+    background: @memo-grey-lightest;
     width: 400px;
     position: fixed;
     z-index: 2000;
     transition: width 0.3s ease-in-out;
+    padding-top: 40px;
+
+    h4 {
+        display: flex;
+        flex-wrap: nowrap;
+        align-items: center;
+        flex-direction: row;
+
+        .smol {
+            font-size: 14px;
+        }
+    }
+
+    .header-icon {
+        font-size: 24px;
+        margin-right: 12px;
+        transition: all 0.3s ease-in-out;
+    }
 
     &.collapsed {
         width: 100px;
+
+        .header-icon {
+            font-size: 36px;
+            margin-right: 0px;
+        }
+
+        h4 {
+            flex-direction: column;
+        }
     }
 
     &.hide {
