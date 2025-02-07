e47aba0394959db7bc4a9d8f35d00f34876974f1
Jun <mai.t.tung@gmail.com>
2025-02-07 13:11:49 +0100

    #1243 added favicons

--- src/TrueOrFalse.Frontend.Web.Nuxt/nuxt.config.ts
+++ src/TrueOrFalse.Frontend.Web.Nuxt/nuxt.config.ts
@@ -86,6 +86,16 @@ export default defineNuxtConfig({
     options: {
         scrollBehaviorType: 'smooth'
         }
+    },
+    app: {
+        head: {
+            link: [
+                { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+                { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+                { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+                { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+            ]   
+        }
     }
     // Einkommentieren, wenn Sourcemaps im ProdBuild benötigt:
     // sourcemap: {
