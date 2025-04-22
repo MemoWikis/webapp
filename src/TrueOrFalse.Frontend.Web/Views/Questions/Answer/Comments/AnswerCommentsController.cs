bca68830239442900b07bdb26f0bcdd717a61585
Robert Mischke <robertmischke@gmail.com>
2025-04-22 18:01:30 +0200

    #1057 - removed unused controller

--- src/TrueOrFalse.Frontend.Web/Views/Questions/Answer/Comments/AnswerCommentsController.cs
+++
@@ -1,8 +0,0 @@
-﻿public class AnswerCommentsController : BaseController
-{
-    public AnswerCommentsController(SessionUser sessionUser): base(sessionUser)
-    {
-        
-    }
-    //todo (DaMa) has a Model as partial
-}
