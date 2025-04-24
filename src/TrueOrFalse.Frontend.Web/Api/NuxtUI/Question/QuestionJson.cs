be62c7cddbb1fe567a03c0d10af183148ccf153d
Robert Mischke <robertmischke@gmail.com>
2025-04-24 22:28:59 +0200

    #1299 - reorganized files and folders

---
+++ src/TrueOrFalse.Frontend.Web/Api/NuxtUI/Question/QuestionJson.cs
@@ -0,0 +1,19 @@
+﻿namespace QuestionListJson;
+
+public class Question
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public int CorrectnessProbability { get; set; }
+    public string LinkToQuestion { get; set; }
+    public string ImageData { get; set; }
+    public bool IsInWishknowledge { get; set; }
+    public bool HasPersonalAnswer { get; set; }
+    public int LearningSessionStepCount { get; set; }
+    public string LinkToComment { get; set; }
+    public string LinkToQuestionVersions { get; set; }
+    public int SessionIndex { get; set; }
+    public QuestionVisibility Visibility { get; set; }
+    public int CreatorId { get; set; } = 0;
+    public KnowledgeStatus KnowledgeStatus { get; set; } = KnowledgeStatus.NotLearned;
+}
