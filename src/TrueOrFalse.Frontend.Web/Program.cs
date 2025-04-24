4a6467907b4d1869b16c3d6f2c2796685280a722
Robert Mischke <robertmischke@gmail.com>
2025-04-24 22:26:27 +0200

    #1299 - program.cs added try / catch, wip on enabling openApi generation

--- src/TrueOrFalse.Frontend.Web/Program.cs
+++ src/TrueOrFalse.Frontend.Web/Program.cs
@@ -10,6 +10,7 @@ using Serilog;
 using Serilog.Exceptions;
 using Stripe;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Text.Json;
 using Scalar.AspNetCore;
@@ -20,145 +21,152 @@ using TrueOrFalse.Utilities.ScheduledJobs;
 using TrueOrFalse.View.Web.Middlewares;
 using static System.Int32;
 
-
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Host.UseSerilog((ctx, _, log) =>
+try
 {
-    log.Enrich.FromLogContext()
-        .Enrich.WithExceptionDetails()
-        .WriteTo.Console()
-        .WriteTo.Seq(Settings.SeqUrl);
 
-    log.MinimumLevel.Is(
-        ctx.HostingEnvironment.IsDevelopment()
-            ? Serilog.Events.LogEventLevel.Information
-            : Serilog.Events.LogEventLevel.Error);
-});
+    var builder = WebApplication.CreateBuilder(args);
 
-
-builder.WebHost.ConfigureKestrel(serverOptions =>
-{
-    serverOptions.Limits.MaxRequestBodySize = 1073741824;
-});
-builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory())
-    .ConfigureContainer<ContainerBuilder>(containerBuilder =>
+    builder.Host.UseSerilog((ctx, _, log) =>
     {
-        containerBuilder.RegisterModule<AutofacCoreModule>();
+        log.Enrich.FromLogContext()
+            .Enrich.WithExceptionDetails()
+            .WriteTo.Console()
+            .WriteTo.Seq(Settings.SeqUrl);
+
+        log.MinimumLevel.Is(
+            ctx.HostingEnvironment.IsDevelopment()
+                ? Serilog.Events.LogEventLevel.Information
+                : Serilog.Events.LogEventLevel.Error);
     });
 
-builder.Services
-    .AddDistributedMemoryCache()
-    .AddHttpContextAccessor()
-    .AddEndpointsApiExplorer()
-    .AddControllersWithViews()
-    .AddJsonOptions(options =>
+
+    builder.WebHost.ConfigureKestrel(serverOptions => { serverOptions.Limits.MaxRequestBodySize = 1073741824; });
+    builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory())
+        .ConfigureContainer<ContainerBuilder>(containerBuilder =>
+        {
+            containerBuilder.RegisterModule<AutofacCoreModule>();
+        });
+
+    builder.Services
+        .AddDistributedMemoryCache()
+        .AddHttpContextAccessor()
+        .AddControllers()
+        .AddJsonOptions(options =>
+        {
+            options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+            options.JsonSerializerOptions.DefaultIgnoreCondition =
+                System.Text.Json.Serialization.JsonIgnoreCondition.Never;
+        });
+
+    builder.Services.AddEndpointsApiExplorer();
+
+    Settings.Initialize(builder.Configuration);
+
+    if (Settings.UseRedisSession)
+        builder.Services.AddStackExchangeRedisCache(options => { options.Configuration = Settings.RedisUrl; });
+
+    builder.Services.AddSession(options =>
     {
-        options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
-        options.JsonSerializerOptions.DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.Never;
+        options.IdleTimeout = TimeSpan.FromMinutes(Settings.SessionStateTimeoutInMin);
+        options.Cookie.HttpOnly = true;
+        options.Cookie.IsEssential = true;
     });
 
-Settings.Initialize(builder.Configuration);
-
-if (Settings.UseRedisSession)
-    builder.Services.AddStackExchangeRedisCache(options =>
+    builder.Services.Configure<FormOptions>(options =>
     {
-        options.Configuration = Settings.RedisUrl;
+        options.MemoryBufferThreshold = MaxValue;
+        options.ValueLengthLimit = MaxValue;
+        options.MultipartBodyLengthLimit = long.MaxValue;
+        options.BufferBodyLengthLimit = MaxValue;
     });
 
-builder.Services.AddSession(options =>
-{
-    options.IdleTimeout = TimeSpan.FromMinutes(Settings.SessionStateTimeoutInMin);
-    options.Cookie.HttpOnly = true;
-    options.Cookie.IsEssential = true;
-});
+    builder.Services.AddCors(options =>
+    {
+        options.AddPolicy("LocalhostCorsPolicy",
+            builder => builder
+                .WithOrigins("http://localhost:3000")
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .AllowCredentials());
+    });
 
-builder.Services.Configure<FormOptions>(options =>
-{
-    options.MemoryBufferThreshold = MaxValue;
-    options.ValueLengthLimit = MaxValue;
-    options.MultipartBodyLengthLimit = long.MaxValue;
-    options.BufferBodyLengthLimit = MaxValue;
-});
+    builder.Services.AddAntiforgery(options => { options.HeaderName = "X-CSRF-TOKEN"; });
+    builder.Services.AddHealthChecks();
+    builder.Services.AddOpenApi();
 
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("LocalhostCorsPolicy",
-        builder => builder
-            .WithOrigins("http://localhost:3000")
-            .AllowAnyMethod()
-            .AllowAnyHeader()
-            .AllowCredentials());
-});
-
-builder.Services.AddAntiforgery(options => { options.HeaderName = "X-CSRF-TOKEN"; });
-builder.Services.AddHealthChecks();
-builder.Services.AddOpenApi();
-
-builder.WebHost.ConfigureServices(services =>
-{
-    WebHostEnvironmentProvider.Initialize(services.BuildServiceProvider());
-});
+    builder.WebHost.ConfigureServices(services =>
+    {
+        WebHostEnvironmentProvider.Initialize(services.BuildServiceProvider());
+    });
 
 
-var app = builder.Build();
+    var app = builder.Build();
 
-var env = app.Environment;
-App.Environment = env;
+    var env = app.Environment;
+    App.Environment = env;
 
-if (env.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-    app.MapOpenApi();
-    app.MapScalarApiReference();
+    if (env.IsDevelopment())
+    {
+        app.UseDeveloperExceptionPage();
+        app.MapOpenApi();
+        app.MapScalarApiReference(options =>
+        {
+            options.DefaultHttpClient =
+                new KeyValuePair<ScalarTarget, ScalarClient>(ScalarTarget.CSharp, ScalarClient.Curl);
+        });
+    }
+    else
+    {
+        app.UseExceptionHandler("/Home/Error");
+        app.UseHsts();
+    }
+
+    ImageDirectoryCreator.CreateImageDirectories(env.ContentRootPath);
+
+    if (string.IsNullOrEmpty(env.WebRootPath))
+    {
+        env.WebRootPath = Path.Combine(AppContext.BaseDirectory, "wwwroot");
+    }
+
+    var update = app.Services.GetRequiredService<Update>();
+    update.Run();
+
+    StripeConfiguration.ApiKey = Settings.StripeSecurityKey;
+
+    var imagesPath = Settings.ImagePath;
+    app.UseStaticFiles(new StaticFileOptions
+    {
+        FileProvider = new PhysicalFileProvider(imagesPath),
+        RequestPath = "/Images"
+    });
+
+    app.UseSession();
+    app.UseRouting();
+    app.MapControllers();
+
+    app.UseMiddleware<ErrorHandlerMiddleware>();
+    app.UseMiddleware<AutoLoginMiddleware>();
+    app.UseMiddleware<RequestTimingMiddleware>();
+
+    app.MapControllerRoute(
+        name: "default", 
+        pattern: "apiVue/{controller}/{action}/{id?}"
+        ).WithOpenApi();
+    
+    app.MapHealthChecks("healthcheck_backend");
+
+    app.Urls.Add("http://*:5069");
+
+    var entityCacheInitializer = app.Services.GetRequiredService<EntityCacheInitializer>();
+    entityCacheInitializer.Init();
+
+    await JobScheduler.InitializeAsync();
+
+    app.Run();
+
 }
-else
+catch (Exception e)
 {
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
+    Logg.r.Fatal(e, "Fatal error");
+    throw;
 }
-
-ImageDirectoryCreator.CreateImageDirectories(env.ContentRootPath);
-
-if (string.IsNullOrEmpty(env.WebRootPath))
-{
-    env.WebRootPath = Path.Combine(AppContext.BaseDirectory, "wwwroot");
-}
-
-var update = app.Services.GetRequiredService<Update>();
-update.Run();
-
-StripeConfiguration.ApiKey = Settings.StripeSecurityKey;
-
-var imagesPath = Settings.ImagePath;
-app.UseStaticFiles(new StaticFileOptions
-{
-    FileProvider = new PhysicalFileProvider(imagesPath),
-    RequestPath = "/Images"
-});
-
-app.UseSession();
-app.UseRouting();
-app.MapControllers();
-
-app.UseMiddleware<ErrorHandlerMiddleware>();
-app.UseMiddleware<AutoLoginMiddleware>();
-app.UseMiddleware<RequestTimingMiddleware>();
-
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapControllerRoute(
-        name: "default",
-        pattern: "apiVue/{controller}/{action}/{id?}");
-
-    endpoints.MapHealthChecks("healthcheck_backend");
-});
-
-app.Urls.Add("http://*:5069");
-
-var entityCacheInitializer = app.Services.GetRequiredService<EntityCacheInitializer>();
-entityCacheInitializer.Init();
-
-await JobScheduler.InitializeAsync();
-
-app.Run();
