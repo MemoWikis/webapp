1d86f859cc38358f75112d33a9b31c99fb0bfc3e
Robert Mischke <robertmischke@gmail.com>
2025-04-26 10:08:18 +0200

    #1299 - developer api (wip) minor

---
+++ src/Backend/Api/Developer/DeveloperController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Hosting;
+
+[ApiController]
+[Route("[controller]/[action]/{id?}")]
+public class DeveloperController : ControllerBase
+{
+    [HttpPost]
+    [AccessOnlyLocal]
+    public IActionResult CreateSampleData()
+    {
+        if (!App.Environment.IsDevelopment())
+        {
+            return BadRequest("This method can only be called in a Development environment.");
+        }
+        
+        
+        
+        return Ok("Sample data created successfully.");
+    }
+}
