2576e4c5d868f873dcd0f69a68d4131d30a283c6
Robert Mischke <robertmischke@gmail.com>
2025-04-28 20:24:30 +0200

    #1299 - folder renames (wip) adjust docker pathes

--- src/Docker/Nuxt-docker.bat
+++ src/Docker/Nuxt-docker.bat
@@ -7,9 +7,9 @@ if %errorlevel% equ 0 (
   echo Image '%IMAGE_NAME%' already exists.
 ) else (
   echo Image '%IMAGE_NAME%' not found. Building the image...
-  docker build -t %IMAGE_NAME% ..\.\TrueOrFalse.Frontend.Web.Nuxt\
+  docker build -t %IMAGE_NAME% ..\.\Frontend.Web.Nuxt\
 )
 
-cd ..\.\TrueOrFalse.Frontend.Web.Nuxt\
+cd ..\.\Frontend.Web.Nuxt\
 docker-compose up -d
 timeout 1000
