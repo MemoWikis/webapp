b3fa3c20fcd039df0fcdc44fd4be6d93593c7572
Robert Mischke <robertmischke@gmail.com>
2025-05-10 15:03:21 +0200

    #1178 - mission-control.vue (wip) test infrastructure
    - Verify: scrubb DateCreated and DateModified

---
+++ src/Tests/VerifyGlobalInit.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.CompilerServices;
+
+internal static class VerifyGlobalInit
+{
+    [ModuleInitializer]
+    internal static void Init()
+    {
+        // Replace the values with «scrubbed» placeholders
+        VerifierSettings.ScrubMember("DateCreated");
+        VerifierSettings.ScrubMember("DateModified");
+    }
+}
