0225f6230d2caa505d7892546d99da6b9a403c24
Robert Mischke <robertmischke@gmail.com>
2025-05-25 14:53:45 +0200

    #1438 - moved learning session step to separate file

--- src/Backend.Core/Domain/Knowledge/Learning/LearningSessionNew/Ops/LearningSessionCreator.cs
+++ src/Backend.Core/Domain/Knowledge/Learning/LearningSessionNew/Ops/LearningSessionCreator.cs
@@ -57,17 +57,6 @@ public class LearningSessionCreator : IRegisterAsInstancePerLifetime
         public bool isLastStep { get; set; }
     }
 
-    public record struct LearningSessionResult()
-    {
-        public int Index { get; set; } = 0;
-        public Step[] Steps { get; set; } = Array.Empty<Step>();
-        public Step? CurrentStep { get; set; } = null;
-        public int ActiveQuestionCount { get; set; } = 0;
-        public bool AnswerHelp { get; set; } = true;
-        public bool IsInTestMode { get; set; } = false;
-        public string? MessageKey { get; set; } = null;
-    }
-
     private LearningSessionResult FillLearningSessionResult(
         LearningSession learningSession,
         LearningSessionResult result)
