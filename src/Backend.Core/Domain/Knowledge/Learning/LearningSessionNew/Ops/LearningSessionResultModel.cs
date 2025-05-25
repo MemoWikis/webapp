171cb713b740db898959fd5b5c17101a97654bf7
Robert Mischke <robertmischke@gmail.com>
2025-05-25 15:00:14 +0200

    #1438 - some minor refactorings

--- src/Backend.Core/Domain/Knowledge/Learning/LearningSessionNew/Ops/LearningSessionResultModel.cs
+++ src/Backend.Core/Domain/Knowledge/Learning/LearningSessionNew/Ops/LearningSessionResultModel.cs
@@ -26,35 +26,53 @@
         var numberQuestions = LearningSession.Steps.Count(s => s.AnswerState == AnswerState.Unanswered || s.AnswerState == AnswerState.Skipped);
         PercentageAverageRightAnswers = (int)Math.Round(LearningSession.Steps.Sum(s => s.Question.CorrectnessProbability) / (float)numberQuestions);
 
-        if (NumberSteps > 0)
+        if (NumberSteps <= 0)
         {
-            AnsweredStepsGrouped = LearningSession.Steps.GroupBy(d => d.Question.Id);
-
-            NumberUniqueQuestions = AnsweredStepsGrouped.Count();
-
-            NumberCorrectAnswers = AnsweredStepsGrouped.Count(
-                g => g.First().AnswerState != AnswerState.Unanswered
-                && g.First().AnswerState == AnswerState.Correct);
-
-            NumberCorrectAfterRepetitionAnswers = AnsweredStepsGrouped.Count(
-                g => g.Last().AnswerState != AnswerState.Unanswered 
-                && g.Count() > 1 && g.Last().AnswerState == AnswerState.Correct);
-
-            NumberNotAnswered = AnsweredStepsGrouped.Count(g => g.All(a => a.AnswerState == AnswerState.Unanswered || a.AnswerState == AnswerState.Skipped));
-
-            NumberWrongAnswers = NumberUniqueQuestions - NumberNotAnswered - NumberCorrectAnswers - NumberCorrectAfterRepetitionAnswers;
-            
-            if (NumberWrongAnswers < 0)
-                Log.Error("Answered questions (wrong+skipped+...) don't add up at LearningSessionResult for LearningSession");
-
-            PercentageShares.FromAbsoluteShares(
-                new List<ValueWithResultAction>
-                {
-                    new() {AbsoluteValue = NumberCorrectAnswers, ActionForPercentage = p => NumberCorrectPercentage = p},
-                    new() {AbsoluteValue = NumberCorrectAfterRepetitionAnswers, ActionForPercentage = p => NumberCorrectAfterRepetitionPercentage = p},
-                    new() {AbsoluteValue = NumberWrongAnswers, ActionForPercentage = p => NumberWrongAnswersPercentage = p},
-                    new() {AbsoluteValue = NumberNotAnswered, ActionForPercentage = p => NumberNotAnsweredPercentage = p},
-                });
+            return;
         }
+
+        AnsweredStepsGrouped = LearningSession.Steps.GroupBy(d => d.Question.Id);
+
+        var answeredStepsGrouped = 
+            AnsweredStepsGrouped as IGrouping<int, LearningSessionStep>[] ?? 
+            AnsweredStepsGrouped.ToArray();
+            
+        NumberUniqueQuestions = answeredStepsGrouped.Count();
+
+        NumberCorrectAnswers = answeredStepsGrouped
+            .Count(group => 
+                group.First().AnswerState != AnswerState.Unanswered && 
+                group.First().AnswerState == AnswerState.Correct
+            );
+
+        NumberCorrectAfterRepetitionAnswers = answeredStepsGrouped
+            .Count(group => 
+                group.Last().AnswerState != AnswerState.Unanswered && 
+                group.Count() > 1 && group.Last().AnswerState == AnswerState.Correct);
+
+        NumberNotAnswered = answeredStepsGrouped
+            .Count(group => 
+                group.All(a => 
+                    a.AnswerState == AnswerState.Unanswered || 
+                    a.AnswerState == AnswerState.Skipped)
+            );
+
+        NumberWrongAnswers = 
+            NumberUniqueQuestions - 
+            NumberNotAnswered - 
+            NumberCorrectAnswers - 
+            NumberCorrectAfterRepetitionAnswers;
+            
+        if (NumberWrongAnswers < 0)
+            Log.Error("Answered questions (wrong+skipped+...) don't add up at LearningSessionResult for LearningSession");
+
+        PercentageShares.FromAbsoluteShares(
+            new List<ValueWithResultAction>
+            {
+                new() { AbsoluteValue = NumberCorrectAnswers, ActionForPercentage = p => NumberCorrectPercentage = p },
+                new() { AbsoluteValue = NumberCorrectAfterRepetitionAnswers, ActionForPercentage = p => NumberCorrectAfterRepetitionPercentage = p },
+                new() { AbsoluteValue = NumberWrongAnswers, ActionForPercentage = p => NumberWrongAnswersPercentage = p },
+                new() { AbsoluteValue = NumberNotAnswered, ActionForPercentage = p => NumberNotAnsweredPercentage = p },
+            });
     }
 }
