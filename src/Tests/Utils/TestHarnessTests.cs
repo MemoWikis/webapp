2b354b59707abd6601d7134bd6357304686c16ed
Robert Mischke <robertmischke@gmail.com>
2025-06-05 20:38:56 +0200

    #1438 - decluttering learning session (wip) progress ScenarioBuilder, usage in TestHarness

--- src/Tests/Utils/TestHarnessTests.cs
+++ src/Tests/Utils/TestHarnessTests.cs
@@ -1,21 +1,11 @@
 ﻿[TestFixture]
-internal class TestHarnessTests
+internal class TestHarnessTests : BaseTestHarness
 {
-    private TestHarness _testHarness;
-
-    [OneTimeSetUp]
-    public async Task OneTimeSetUp()
-    {
-        _testHarness = await TestHarness.CreateAsync();
-        await _testHarness.InitAsync(keepData: false);
-    }
-
-    [OneTimeTearDown]
-    public async Task OneTimeTearDown() => await _testHarness.DisposeAsync();
-
     [Test]
     public async Task Should_create_testHarness_with_host_and_db_access()
     {
+        await ReloadCaches();
+        
         // arrange 
         var userWritRepo = _testHarness.R<UserWritingRepo>();
         var user = new User
@@ -37,19 +27,21 @@ internal class TestHarnessTests
     [Test]
     public async Task Should_create_testHarness_and_make_api_call()
     {
+        await ReloadCaches();
+
         string result = await _testHarness.ApiCall("apiVue/App/GetCurrentUser");
 
         await Verify(new { formattedJson = result });
     }
 
-    [Test]
-    public async Task TestHarness_CanUseSpecificScenarioImage()
-    {
-        await using var harness = await TestHarness.CreateWithScenarioImageAsync(ScenarioImageConstants.BaseName);
+    //[Test]
+    //public async Task TestHarness_CanUseSpecificScenarioImage()
+    //{
+    //    await using var harness = await TestHarness.CreateWithScenarioImageAsync(ScenarioImageConstants.BaseName);
 
-        // Assert
-        Assert.That(harness.Client, Is.Not.Null);
-        Assert.That(harness.ConnectionString, Is.Not.Empty);
-    }
+    //    // Assert
+    //    Assert.That(harness.Client, Is.Not.Null);
+    //    Assert.That(harness.ConnectionString, Is.Not.Empty);
+    //}
 }
 
