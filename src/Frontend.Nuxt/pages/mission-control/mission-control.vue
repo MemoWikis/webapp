5bb365c07419279f22ed1e1fd41c31c94b5367da
Jun <mai.t.tung@gmail.com>
2025-09-17 13:37:31 +0200

    #1509 added outline to mission control, fixed outline in general for small elements/sections

--- src/Frontend.Nuxt/pages/mission-control/mission-control.vue
+++ src/Frontend.Nuxt/pages/mission-control/mission-control.vue
@@ -4,6 +4,8 @@ import { KnowledgeSummary } from '~/composables/knowledgeSummary'
 import { ActivityCalendarData } from '~/composables/missionControl/learnCalendar'
 import { PageData } from '~/composables/missionControl/pageData'
 import { useUserStore } from '~/components/user/userStore'
+import { LayoutCardSize } from '~/composables/layoutCardSize'
+import missionControlSections from '~/constants/missionControlSections'
 
 const userStore = useUserStore()
 
@@ -62,71 +64,119 @@ watch(() => locale.value, async () => {
         await navigateToLocaleMissionControl()
 })
 
+const hasWikis = computed(() => {
+    return dashboardData.value?.wikis && dashboardData.value.wikis.length > 0
+})
+
+const hasFavorites = computed(() => {
+    return dashboardData.value?.favorites && dashboardData.value.favorites.length > 0
+})
+
+const missionControlHeader = ref()
+const missionControlHeaderHeight = ref(123) // Default height
+
+const sideBarMarginTop = computed(() => {
+    const defaultHeaderHeight = 123
+    if (missionControlHeaderHeight.value > defaultHeaderHeight) {
+        return missionControlHeaderHeight.value - defaultHeaderHeight + 25
+    }
+    return 25 // Default margin top if no header is available
+})
+
+// Watch for header height changes
+onMounted(() => {
+    if (missionControlHeader.value) {
+        // Initial height measurement
+        missionControlHeaderHeight.value = missionControlHeader.value.offsetHeight
+
+        // Set up ResizeObserver to watch for height changes
+        const resizeObserver = new ResizeObserver((entries) => {
+            for (const entry of entries) {
+                missionControlHeaderHeight.value = entry.contentRect.height
+            }
+        })
+
+        resizeObserver.observe(missionControlHeader.value)
+
+        // Cleanup on unmount
+        onUnmounted(() => {
+            resizeObserver.disconnect()
+        })
+    }
+})
+
 </script>
 
 <template>
 
     <div class="mission-control-container">
-        <h1>{{ t('missionControl.heading') }}</h1>
-        <div class="mission-control-content" v-if="dashboardData">
-            <!-- Knowledge Status Section -->
-            <LayoutPanel :title="t('missionControl.sections.knowledgeStatus')">
-                <LayoutCard :size="LayoutCardSize.Flex">
-                    <MissionControlKnowledgeSummary v-if="dashboardData.knowledgeStatus"
-                        :knowledgeStatus="dashboardData.knowledgeStatus" />
-                </LayoutCard>
-            </LayoutPanel>
+        <div class="mission-control-content">
+            <div class="mission-control-header" ref="missionControlHeader">
+                <h1>{{ t('missionControl.heading') }}</h1>
+            </div>
 
-            <DevOnly>
-                <!-- LearnCalendar Section -->
-                <LayoutPanel :title="t('missionControl.sections.learnCalendar')">
-                    <LayoutCard>
-                        <MissionControlLearnCalendar v-if="dashboardData.activityCalendar"
-                            :calendarData="dashboardData.activityCalendar" />
+            <div v-if="dashboardData">
+                <!-- Knowledge Status Section -->
+                <LayoutPanel :id="missionControlSections.KNOWLEDGE_STATUS_SECTION.id" :title="t(missionControlSections.KNOWLEDGE_STATUS_SECTION.translationKey)">
+                    <LayoutCard :size="LayoutCardSize.Flex">
+                        <MissionControlKnowledgeSummary v-if="dashboardData.knowledgeStatus"
+                            :knowledgeStatus="dashboardData.knowledgeStatus" />
                     </LayoutCard>
                 </LayoutPanel>
-            </DevOnly>
 
-            <template v-if="isMobile">
-                <!-- Wikis Section -->
-                <LayoutPanel v-if="dashboardData.wikis"
-                    :title="t('missionControl.sections.wikis')">
-                    <MissionControlGrid :pages="dashboardData.wikis" :no-pages-text="t('missionControl.pageTable.noWikis')" />
-                </LayoutPanel>
-                <!-- Favorites Section -->
-                <LayoutPanel v-if="dashboardData.favorites"
-                    :title="t('missionControl.sections.favorites')">
-                    <MissionControlGrid :pages="dashboardData.favorites" :no-pages-text="t('missionControl.pageTable.noFavorites')" />
-                </LayoutPanel>
-            </template>
+                <DevOnly>
+                    <!-- LearnCalendar Section -->
+                    <LayoutPanel :id="missionControlSections.LEARN_CALENDAR_SECTION.id" :title="t(missionControlSections.LEARN_CALENDAR_SECTION.translationKey)">
+                        <LayoutCard>
+                            <MissionControlLearnCalendar v-if="dashboardData.activityCalendar"
+                                :calendarData="dashboardData.activityCalendar" />
+                        </LayoutCard>
+                    </LayoutPanel>
+                </DevOnly>
 
-            <template v-else>
-                <!-- Wikis Section -->
-                <LayoutPanel v-if="dashboardData.wikis" :title="t('missionControl.sections.wikis')">
-                    <LayoutCard :no-padding="true">
-                        <MissionControlTable :pages="dashboardData.wikis" :no-pages-text="t('missionControl.pageTable.noWikis')" />
-                    </LayoutCard>
-                </LayoutPanel>
-                <!-- Favorites Section -->
-                <LayoutPanel v-if="dashboardData.favorites" :title="t('missionControl.sections.favorites')">
-                    <LayoutCard :no-padding="true">
-                        <MissionControlTable :pages="dashboardData.favorites" :no-pages-text="t('missionControl.pageTable.noFavorites')" />
-                    </LayoutCard>
-                </LayoutPanel>
-            </template>
+                <template v-if="isMobile">
+                    <!-- Wikis Section -->
+                    <LayoutPanel v-if="dashboardData.wikis" :id="missionControlSections.WIKIS_SECTION.id"
+                        :title="t(missionControlSections.WIKIS_SECTION.translationKey)">
+                        <MissionControlGrid :pages="dashboardData.wikis" :no-pages-text="t('missionControl.pageTable.noWikis')" />
+                    </LayoutPanel>
+                    <!-- Favorites Section -->
+                    <LayoutPanel v-if="dashboardData.favorites" :id="missionControlSections.FAVORITES_SECTION.id"
+                        :title="t(missionControlSections.FAVORITES_SECTION.translationKey)">
+                        <MissionControlGrid :pages="dashboardData.favorites" :no-pages-text="t('missionControl.pageTable.noFavorites')" />
+                    </LayoutPanel>
+                </template>
 
-            <!-- LearnCalendar Section with Coming Soon overlay -->
-            <LayoutPanel :title="t('missionControl.sections.learnCalendar')">
-                <div class="coming-soon-container">
-                    <MissionControlLearnCalendar v-if="dashboardData.activityCalendar" :calendarData="dashboardData.activityCalendar" />
-                    <div class="coming-soon-overlay">
-                        <div class="coming-soon-content">
-                            <div class="coming-soon-text">{{ t('general.comingSoon') }}</div>
+                <template v-else>
+                    <!-- Wikis Section -->
+                    <LayoutPanel v-if="dashboardData.wikis" :id="missionControlSections.WIKIS_SECTION.id" :title="t(missionControlSections.WIKIS_SECTION.translationKey)">
+                        <LayoutCard :no-padding="true">
+                            <MissionControlTable :pages="dashboardData.wikis" :no-pages-text="t('missionControl.pageTable.noWikis')" />
+                        </LayoutCard>
+                    </LayoutPanel>
+                    <!-- Favorites Section -->
+                    <LayoutPanel v-if="dashboardData.favorites" :id="missionControlSections.FAVORITES_SECTION.id" :title="t(missionControlSections.FAVORITES_SECTION.translationKey)">
+                        <LayoutCard :no-padding="true">
+                            <MissionControlTable :pages="dashboardData.favorites" :no-pages-text="t('missionControl.pageTable.noFavorites')" />
+                        </LayoutCard>
+                    </LayoutPanel>
+                </template>
+
+                <!-- LearnCalendar Section with Coming Soon overlay -->
+                <LayoutPanel :id="missionControlSections.LEARN_CALENDAR_SECTION.id" :title="t(missionControlSections.LEARN_CALENDAR_SECTION.translationKey)">
+                    <div class="coming-soon-container">
+                        <MissionControlLearnCalendar v-if="dashboardData.activityCalendar" :calendarData="dashboardData.activityCalendar" />
+                        <div class="coming-soon-overlay">
+                            <div class="coming-soon-content">
+                                <div class="coming-soon-text">{{ t('general.comingSoon') }}</div>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </LayoutPanel>
+                </LayoutPanel>
+            </div>
         </div>
+
+        <SidebarMissionControl :show-wikis="hasWikis" :show-favorites="hasFavorites" :margin-top="sideBarMarginTop" />
     </div>
 
 </template>
@@ -135,15 +185,37 @@ watch(() => locale.value, async () => {
 @import (reference) '~~/assets/includes/imports.less';
 
 .mission-control-container {
+    display: flex;
+    flex-wrap: nowrap;
+    gap: 0 1rem;
     width: 100%;
 
-    h1 {
-        margin-bottom: 24px;
-        font-weight: 600;
+    .mission-control-content {
+        max-width: 1200px;
+        width: calc(75% - 1rem);
+        flex-grow: 2;
+
+        .mission-control-header {
+            margin: 25px 0;
+
+            h1 {
+                margin-bottom: 24px;
+                font-weight: 600;
+            }
+        }
     }
 
-    .mission-control-content {
-        margin-top: 20px;
+    @media (max-width: 900px) {
+        .mission-control-content {
+            width: 100%;
+        }
+    }
+
+    .panel-divider {
+        height: 1px;
+        background: @memo-grey-lighter;
+        width: 100%;
+        margin-bottom: 20px;
     }
 
     .coming-soon-container {
