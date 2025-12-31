a8f557ec0ac7fa0754b592e38a476fee8f06f69d
Robert Mischke <robertmischke@gmail.com>
2025-12-31 15:20:18 +0100

    chore: enable dev tools only in development environment

--- src/Frontend.Nuxt/nuxt.config.ts
+++ src/Frontend.Nuxt/nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 
 export default defineNuxtConfig({
-    devtools: { enabled: true },
+    devtools: { enabled: process.env.NODE_ENV === 'development' },
     nitro: {
         compatibilityDate: '2025-05-08',
         preset: 'node-cluster',
