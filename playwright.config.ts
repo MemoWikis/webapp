0cf0e111b633b7cb1c3e861574e8c27de711d534
Robert Mischke <robertmischke@gmail.com>
2026-01-17 00:49:14 +0100

    refactor: clean up code formatting in Playwright tests for consistency

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test'
 
 export default defineConfig({
   testDir: './src/Frontend.Nuxt/tests/playwright',
@@ -12,4 +12,4 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
   ],
-});
+})
